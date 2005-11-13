2d169b61869ac55f115dfe3199cbe9191c32370a
hase <hase@05cee5c3-a2e9-0310-9523-9dfc2f93dbe1>
2005-11-13 15:24:24 +0000

    git-svn-id: http://springhead.info/spr2/Springhead2/trunk@789 05cee5c3-a2e9-0310-9523-9dfc2f93dbe1

---
+++ src/tests/Physics/PHSimpleGL/main.cpp
@@ -0,0 +1,37 @@
+#include <Springhead.h>		//	Springheadのインタフェース
+#include <gl/glut.h>
+#pragma hdrstop
+using namespace Spr;
+
+
+PHSdkIf* sdk;
+PHSceneIf* scene;
+PHSolidIf* solid1, *solid2;
+
+
+void Display(){
+}
+void Idle(){
+	solid1->AddForce(Vec3f(10,0,0), Vec3f(0,2,0));
+	solid2->AddForce(Vec3f(10,0,0), Vec3f(0,2,0));
+	scene->Step();
+	std::cout << solid1->GetFramePosition();
+	std::cout << solid2->GetFramePosition() << std::endl;
+}
+int main(int argc, char* argv[]){
+	sdk = CreatePHSdk();				//	SDKの作成
+	scene = sdk->CreateScene();			//	シーンの作成
+	PHSolidDesc desc;
+	desc.mass = 2.0;
+	desc.inertia *= 2.0;
+	solid1 = scene->CreateSolid(desc);	//	剛体をdescに基づいて作成
+	desc.center = Vec3f(1,0,0);
+	solid2 = scene->CreateSolid(desc);	//	剛体をdescに基づいて作成
+
+	glutInit(&argc, argv);
+	glutCreateWindow("PHSimple");
+	glutDisplayFunc(Display);
+	glutIdleFunc(Idle);
+	glutMainLoop();
+}
+
