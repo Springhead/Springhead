d6b833d6dc8510ab3c37ec9d59a2a693519d8a5a
tazaki <tazaki@05cee5c3-a2e9-0310-9523-9dfc2f93dbe1>
2005-12-05 16:12:47 +0000

    git-svn-id: http://springhead.info/spr2/Springhead2/trunk@938 05cee5c3-a2e9-0310-9523-9dfc2f93dbe1

--- src/Physics/PHConstraintEngine.cpp
+++ src/Physics/PHConstraintEngine.cpp
@@ -7,6 +7,106 @@
 using namespace PTM;
 namespace Spr{;
 
+/*	
+	u		: 各剛体の速度を並べたベクトル
+	udot	: uの時間微分
+	w		: 接触点での速度
+	J		: uからwを与えるヤコビ行列. w = J u
+	M		: 慣性行列
+	dt		: オイラー積分の時間ステップ
+	λ		: 接触力 * dt
+	Fext	: 外力およびコリオリ項
+
+	運動方程式は
+		M udot = J' λ/dt + Fext
+	オイラー積分により
+		u(t+1) = u(t) + M^-1 J' λ + dt M^-1 Fext
+	左からJをかけて
+		w = J M^-1 J' λ + J (u(t) + dt M^-1 Fext)
+	A = J M^-1 J'
+	b = J (u(t) + dt M^-1 Fext)
+	とおくと
+		w = A λ + b
+	さらに，垂直抗力，静止摩擦，動摩擦などを考慮することにより
+	以下の制約条件を得る
+		λ_i = λ_lo_i				 ==> w_i >= 0
+		λ_i = λ_hi_i				 ==> w_i <= 0
+		λ_lo_i < λ_i < λ_hi_i ==> w_i = 0
+	これの解λをLCP(A, b, λ_hi, λ_lo)と書く
+
+	反復解法:
+	A = D - Nとしたとき，適当なλ[0]を初期値として，漸化式
+		λ[k+1] = LCP(D, -N λ[k] + b, λ_hi, λ_lo)
+	によりλ[k]を更新していく．このとき
+		λ[k+1] = λ[k]
+	が成り立つならば
+		λ[k] = LCP(A, b, λ_hi, λ_lo)
+	となり元の問題が解けたことになる
+	さて，Dが対角行列ならば，LCP(D, -N λ[k] + b, λ_hi, λ_lo)の解は
+		λ[k+1] = min(max(λ_lo, D^-1(N λ[k] - b)), λ_hi)
+	で得られる．要するにこの式を繰り返し計算すればよい．
+	*/
+
+/* 以下は詳細な式展開
+	N : 剛体の数
+	K : 接触の数
+	lhs(i) : i番目の接触の「左側」の剛体の番号
+	rhs(i) : i番目の接触の「右側」の剛体の番号
+		lhs(i) < rhs(i)
+
+	[予備知識]
+	行列の積C = A Bについて
+	C(i,j) = Arow(i)' Bcol(j) = ∑_k=1^n A(i,k) B(k,j)		(n = Aの列数 = Bの行数)
+
+	[各行列の3x3ブロックの内容]
+	ある行列Aの(i,j)番目の3x3部分行列をA(i,j)と書く．
+
+	size(M) = (6N, 6N)
+	M(i,j) =
+		O		(i != j)
+		m_k * 1	(i = 2k)	m_kはk番目の剛体の質量, 1は3x3単位行列
+		I_k		(i = 2k+1)	I_kはk番目の剛体の慣性テンソル
+	
+	size(J) = (3K, 6N)
+	J(i,j) =
+		J_lin(i,lhs(i))		(j == 2lhs(i))
+		J_ang(i,lhs(i))		(j == 2lhs(i)+1)
+		J_lin(i,rhs(i))		(j == 2rhs(i))
+		J_ang(i,rhs(i))		(j == 2rhs(i)+1)
+		O					otherwise
+	＊J_lin, J_angの定義は省略（lecture12.pdfを参照してください）
+
+    T := M^-1 J'
+	T(i,j)
+		= ∑_k=1^2N M^-1(i,k) J'(k,j)
+		= ∑_k=1^2N M(i,k)^-1 J(j,k)'
+		= M(i,i)^-1 J(j,i)'
+		=	1/m_lhs(j)  J_lin(j,lhs(j))'	:= T_lin(j,lhs(j))	(i == 2lhs(j))
+			I_lhs(j)^-1 J_ang(j,lhs(j))'	:= T_ang(j,lhs(j))	(i == 2lhs(j)+1)
+			1/m_rhs(j)  J_lin(j,rhs(j))'	:= T_lin(j,rhs(j))	(i == 2rhs(j))
+			I_rhs(j)^-1 J_ang(j,rhs(j))'	:= T_ang(j,rhs(j))	(i == 2rhs(j)+1)
+			O							otherwise
+
+	A = J M^-1 J' = J T
+	size(A) = (3K, 3K)
+	A(i,j) = ∑_k=1^2N J(i,k) T(k,j) =
+		δ(lhs(i),lhs(j)) * J_lin(i,lhs(i)) T_lin(j,lhs(j)) + J_ang(i,lhs(i)) T_ang(j,lhs(j))) +
+		δ(lhs(i),rhs(j)) * J_lin(i,lhs(i)) T_lin(j,rhs(j)) + J_ang(i,lhs(i)) T_ang(j,rhs(j))) +
+		δ(rhs(i),lhs(j)) * J_lin(i,rhs(i)) T_lin(j,lhs(j)) + J_ang(i,rhs(i)) T_ang(j,lhs(j))) +
+		δ(rhs(i),rhs(j)) * J_lin(i,rhs(i)) T_lin(j,rhs(j)) + J_ang(i,rhs(i)) T_ang(j,rhs(j)))
+		=
+		δ(lhs(i),lhs(j)) * (1/m_lhs(i) J_lin(i,lhs(i)) J_lin(j,lhs(j))' + J_ang(i,lhs(i)) I_lhs(i)^-1 J_ang(j,lhs(j))') +
+		δ(lhs(i),rhs(j)) * (1/m_lhs(i) J_lin(i,lhs(i)) J_lin(j,rhs(j))' + J_ang(i,lhs(i)) I_lhs(i)^-1 J_ang(j,rhs(j))') +
+		δ(rhs(i),lhs(j)) * (1/m_rhs(i) J_lin(i,rhs(i)) J_lin(j,lhs(j))' + J_ang(i,rhs(i)) I_rhs(i)^-1 J_ang(j,lhs(j))') +
+		δ(rhs(i),rhs(j)) * (1/m_rhs(i) J_lin(i,rhs(i)) J_lin(j,rhs(j))' + J_ang(i,rhs(i)) I_rhs(i)^-1 J_ang(j,rhs(j))')
+	ただしδはクロネッカーデルタ(δ(i,j) == 1 if i == j or 0 otherwise)
+
+	b = J Vnc		(Vnc = u(t) + dt M^-1 Fext)
+	size(b) = 3K
+	b(i) = ∑_k=1^2N J(i,k) Vnc(k) =
+		J_lin(i,lhs(i)) Vnc(2lhs(i)) + J_ang(i,lhs(i)) Vnc(2lhs(i)+1) +
+		J_lin(i,rhs(i)) Vnc(2rhs(i)) + J_ang(i,rhs(i)) Vnc(2rhs(i)+1)
+*/
 
 /*
 //	衝突検出と接触力の計算
@@ -53,13 +153,11 @@ void PHConstraintEngine::RemoveSolid(PHSolid* s){
 
 void PHConstraintEngine::Init(){
 	int N = solids.size();
-	minv.resize(N);
-	Iinv.resize(N);
+	solidauxs.resize(N);
 	for(int i = 0; i < N; i++){
-		minv[i] = solids[i]->GetMassInv();
-		Iinv[i] = solids[i]->GetInertiaInv();
+		solidauxs[i].minv = solids[i]->GetMassInv();
+		solidauxs[i].Iinv = solids[i]->GetInertiaInv();
 	}
-
 }
 
 /**	Solidの端の位置をもち，ソートされるもの	*/
@@ -112,20 +210,35 @@ void PHConstraintEngine::Step(){
 		}
 	}
 */
+
+	double dt = ((PHSceneIf*)GetScene())->GetTimeStep();
+
 	//ここまでの処理でPHContactの配列を得る.
-	//これをもとにLCP構築
-	//ヤコビ行列に値を詰める
+
+	//LCP構築
+	//各Solidに関係する変数
+	for(int i = 0; i < solids.size(); i++){
+		solidauxs[i].dVlin_nc = solids[i]->GetVelocity() + solidauxs[i].minv * solids[i]->GetForce() * dt;
+		Vec3d w = solids[i]->GetAngularVelocity();
+		solidauxs[i].dVang_nc = w + solidauxs[i].Iinv * (solids[i]->GetTorque() - w % (solids[i]->GetInertia() * w)) * dt;
+	}
+
+	//各Contactに関係する変数
 	Vec3d n, c, r[2], v[2], vrel, t[2];
 	Matrix3d rcross[2];
 	Posed q[2];
 	PHSolid* solid[2];
-	for(PHContacts::iterator iv = contacts.begin(); iv != contacts.end(); iv++){
-		n = iv->normal;	//法線
-		for(PHContactPoints::iterator ip = iv->points.begin(); ip != iv->points.end(); ip++){
+	PHSolidAux* solidaux[2];
+	for(PHContacts::iterator icon = contacts.begin(); icon != contacts.end(); icon++){
+		n = icon->normal;	//法線
+		for(int i = 0; i < 2; i++){
+			solid[i]    =  solids[icon->solids[i]];
+			solidaux[i] = &solidauxs[icon->solids[i]];
+            q[i] = solid[i]->GetPose();
+		}
+		for(PHContactPoints::iterator ip = icon->points.begin(); ip != icon->points.end(); ip++){
 			c = ip->pos;	//接触点
 			for(int i = 0; i < 2; i++){
-				solid[i] = solids[iv->solids[i]];
-                q[i] = solid[i]->GetPose();
 				r[i] = c - q[i].pos;	//剛体の中心から接触点までのベクトル
 				rcross[i] = Matrix3d::Cross(r[i]);
 				v[i] = solid[i]->GetVelocity() + solid[i]->GetAngularVelocity() % r[i];	//接触点での速度
@@ -137,120 +250,76 @@ void PHConstraintEngine::Step(){
 			t[0] = vrel - (n * vrel) * n;
 			t[1] = t[0] % n;
 
-			ip->Jlin[0].row(0) = -n;
-			ip->Jlin[0].row(1) = -t[0];
-			ip->Jlin[0].row(2) = -t[1];
-			ip->Jang[0].row(0) = -rcross[0] * n;
-			ip->Jang[0].row(1) = -rcross[0] * t[0];
-			ip->Jang[0].row(2) = -rcross[0] * t[1];
-			ip->Jlin[1].row(0) =  n;
-			ip->Jlin[1].row(1) =  t[0];
-			ip->Jlin[1].row(2) =  t[1];
-			ip->Jang[1].row(0) =  rcross[1] * n;
-			ip->Jang[1].row(1) =  rcross[1] * t[0];
-			ip->Jang[1].row(2) =  rcross[1] * t[1];
+			for(int i = 0; i < 2; i++){
+				ip->Jlin[i].row(0) = n;
+				ip->Jlin[i].row(1) = t[0];
+				ip->Jlin[i].row(2) = t[1];
+				ip->Jang[i].row(0) = rcross[0] * n;
+				ip->Jang[i].row(1) = rcross[0] * t[0];
+				ip->Jang[i].row(2) = rcross[0] * t[1];
+				if(i == 0){
+					ip->Jlin[i] *= -1.0;
+					ip->Jang[i] *= -1.0;
+				}
+				ip->Tlin[i] = solidaux[i]->minv * ip->Jlin[i].trans();
+				ip->Tang[i] = solidaux[i]->Iinv * ip->Jang[i].trans();
+			}
+			ip->b = ip->Jlin[0] * solidaux[0]->dVlin_nc + ip->Jang[0] * solidaux[0]->dVang_nc +
+					ip->Jlin[1] * solidaux[1]->dVlin_nc + ip->Jang[1] * solidaux[1]->dVang_nc;
 		}
 	}
 
-	//LCPを解く
-	/*	u		: 各剛体の速度を並べたベクトル
-		udot	: uの時間微分
-		w		: 接触点での速度
-		J		: uからwを与えるヤコビ行列. w = J u
-		M		: 慣性行列
-		dt		: オイラー積分の時間ステップ
-		λ		: 接触力 * dt
-		Fext	: 外力およびコリオリ項
+	//A行列
+	A.resize(contacts.ncon);
+	A.clear();
+	int K = contacts.size();
+	int irow, icol;
+	for(int icon_r = 0; icon_r < K; icon_r++){
+		PHContact& con_r = contacts[icon_r];
+		int npnt_r = con_r.points.size();
 
-		運動方程式は
-			M udot = J' λ/dt + Fext
-		オイラー積分により
-			u(t+1) = u(t) + M^-1 J' λ + dt M^-1 Fext
-		左からJをかけて
-			w = J M^-1 J' λ + J (u(t) + dt M^-1 Fext)
-		A = J M^-1 J'
-		b = J (u(t) + dt M^-1 Fext)
-		とおくと
-			w = A λ + b
-		さらに，垂直抗力，静止摩擦，動摩擦などを考慮することにより
-		以下の制約条件を得る
-			λ_i = λ_lo_i				 ==> w_i >= 0
-			λ_i = λ_hi_i				 ==> w_i <= 0
-			λ_lo_i < λ_i < λ_hi_i ==> w_i = 0
-		これの解λをLCP(A, b, λ_hi, λ_lo)と書く
+		int lhs_r = contacts[icon_r].solids[0];
+		for(int icon_c = 0; icon_c < K; icon_c++){
+			PHContact& con_c = contacts[icon_c];
+			int npnt_c = con_c.points.size();
 
-		反復解法:
-		A = D - Nとしたとき，適当なλ[0]を初期値として，漸化式
-			λ[k+1] = LCP(D, -N λ[k] + b, λ_hi, λ_lo)
-		によりλ[k]を更新していく．このとき
-			λ[k+1] = λ[k]
-		が成り立つならば
-			λ[k] = LCP(A, b, λ_hi, λ_lo)
-		となり元の問題が解けたことになる
-		さて，Dが対角行列ならば，LCP(D, -N λ[k] + b, λ_hi, λ_lo)の解は
-			λ[k+1] = min(max(λ_lo, D^-1(N λ[k] - b)), λ_hi)
-		で得られる．要するにこの式を繰り返し計算すればよい．
-	 */
-
-	/* 以下は詳細な式展開
-		N : 剛体の数
-		K : 接触の数
-		lhs(i) : i番目の接触の「左側」の剛体の番号
-		rhs(i) : i番目の接触の「右側」の剛体の番号
-			lhs(i) < rhs(i)
-
-		[予備知識]
-		行列の積C = A Bについて
-		C(i,j) = Arow(i)' Bcol(j) = ∑_k=1^n A(i,k) B(k,j)		(n = Aの列数 = Bの行数)
-
-		[各行列の3x3ブロックの内容]
-		ある行列Aの(i,j)番目の3x3部分行列をA(i,j)と書く．
-
-		size(M) = (6N, 6N)
-		M(i,j) =
-			O		(i != j)
-			m_k * 1	(i = 2k)	m_kはk番目の剛体の質量, 1は3x3単位行列
-			I_k		(i = 2k+1)	I_kはk番目の剛体の慣性テンソル
-		
-		size(J) = (3K, 6N)
-		J(i,j) =
-			J_lin(i,lhs(i))		(j == 2lhs(i))
-			J_ang(i,lhs(i))		(j == 2lhs(i)+1)
-			J_lin(i,rhs(i))		(j == 2rhs(i))
-			J_ang(i,rhs(i))		(j == 2rhs(i)+1)
-			O					otherwise
-
-        T := M^-1 J'
-		T(i,j)
-			= ∑_k=1^2N M^-1(i,k) J'(k,j)
-			= ∑_k=1^2N M(i,k)^-1 J(j,k)'
-			= M(i,i)^-1 J(j,i)'
-			=	1/m_lhs(j)  J_lin(j,lhs(j))'	(i == 2lhs(j))
-				I_lhs(j)^-1 J_ang(j,lhs(j))'	(i == 2lhs(j)+1)
-				1/m_rhs(j)  J_lin(j,rhs(j))'	(i == 2rhs(j))
-				I_rhs(j)^-1 J_ang(j,rhs(j))'	(i == 2rhs(j)+1)
-				O							otherwise
-
-		A = J M^-1 J' = J T
-		size(A) = (3K, 3K)
-		A(i,j) = ∑_k=1^2N J(i,k) T(k,j) =
-			δ(lhs(i),lhs(j)) * (1/m_lhs(i) J_lin(i,lhs(i)) J_lin(j,lhs(j))' + J_ang(i,lhs(i)) I_lhs(i)^-1 J_ang(j,lhs(j))') +
-			δ(rhs(i),rhs(j)) * (1/m_rhs(i) J_lin(i,rhs(i)) J_lin(j,rhs(j))' + J_ang(i,rhs(i)) I_rhs(i)^-1 J_ang(j,rhs(j))') +
+			/*δ(lhs(i),lhs(j)) * (1/m_lhs(i) J_lin(i,lhs(i)) J_lin(j,lhs(j))' + J_ang(i,lhs(i)) I_lhs(i)^-1 J_ang(j,lhs(j))') +
 			δ(lhs(i),rhs(j)) * (1/m_lhs(i) J_lin(i,lhs(i)) J_lin(j,rhs(j))' + J_ang(i,lhs(i)) I_lhs(i)^-1 J_ang(j,rhs(j))') +
 			δ(rhs(i),lhs(j)) * (1/m_rhs(i) J_lin(i,rhs(i)) J_lin(j,lhs(j))' + J_ang(i,rhs(i)) I_rhs(i)^-1 J_ang(j,lhs(j))')
-		ただしδはクロネッカーデルタ(δ(i,j) == 1 if i == j or 0 otherwise)
+			δ(rhs(i),rhs(j)) * (1/m_rhs(i) J_lin(i,rhs(i)) J_lin(j,rhs(j))' + J_ang(i,rhs(i)) I_rhs(i)^-1 J_ang(j,rhs(j))') +
+			*/
+			if(con_r.solids[0] == con_c.solids[0]){
+				for(int ipnt_r = 0; ipnt_r < npnt_r; ipnt_r++){
+					for(int ipnt_c = 0; ipnt_c < npnt_c; ipnt_c++){
+						
+					}
+				}
+			}
+			if(con_r.solids[0] == con_c.solids[1]){
+				for(int ipnt_r = 0; ipnt_r < npnt_r; ipnt_r++){
+					for(int ipnt_c = 0; ipnt_c < npnt_c; ipnt_c++){
 
-		b = J Vnc		(Vnc = u(t) + dt M^-1 Fext)
-		size(b) = 3K
-		b(i) = ∑_k=1^2N J(i,k) Vnc(k) =
-			J_lin(i,lhs(i)) Vnc(2lhs(i)) + J_ang(i,lhs(i)) Vnc(2lhs(i)+1) +
-			J_lin(i,rhs(i)) Vnc(2rhs(i)) + J_ang(i,rhs(i)) Vnc(2rhs(i)+1)
-	*/
+					}
+				}
+			}
+			if(con_r.solids[1] == con_c.solids[0]){
+				for(int ipnt_r = 0; ipnt_r < npnt_r; ipnt_r++){
+					for(int ipnt_c = 0; ipnt_c < npnt_c; ipnt_c++){
 
+					}
+				}
+			}
+			if(con_r.solids[1] == con_c.solids[1]){
+				for(int ipnt_r = 0; ipnt_r < npnt_r; ipnt_r++){
+					for(int ipnt_c = 0; ipnt_c < npnt_c; ipnt_c++){
 
-	//precomputation
+					}
+				}
+			}
+
+		}
+	}
 	
-
 	while(true){
 		//terminate condition
 
--- src/Physics/PHConstraintEngine.h
+++ src/Physics/PHConstraintEngine.h
@@ -13,10 +13,20 @@ namespace Spr{;
 class PHConstraintEngine: public PHEngine{
 	OBJECTDEF(PHConstraintEngine);
 
+	/// 接触に関与する剛体の情報
+	struct PHSolidAux{
+		double		minv;				/// 各剛体の質量の逆数
+		Matrix3d	Iinv;				/// 各剛体の慣性行列の逆行列
+		Vec3d		dVlin_nc, dVang_nc;
+	};
+	typedef std::vector<PHSolidAux> PHSolidAuxArray;
+
 	/// 1つの接触点
 	struct PHContactPoint{
-		Vec3d pos;				/// 接触点の位置
-		Matrix3d Jlin[2], Jang[2];	/// LCPを構成するヤコビ行列
+		Vec3d pos;					/// 接触点の位置
+		Matrix3d Jlin[2], Jang[2];	/// J行列のブロック
+		Matrix3d Tlin[2], Tang[2];	/// T行列のブロック
+		Vec3d b;					/// bベクトルのブロック
 
 	};
 	typedef std::vector<PHContactPoint> PHContactPoints;
@@ -38,19 +48,31 @@ class PHConstraintEngine: public PHEngine{
 		　新たな接触が上書きされる．このときbNew = trueとなる
 		・同じ接触が2ステップ以上継続した場合bNew = falseとなる
 		・接触が解消された場合，該当する要素はbValid == falseとなり無効化される
+
+		＊＊上の方式は予定です。やめるかも。＊＊
 	  */
 	class PHContacts : public std::vector<PHContact>{
 	public:
-		bool IsInContact(){}
+		int	ncon;	/// 接触点の総数
+	};
+
+	/// LCPを与える行列
+	class PHLCPMatrix : public std::vector<Matrix3d>{
+		int ncon;	/// 接触の数
+	public:
+		typedef std::vector<Matrix3d> base_type;
+		void resize(size_t K){
+			base_type::resize(K * K);
+			ncon = K;
+		}
+		Matrix3d& item(int i, int j){return (*this)[i * ncon + j];}
 	};
 	
-
 protected:
-	PHSolids	solids;		/// 拘束力計算の対象となる剛体
-	PHContacts	contacts;	/// 剛体同士の接触情報
-
-	std::vector<double>		minv;	/// 各剛体の質量の逆数
-	std::vector<Matrix3d>	Iinv;	/// 各剛体の慣性行列の逆行列
+	PHSolids		solids;		/// 拘束力計算の対象となる剛体
+	PHContacts		contacts;	/// 剛体同士の接触情報
+	PHSolidAuxArray	solidauxs;
+	PHLCPMatrix		A;
 
 	void Init();	/// 初期化
 
