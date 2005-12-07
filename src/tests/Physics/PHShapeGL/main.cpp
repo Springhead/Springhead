2a56a6ebb17cac027593fb6c74af894be21c9cf3
shibuya <shibuya@05cee5c3-a2e9-0310-9523-9dfc2f93dbe1>
2005-12-07 05:47:31 +0000

    git-svn-id: http://springhead.info/spr2/Springhead2/trunk@948 05cee5c3-a2e9-0310-9523-9dfc2f93dbe1

--- src/tests/Physics/PHShapeGL/main.cpp
+++ src/tests/Physics/PHShapeGL/main.cpp
@@ -1,37 +1,298 @@
 /**
  @file	Springhead2/src/tests/Physics/PHShapeGL/main.cpp
 
-@brief 剛体Solidに形状を持たせたテストプログラム（位置を出力、面の頂点座標を出力,GL表示）
+ @brief 剛体Solidに形状を持たせたテストプログラム（位置を出力、面の頂点座標を出力、GL表示）
   
  <PRE>
- <B>テスト内容</B>
- 　・ペナルティ法による凸多面体同士の接触判定と接触力を確認する。
-   ・床の上に2個のブロックを積み上げ、床の上で静止したら正常終了とする。
+ <B>概要：</B>
+  ・ペナルティ法による凸多面体同士の接触判定と接触力を確認する。
+  ・剛体を自然落下させ、床の上に2個のブロックを積み上げる。
+  ・頂点座標をデバッグ出力させ、OpenGLでシミュレーションを行う。
+  
+ <B>終了基準：</B>
+　・自由落下させた剛体が床の上で5秒間静止したら正常終了とする。
+  ・自由落下させた剛体が床の上で静止せず、-500m地点まで落下した場合、異常終了とする。
  
- <B>プログラム概要</B>
+ <B>処理の流れ：</B>
   ・シミュレーションに必要な情報(剛体の形状・質量・慣性テンソルなど)を設定する。
   　剛体の形状はOpenGLで指定するのではなく、Solid自体で持たせる。  
   ・与えられた条件により⊿t秒後の位置の変化を積分し、OpenGLでシミュレーションする。
 　・デバッグ出力として、多面体の面(三角形)の頂点座標を出力する。   
-</PRE>
+ </PRE>
  
  */
- 
-  
 
 #include <Springhead.h>		//	Springheadのインタフェース
+#include <ctime>
+#include <gl/glut.h>
 #pragma hdrstop
 using namespace Spr;
+
+#define ESC				27
+#define RATIO			1.000001	// overflow
+#define FALL_DISTANCE	-500		// 落下距離(異常終了時)
 
 PHSdkIf* sdk;
 PHSceneIf* scene;
 PHSolidIf* solid1, *solid2;
 
+// 光源の設定 
+static GLfloat light_position[] = { 15.0, 30.0, 20.0, 1.0 };
+static GLfloat light_ambient[]  = { 0.0, 0.0, 0.0, 1.0 };
+static GLfloat light_diffuse[]  = { 1.0, 1.0, 1.0, 1.0 }; 
+static GLfloat light_specular[] = { 1.0, 1.0, 1.0, 1.0 };
+// 材質の設定
+static GLfloat mat_red[]        = { 1.0, 0.0, 0.0, 1.0 };
+static GLfloat mat_blue[]       = { 0.0, 0.0, 1.0, 1.0 };
+static GLfloat mat_specular[]   = { 1.0, 1.0, 1.0, 1.0 };
+static GLfloat mat_shininess[]  = { 120.0 };
+
+static clock_t starttime, endtime, count;
+static bool timeflag = false;
+
 /**
- @brief  メイン関数
- @param	 <in/--> argc    コマンドライン入力の個数
- @param  <in/--> argv    コマンドライン入力
- @return  0 (正常終了)
+ @brief     多面体の面(三角形)の法線を求める
+ @param	 	<in/out> normal　　法線
+ @param     <in/-->  base　　　meshの頂点
+ @param     <in/-->  face　　　多面体の面
+ @return 	なし
+ */
+void genFaceNormal(float *normal, Vec3f* base, CDFaceIf* face){
+		normal[0] = base[face->GetIndices()[0]].y * base[face->GetIndices()[1]].z
+						- base[face->GetIndices()[1]].y * base[face->GetIndices()[0]].z
+						+ base[face->GetIndices()[1]].y * base[face->GetIndices()[2]].z
+						- base[face->GetIndices()[2]].y * base[face->GetIndices()[1]].z
+						+ base[face->GetIndices()[2]].y * base[face->GetIndices()[0]].z
+						- base[face->GetIndices()[0]].y * base[face->GetIndices()[2]].z;
+		normal[1] = base[face->GetIndices()[0]].z * base[face->GetIndices()[1]].x
+						- base[face->GetIndices()[1]].z * base[face->GetIndices()[0]].x
+						+ base[face->GetIndices()[1]].z * base[face->GetIndices()[2]].x
+						- base[face->GetIndices()[2]].z * base[face->GetIndices()[1]].x
+						+ base[face->GetIndices()[2]].z * base[face->GetIndices()[0]].x
+						- base[face->GetIndices()[0]].z * base[face->GetIndices()[2]].x;
+		normal[2] = base[face->GetIndices()[0]].x * base[face->GetIndices()[1]].y
+						- base[face->GetIndices()[1]].x * base[face->GetIndices()[0]].y
+						+ base[face->GetIndices()[1]].x * base[face->GetIndices()[2]].y
+						- base[face->GetIndices()[2]].x * base[face->GetIndices()[1]].y
+						+ base[face->GetIndices()[2]].x * base[face->GetIndices()[0]].y
+						- base[face->GetIndices()[0]].x * base[face->GetIndices()[2]].y;
+}
+
+/**
+ @brief     glutDisplayFuncで指定したコールバック関数
+ @param	 	なし
+ @return 	なし
+ */
+void display(){
+	glClear(GL_DEPTH_BUFFER_BIT | GL_COLOR_BUFFER_BIT);
+
+	glMaterialf(GL_FRONT, GL_AMBIENT_AND_DIFFUSE, (1.f,1.f,1.f,1.f));
+	glMaterialfv(GL_FRONT, GL_SPECULAR, mat_specular);
+	glMaterialfv(GL_FRONT, GL_SHININESS, mat_shininess);
+
+	Affined ad;
+	
+	// 下の赤い剛体(solid1)
+	glMaterialfv(GL_FRONT, GL_AMBIENT_AND_DIFFUSE, mat_red);
+	glPushMatrix();
+	Posed pose = solid1->GetPose();
+	pose.ToAffine(ad);
+	glMultMatrixd(ad);	
+
+	float normal[3];
+	for(int i=0; i<solid1->GetNShapes(); ++i){
+		CDShapeIf** shapes = solid1->GetShapes();
+		CDConvexMeshIf* mesh = ICAST(CDConvexMeshIf, shapes[i]);
+		Vec3f* base = mesh->GetVertices();
+		for(size_t f=0; f<mesh->GetNFaces();++f){
+			CDFaceIf* face = mesh->GetFace(f);
+			
+			glBegin(GL_POLYGON);
+			genFaceNormal(normal, base, face);
+			glNormal3fv(normal);
+			for(int v=0; v<face->GetNIndices(); ++v){	
+				glVertex3fv(base[face->GetIndices()[v]].data);
+			}
+			glEnd();
+		}
+	}
+	glPopMatrix();
+
+	
+	// 上の青い剛体(solid2)
+	glMaterialfv(GL_FRONT, GL_AMBIENT_AND_DIFFUSE, mat_blue);
+	glPushMatrix();
+	pose = solid2->GetPose();
+	ad = Affined(pose);
+	glMultMatrixd(ad);
+		for(int i=0; i<solid2->GetNShapes(); ++i){
+		CDShapeIf** shapes = solid2->GetShapes();
+		CDConvexMeshIf* mesh = ICAST(CDConvexMeshIf, shapes[i]);
+		Vec3f* base = mesh->GetVertices();
+		for(size_t f=0; f<mesh->GetNFaces();++f){
+			CDFaceIf* face = mesh->GetFace(f);
+			
+			glBegin(GL_POLYGON);
+			genFaceNormal(normal, base, face);
+			glNormal3fv(normal);	
+			for(int v=0; v<face->GetNIndices(); ++v){	
+				glVertex3fv(base[face->GetIndices()[v]].data);
+			}
+			glEnd();
+		}
+	}
+	glPopMatrix();
+	
+	glutSwapBuffers();
+}
+
+/**
+ @brief     光源の設定
+ @param	 	なし
+ @return 	なし
+ */
+void setLight() {
+	glLightfv(GL_LIGHT0, GL_POSITION, light_position);
+	glLightfv(GL_LIGHT0, GL_AMBIENT, light_ambient);
+	glLightfv(GL_LIGHT0, GL_DIFFUSE, light_diffuse);
+	glLightfv(GL_LIGHT0, GL_SPECULAR, light_specular);
+	glEnable(GL_LIGHTING);
+	glEnable(GL_LIGHT0);
+}
+
+/**
+ @brief     初期化処理
+ @param	 	なし
+ @return 	なし
+ */
+void initialize(){
+	glClearColor(0.0, 0.0, 0.0, 1.0);
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+
+	gluLookAt(0.0, 3.0, 9.0, 
+		      0.0, 0.0, 0.0,
+		 	  0.0, 1.0, 0.0);
+
+	glEnable(GL_DEPTH_TEST);
+	glEnable(GL_NORMALIZE);
+	setLight();
+}
+
+/**
+ @brief　　glutReshapeFuncで指定したコールバック関数
+ @param　　<in/--> w　　幅
+ @param　　<in/--> h　　高さ
+ @return 　なし
+ */
+void reshape(int w, int h){
+	glViewport(0, 0, w, h);
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	gluPerspective(60.0, (GLfloat)w/(GLfloat)h, 1.0, 500.0);
+	glMatrixMode(GL_MODELVIEW);
+}
+
+/**
+ @brief 	glutKeyboardFuncで指定したコールバック関数 
+ @param		<in/--> key　　 ASCIIコード
+ @param 	<in/--> x　　　 キーが押された時のマウス座標
+ @param 	<in/--> y　　　 キーが押された時のマウス座標
+ @return 	なし
+ */
+void keyboard(unsigned char key, int x, int y){
+	if (key == ESC) exit(0);
+}	
+
+/**
+ @brief		オーバーフローに対する処理関数
+ @param		<in/--> d1　　 入力パラメータ
+ @param		<in/--> d2　　 入力パラメータ
+ @return	true　　　　　 2つの入力データが等しいまたはほぼ等しい場合。
+ */
+bool doubleEqual(double d1, double d2){
+	double ratio;
+	if (!d1 && !d2) {
+		return true;
+	} else {
+		ratio = max((double)(d1/d2), (double)(d2/d1));
+		return RATIO > ratio && ratio > 0;
+	}	
+}
+
+/**
+ @brief  	glutIdleFuncで指定したコールバック関数
+ @param	 	なし
+ @return 	なし
+ */
+void idle(){
+	Vec3d prepos, curpos;	// position
+	prepos = solid2->GetFramePosition();
+
+	scene->Step();
+
+	// 位置情報を出力
+	std::cout << solid1->GetFramePosition();
+	std::cout << solid2->GetFramePosition() << std::endl;
+
+	curpos = solid2->GetFramePosition();
+
+	// 床の上に5秒静止したら正常終了とする。
+	if (doubleEqual(prepos.x, curpos.x) && doubleEqual(prepos.y, curpos.y) && doubleEqual(prepos.z, curpos.z)) {
+		if (timeflag == false){
+			starttime = clock();
+			timeflag = true;
+		} else {
+			endtime = clock();
+			count = (endtime - starttime) / (float)CLOCKS_PER_SEC;
+			if (count > 5) {
+				DSTR << "\n正常終了." << std::endl;
+				exit(EXIT_SUCCESS);
+			} 
+		}		
+	}
+	else {
+		// 自由落下させた剛体が床の上で静止せず、-500m地点まで落下した場合、異常終了とする。
+		timeflag = false;
+		if (curpos.y < FALL_DISTANCE) {	
+			DSTR << "\n異常終了." << std::endl;
+			exit(EXIT_FAILURE);
+		}
+	}	
+
+	glutPostRedisplay();
+}
+
+/**
+ @brief 	多面体の面(三角形)の頂点座標をデバッグ出力させる。
+ @param 	<in/--> solidID　　solidのID
+ @return 	なし
+ */
+void dstrSolid(int solidID) {
+	PHSolidIf* solid;
+	DSTR << "***  solid" << solidID << "   ***\n";
+	if (solidID == 1)		solid = solid1;
+	else if (solidID == 2)	solid = solid2;
+
+	for(int i=0; i<solid->GetNShapes(); ++i){
+		CDShapeIf** shapes = solid->GetShapes();
+		CDConvexMeshIf* mesh = ICAST(CDConvexMeshIf, shapes[i]);
+		Vec3f* base = mesh->GetVertices();
+		for(size_t f=0; f<mesh->GetNFaces();++f){
+			CDFaceIf* face = mesh->GetFace(f);
+			for(int v=0; v<face->GetNIndices(); ++v){
+				DSTR << base[face->GetIndices()[v]];
+			}
+			DSTR << std::endl;
+		}
+	}
+}
+
+/**
+ @brief		メイン関数
+ @param		<in/--> argc　　コマンドライン入力の個数
+ @param		<in/--> argv　　コマンドライン入力
+ @return	0 (正常終了)
  */
 int main(int argc, char* argv[]){
 	sdk = CreatePHSdk();				//	SDKの作成　
@@ -49,63 +310,46 @@ int main(int argc, char* argv[]){
 	//	形状の作成
 	CDConvexMeshDesc md;
 	md.vertices.push_back(Vec3f(-1,-1,-1));
-	md.vertices.push_back(Vec3f(-1,-1, 1));
-	md.vertices.push_back(Vec3f(-1, 1,-1));
+	md.vertices.push_back(Vec3f(-1,-1, 1));	
+	md.vertices.push_back(Vec3f(-1, 1,-1));	
 	md.vertices.push_back(Vec3f(-1, 1, 1));
-	md.vertices.push_back(Vec3f( 1,-1,-1));
+	md.vertices.push_back(Vec3f( 1,-1,-1));	
 	md.vertices.push_back(Vec3f( 1,-1, 1));
 	md.vertices.push_back(Vec3f( 1, 1,-1));
 	md.vertices.push_back(Vec3f( 1, 1, 1));
 	CDConvexMeshIf* mesh2 = ICAST(CDConvexMeshIf, scene->CreateShape(md));
 
+	// solid1(mesh1)に対してスケーリング
 	for(unsigned i=0; i<md.vertices.size(); ++i){
-		md.vertices[i].x *= 10;
+		md.vertices[i].x *= 1.5;
+		md.vertices[i].z *= 1.5;
 	}
 	CDConvexMeshIf* mesh1 = ICAST(CDConvexMeshIf, scene->CreateShape(md));
 
 	solid1->AddShape(mesh1);
 	solid2->AddShape(mesh2);
 	solid1->SetFramePosition(Vec3f(0,-1,0));
-	solid2->SetFramePosition(Vec3f(0,2,0));
+	solid2->SetFramePosition(Vec3f(0,3,0));	
 
 	scene->SetGravity(Vec3f(0,-9.8f, 0));	// 重力を設定
 
-	for(int i=0; i<2; ++i){
-		scene->Step();
-		std::cout << solid1->GetFramePosition();
-		std::cout << solid2->GetFramePosition() << std::endl;
-		// std::cout << solid1->GetOrientation() << std::endl;
-	}
-	
 	// デバッグ出力
-	DSTR << "***  solid1  ***\n";
-	for(int i=0; i<solid1->GetNShapes();++i){
-		CDShapeIf** shapes = solid1->GetShapes();
-		CDConvexMeshIf* mesh = ICAST(CDConvexMeshIf, shapes[i]);
-		Vec3f* base = mesh->GetVertices();
-		for(size_t f=0; f<mesh->GetNFaces();++f){
-			CDFaceIf* face = mesh->GetFace(f);
-			for(int v=0; v<face->GetNIndices(); ++v){
-				DSTR << base[face->GetIndices()[v]];
-			}
-			DSTR << std::endl;
-		}
-	}
-	DSTR << "***  solid2  ***\n";
-	for(int i=0; i<solid2->GetNShapes();++i){
-		CDShapeIf** shapes = solid2->GetShapes();
-		CDConvexMeshIf* mesh = ICAST(CDConvexMeshIf, shapes[i]);
-		Vec3f* base = mesh->GetVertices();
-		for(size_t f=0; f<mesh->GetNFaces();++f){
-			CDFaceIf* face = mesh->GetFace(f);
-			for(int v=0; v<face->GetNIndices(); ++v){
-				DSTR << base[face->GetIndices()[v]];
-			}
-			DSTR << std::endl;
-		}
-	}
+	dstrSolid(1);
+	dstrSolid(2);
+
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA | GLUT_DEPTH);
+	glutCreateWindow("PHShapeGL");
+	initialize();
+	glutDisplayFunc(display);
+	glutReshapeFunc(reshape);
+	glutKeyboardFunc(keyboard);
+	glutIdleFunc(idle);
+
+	glutMainLoop();
 
 	//	SDKは開放しなくても良い．しなくてもmainを抜けてから開放される．
 	delete sdk;
-
 }
+
+
