a6c1383902dd885085490c75f8308eb3d8932028
shibuya <shibuya@05cee5c3-a2e9-0310-9523-9dfc2f93dbe1>
2005-12-15 05:49:29 +0000

    fasb(x/y)をfabs(x/y-1.0)に修正
    git-svn-id: http://springhead.info/spr2/Springhead2/trunk@997 05cee5c3-a2e9-0310-9523-9dfc2f93dbe1

--- include/Physics/SprPHDef.h
+++ include/Physics/SprPHDef.h
@@ -3,14 +3,14 @@
 
 namespace Spr {;
 
-inline bool approx(double const x, double const y){
+inline bool approx(const double x, const double y){
 	const double eps = 1e-6;
 	return ((x==y)
 			|| (fabs(x-y) < eps)
-			|| (fabs(x/y) < eps));
+			|| (fabs(x/y - 1.0) < eps));
 }
 
-inline bool approx(Vec3d v1, Vec3d v2){
+inline bool approx(const Vec3d v1, const Vec3d v2){
 	return (approx(v1.x, v2.x) && approx(v1.y, v2.y) && approx(v1.z, v2.z));
 }
 
