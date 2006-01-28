1f8272208ec3d5cd1c399eccb75be882d809418d
hase <hase@05cee5c3-a2e9-0310-9523-9dfc2f93dbe1>
2006-01-28 18:51:50 +0000

    git-svn-id: http://springhead.info/spr2/Springhead2/trunk@1171 05cee5c3-a2e9-0310-9523-9dfc2f93dbe1

--- src/FileIO/MakeTypeDesc.bat
+++ src/FileIO/MakeTypeDesc.bat
@@ -1,3 +1,7 @@
+rem include/*/*.h から， struct XXXDesc と struct XXXState の定義を抜き出して，
+rem FIDesc.cppに文字列定数の定義として書き出す．
+rem bin/typedesc.exe が抜き出しを行う．
+
 set PATH=%SPRINGHEAD2%\bin
 del FIDesc.cpp
 echo //	Don't edit. MakeTypeDesc.bat update this file.> FIDesc.cpp
