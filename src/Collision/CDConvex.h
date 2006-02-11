5e191c034bbfad9dba1f2af6ec3c78d35133eace
tazaki <tazaki@05cee5c3-a2e9-0310-9523-9dfc2f93dbe1>
2006-02-11 15:13:46 +0000

    git-svn-id: http://springhead.info/spr2/Springhead2/trunk@1219 05cee5c3-a2e9-0310-9523-9dfc2f93dbe1

--- src/Collision/CDConvex.h
+++ src/Collision/CDConvex.h
@@ -9,7 +9,7 @@ namespace Spr{;
 class CDConvex : public CDShape{
 public:
 	OBJECTDEFABST(CDConvex);
-	///	サポートポイントを求める．
+	///	サポートポイント(方向ベクトルpとの内積が最大の頂点)を求める．
 	virtual Vec3f Support(const Vec3f& p) const =0;
 	virtual void CalcBBox(Vec3f& bbmin, Vec3f& bbmax);
 };
