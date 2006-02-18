dd0c33769d7c0c099c58c4f1536ebd9a00fb931d
m_oouchi <m_oouchi@05cee5c3-a2e9-0310-9523-9dfc2f93dbe1>
2006-02-18 11:13:35 +0000

    FileIOのテストが通らなくなっていたため調べてるうちに発見。 原因はTPoseのコメントアウトにより変数名が無効になっていたため。 Union内で定義されていた w,x,y,z,px,py,pzに置き換えてみたところ解決した模様。
    git-svn-id: http://springhead.info/spr2/Springhead2/trunk@1252 05cee5c3-a2e9-0310-9523-9dfc2f93dbe1

--- src/FileIO/FIBaseType.h
+++ src/FileIO/FIBaseType.h
@@ -7,8 +7,10 @@ struct Vec4f{ float x; float y; float z; float w;};
 struct Vec4d{ double x; double y; double z; double w;};
 struct Quaternionf{ float x; float y; float z; float w;};
 struct Quaterniond{ double x; double y; double z; double w;};
-struct Posef{ Quaternionf ori; Vec3f pos; }
-struct Posed{ Quaterniond ori; Vec3d pos; }
+//struct Posef{ Quaternionf Ori(); Vec3f Pos(); }
+//struct Posed{ Quaterniond Ori(); Vec3d Pos(); }
+struct Posef{ float w; float x; float y; float z; float px; float py; float pz;}
+struct Posed{ double w; double x; double y; double z; double px; double py; double pz;}
 struct Matrix3f{ float data[9]; }
 struct Matrix3d{ double data[9]; }
 struct Affinef{ float data[16]; }
