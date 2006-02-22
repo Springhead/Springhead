fb80589a07323c4f8839b4e51fe53b25aa253299
shibuya <shibuya@05cee5c3-a2e9-0310-9523-9dfc2f93dbe1>
2006-02-22 12:38:19 +0000

    Physics.h 追加
    git-svn-id: http://springhead.info/spr2/Springhead2/trunk@1308 05cee5c3-a2e9-0310-9523-9dfc2f93dbe1

---
+++ src/Physics/Physics.h
@@ -0,0 +1,9 @@
+#include <Springhead.h>
+#include <Foundation/Object.h>
+#include <Collision/Collision.h>
+#include "PHSolid.h"
+#include "PHSdk.h"
+#include "PHScene.h"
+#include "PHForceField.h"
+#include "PHPenaltyEngine.h"
+#include "PHConstraintEngine.h"
