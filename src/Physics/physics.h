30d0b88ef538b2420875012660c2cb514c44484e
shibuya <shibuya@05cee5c3-a2e9-0310-9523-9dfc2f93dbe1>
2006-02-22 12:37:05 +0000

    ファイル名がPhysics.hではなく、physics.hで登録されているので一旦削除
    git-svn-id: http://springhead.info/spr2/Springhead2/trunk@1307 05cee5c3-a2e9-0310-9523-9dfc2f93dbe1

--- src/Physics/physics.h
+++
@@ -1,9 +0,0 @@
-#include <Springhead.h>
-#include <Foundation/Object.h>
-#include <Collision/Collision.h>
-#include "PHSolid.h"
-#include "PHSdk.h"
-#include "PHScene.h"
-#include "PHForceField.h"
-#include "PHPenaltyEngine.h"
-#include "PHConstraintEngine.h"
