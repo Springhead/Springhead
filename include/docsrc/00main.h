2653faadfb5af2fa26d00ac622bf1805064b4d71
hase <hase@05cee5c3-a2e9-0310-9523-9dfc2f93dbe1>
2006-04-03 04:05:52 +0000

    git-svn-id: http://springhead.info/spr2/Springhead2/trunk@1454 05cee5c3-a2e9-0310-9523-9dfc2f93dbe1

--- include/docsrc/00main.h
+++ include/docsrc/00main.h
@@ -1,16 +1,16 @@
 /** 
 \mainpage Springhead2マニュアル
  このマニュアルは，Springhead2 SDKの使い方を示すもので，
- - \subpage page_users_guide
- - \subpage page_tutorial
- - \subpage page_reference_manual
+ - \subpage pageUsersGuide
+ - \subpage pageTutorial
+ - \subpage pageReferenceManual
  から構成されます．
 
- \ref page_users_guide は，SDKのインストール・ビルドから，
+ \ref pageUsersGuide は，SDKのインストール・ビルドから，
  SDKの設計，動作，使い方の概要を示します．
  
- \ref page_tutorial は，簡単な例題プログラムを作成しながら，
+ \ref pageTutorial は，簡単な例題プログラムを作成しながら，
  SDKの使い方を学ぶためのチュートリアルです．
  
- \ref page_reference_manual は，SDKのAPIと基本的なクラスのリファレンスです．
+ \ref pageReferenceManual は，SDKのAPIと基本的なクラスのリファレンスです．
 */
--- include/docsrc/01UsersGuide/00index.h
+++ include/docsrc/01UsersGuide/00index.h
@@ -1,26 +1,26 @@
 /** \page page_users_guide	【ユーザーズガイド】
 
 \ref page_users_guide は，
- - \subpage page_introduction
- - \subpage page_getting_started
- - \subpage page_concept
- - \subpage page_api_basic
- - \subpage page_structure
+ - \subpage pageIntroduction
+ - \subpage pageGettingStarted
+ - \subpage pageConcept
+ - \subpage pageApiBasic
+ - \subpage pageLibStructure
 から構成されます．
 
-\ref page_introduction
+\ref pageIntroduction
 では，Springhead2の特徴を簡単に紹介すると共に，他の物理エンジンSDKの紹介と比較をします．
 
-\ref page_getting_started
+\ref pageGettingStarted
 では，インストールからサンプルのビルドまでを説明し，SDKが使える環境を作ります．
 
-\ref page_concept
+\ref pageConcept
 では，SDK/APIの設計，物理エンジンなどの構造，動作の概念を説明します．
 
-\ref page_api_basic
+\ref pageApiBasic
 では，SDKのAPIの基礎(APIの仕組み，共通する使い方など)を説明します．
 
-\ref page_structure
+\ref pageLibStructure
 では，SDKのAPIの詳細を説明します．
 
 */
