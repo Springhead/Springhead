8353d5ae61235540460ab1d86ab679d96cba8dce
hase <hase@05cee5c3-a2e9-0310-9523-9dfc2f93dbe1>
2006-04-08 18:21:43 +0000

    git-svn-id: http://springhead.info/spr2/Springhead2/trunk@1517 05cee5c3-a2e9-0310-9523-9dfc2f93dbe1

---
+++ include/FileIO/SprFINodeHandler.h
@@ -0,0 +1,17 @@
+#ifndef SPR_FINODEHANDLER_H
+#define SPR_FINODEHANDLER_H
+#include <Foundation/SprObject.h>
+
+namespace Spr {;
+/** \addtogroup gpFileIO	*/
+//@{
+///	剛体のステート
+
+struct FIInactiveSolids{
+	std::vector<std::string> solids;
+};
+
+}	//	namespace Spr
+
+#endif	
+
---
+++ include/SprFileIO.h
@@ -0,0 +1,10 @@
+/**
+ *	@file SprFileIO.h
+ *	@brief ファイル入出力のヘッダファイル群
+*/
+#ifndef SPR_FILEIO_H
+#define SPR_FILEIO_H
+
+#include "FileIO/SprFINodeHandler.h"
+
+#endif
