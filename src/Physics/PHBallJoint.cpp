75c5a7a008efafe84b7a5aadeb5c6f22fd2276ad
tazaki <tazaki@05cee5c3-a2e9-0310-9523-9dfc2f93dbe1>
2006-04-08 17:22:44 +0000

    git-svn-id: http://springhead.info/spr2/Springhead2/trunk@1512 05cee5c3-a2e9-0310-9523-9dfc2f93dbe1

--- src/Physics/PHBallJoint.cpp
+++ src/Physics/PHBallJoint.cpp
@@ -12,4 +12,57 @@ namespace Spr{;
 //OBJECTIMP(PHBallJoint, PHJoint1D)
 IF_IMP(PHBallJoint, PHJoint1D)
 
+void PHBallJoint::SetDesc(const PHConstraintDesc& desc){
+	const PHBallJointDesc& descBall = (const PHBallJointDesc&)desc;
+	max_angle = descBall.max_angle;
+	torque = descBall.torque;
+}
+
+Quaterniond PHBallJoint::GetPosition(){
+	return qjrel;
+}
+
+Vec3d PHBallJoint::GetVelocity(){
+	return wjrel;
+}
+
+void PHBallJoint::CompConstraintJacobian(){
+	dim_d = 3;
+	dim_c = 3;
+	Ad.clear();
+	Ac.clear();
+	for(int i = 0; i < 2; i++){
+		Jdv[i].SUBMAT(0, 0, 3, 3) = Jvv[i];
+		Jdv[i].SUBMAT(3, 0, 3, 3) = Jwv[i];
+		Jdw[i].SUBMAT(0, 0, 3, 3) = Jvw[i];
+		Jdw[i].SUBMAT(3, 0, 3, 3) = Jww[i];
+		Jcv[i].SUBMAT(0, 0, 3, 3) = Jvv[i];
+		Jcw[i].SUBMAT(0, 0, 3, 3) = Jvw[i];
+		if(solid[i]->solid->IsDynamical()){
+			Tdv[i] = Jdv[i] * solid[i]->minv;
+			Tdw[i] = Jdw[i] * solid[i]->Iinv;
+			solid[i]->dv += Tdv[i].SUBMAT(3, 0, 3, 3).trans() * torque;
+			solid[i]->dw += Tdw[i].SUBMAT(3, 0, 3, 3).trans() * torque;
+
+			Tcv[i].SUBMAT(0, 0, 3, 3) = Tdv[i].SUBMAT(0, 0, 3, 3);
+			Tcw[i].SUBMAT(0, 0, 3, 3) = Tdw[i].SUBMAT(0, 0, 3, 3);
+			for(int j = 0; j < 6; j++)
+				Ad[j] += Jdv[i].row(j) * Tdv[i].row(j) + Jdw[i].row(j) * Tdw[i].row(j);
+			Ac.SUBVEC(0, 3) += Ad.SUBVEC(0, 3);
+		}
+	}
+}
+
+void PHBallJoint::CompBias(double dt){
+	
+}
+
+void PHBallJoint::ProjectionDynamics(double& f, int k){
+	
+}
+
+void PHBallJoint::ProjectionCorrection(double& F, int k){
+	
+}
+
 }
--- src/Physics/PHSliderJoint.cpp
+++ src/Physics/PHSliderJoint.cpp
@@ -12,4 +12,84 @@ namespace Spr{;
 //OBJECTIMP(PHSliderJoint, PHJoint1D)
 IF_IMP(PHSliderJoint, PHJoint1D)
 
+double PHSliderJoint::GetPosition(){
+	return rjrel[2];
 }
+
+double PHSliderJoint::GetVelocity(){
+	return vjrel[2];
+}
+
+void PHSliderJoint::CompConstraintJacobian(){
+	CompDof();
+	Ad.clear();
+	Ac.clear();
+	for(int i = 0; i < 2; i++){
+		Jdv[i].SUBMAT(0, 0, 2, 3) = Jvv[i].SUBMAT(0, 0, 2, 3);
+		Jdv[i].SUBMAT(2, 0, 3, 3) = Jwv[i];
+		Jdv[i].row(5) = Jvv[i].row(2);
+		Jdw[i].SUBMAT(0, 0, 2, 3) = Jvw[i].SUBMAT(0, 0, 2, 3);
+		Jdw[i].SUBMAT(2, 0, 3, 3) = Jww[i];
+		Jdw[i].row(5) = Jww[i].row(2);
+
+		Jcv[i].SUBMAT(0, 0, 2, 3) = Jvv[i].SUBMAT(0, 0, 2, 3);
+		Jcv[i].SUBMAT(2, 0, 3, 3) = Jqv[i];
+		Jcv[i].row(5) = Jvv[i].row(2);
+		Jcw[i].SUBMAT(0, 0, 2, 3) = Jvw[i].SUBMAT(0, 0, 2, 3);
+		Jcw[i].SUBMAT(2, 0, 3, 3) = Jqw[i];
+		Jcw[i].row(5) = Jqw[i].row(2);
+
+		if(solid[i]->solid->IsDynamical()){
+			Tdv[i] = Jdv[i] * solid[i]->minv;
+			Tdw[i] = Jdw[i] * solid[i]->Iinv;
+			solid[i]->dv += Tdv[i].row(5) * torque;
+			solid[i]->dw += Tdw[i].row(5) * torque;
+
+			Tcv[i].SUBMAT(0, 0, 2, 3) = Tdv[i].SUBMAT(0, 0, 2, 3);
+			Tcv[i].SUBMAT(2, 0, 3, 3) = Jqv[i] * solid[i]->minv;
+			Tcv[i].row(5) = Tdv[i].row(2);
+			Tcw[i].SUBMAT(0, 0, 2, 3) = Tdw[i].SUBMAT(0, 0, 2, 3);
+			Tcw[i].SUBMAT(2, 0, 3, 3) = Jqw[i] * solid[i]->Iinv;
+			Tcw[i].row(5) = Tdw[i].row(2);
+			for(int j = 0; j < 6; j++)
+				Ad[j] += Jdv[i].row(j) * Tdv[i].row(j) + Jdw[i].row(j) * Tdw[i].row(j);
+			Ac.SUBVEC(0, 2) += Ad.SUBVEC(0, 2);
+			for(int j = 2; j < 5; j++)
+				Ac[j] += Jcv[i].row(j) * Tcv[i].row(j) + Jcw[i].row(j) * Tcw[i].row(j);
+			Ac[5] += Ad[5];
+		}
+	}
+}
+
+void PHSliderJoint::CompBias(double dt){
+	if(mode == MODE_VELOCITY){
+		b[5] -= vel_d;
+	}
+	else if(spring != 0.0 || damper != 0.0){
+		double diff = GetPosition() - origin;
+		double tmp = 1.0 / (damper + spring * dt);
+		Ad[5] += tmp / dt;
+		b[5] += spring * (diff) * tmp;
+	}
+}
+
+void PHSliderJoint::ProjectionDynamics(double& f, int k){
+	if(k == 5){
+		if(on_lower)
+			f = max(0.0, f);
+		if(on_upper)
+			f = min(0.0, f);
+	}
+}
+
+void PHSliderJoint::ProjectionCorrection(double& F, int k){
+	if(k == 5){
+		if(on_lower)
+			F = max(0.0, F);
+		if(on_upper)
+			F = min(0.0, F);
+	}
+}
+
+}
+
