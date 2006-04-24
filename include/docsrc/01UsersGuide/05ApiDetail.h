70e5748516f10f047684b819491925d088c95a60
tazaki <tazaki@05cee5c3-a2e9-0310-9523-9dfc2f93dbe1>
2006-04-24 08:02:02 +0000

    git-svn-id: http://springhead.info/spr2/Springhead2/trunk@1618 05cee5c3-a2e9-0310-9523-9dfc2f93dbe1

--- include/docsrc/01UsersGuide/05ApiDetail.h
+++ include/docsrc/01UsersGuide/05ApiDetail.h
@@ -3,7 +3,7 @@
 
 ここでは，Springhead2 SDKのAPIの機能，構成など，APIの内容について説明します．
 
-\subsection physics		物理シミュレーションライブラリ
-\subsection graphics	グラフィックスライブラリ
-\subsection fileio		ファイルIOライブラリ
+\subpage pagePhysics 物理シミュレーションライブラリ
+\section graphics	グラフィックスライブラリ
+\section fileio		ファイルIOライブラリ
 */
