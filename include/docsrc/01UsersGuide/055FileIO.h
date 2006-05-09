c50677d32df903caca4efe2290be3072fc192823
hase <hase@05cee5c3-a2e9-0310-9523-9dfc2f93dbe1>
2006-05-09 10:06:47 +0000

    git-svn-id: http://springhead.info/spr2/Springhead2/trunk@1641 05cee5c3-a2e9-0310-9523-9dfc2f93dbe1

---
+++ include/docsrc/01UsersGuide/055FileIO.h
@@ -0,0 +1,16 @@
+/** \page pageFileIO ファイル入出力SDK
+
+- \ref sec_FISdk
+
+\section sec_FISdk ファイル入出力SDKとロードのイメージ
+TBW
+*/
+1つのグラフィックスSDK(GRSdk)は，いくつかのシーン(GRScene)を持つことができ，
+シーンには座標系(Frame)のツリーの中に，メッシュ(GRMesh)，光源(GRLight)，材質(GRMaterial)
+などがあります．
+グラフィックスSDK内のオブジェクトの参照関係の例を図に示します．
+<img src="../../include/docsrc/01UsersGuide/GRSdkScene.png">
+/*
+
+
+*/
