78fbc6447a9f6dd2c921f18684977ecf06c14e61
shibuya <shibuya@05cee5c3-a2e9-0310-9523-9dfc2f93dbe1>
2006-05-23 07:07:08 +0000

    GRColorSpheresテストの修正
    git-svn-id: http://springhead.info/spr2/Springhead2/trunk@1683 05cee5c3-a2e9-0310-9523-9dfc2f93dbe1

--- src/tests/Graphics/GRColorSpheres/main.cpp
+++ src/tests/Graphics/GRColorSpheres/main.cpp
@@ -6,7 +6,7 @@
   DrawScene API で設定されているマテリアルマテリアルサンプルを用いて、カラフルなボックスをレンダリングする。　
   
 【終了基準】
-  ・5000ステップ後に強制終了。
+  ・5000ステップ後に強制終了。 
 
  */
 #include <Springhead.h>		//	Springheadのインタフェース
@@ -17,11 +17,11 @@
 #pragma hdrstop
 #endif
 using namespace Spr;
-#define ESC				27
-#define EXIT_TIMER		5000
-#define WINSIZE_WIDTH	480
-#define WINSIZE_HEIGHT	360
-#define NUM_BLOCKS		30
+#define ESC				27			// ESC key
+#define EXIT_TIMER		5000		// 実行ステップ数
+#define WINSIZE_WIDTH	480			// ウィンドウサイズ(width)
+#define WINSIZE_HEIGHT	360			// ウィンドウサイズ(height)
+#define NUM_SPHERES		10			// sphere数
 
 GRSdkIf* grSdk;
 GRDebugRenderIf* render;
@@ -30,24 +30,7 @@ GRDeviceGLIf* grDevice;
 PHSdkIf* phSdk;
 PHSceneIf* scene;
 PHSolidIf* soFloor;
-std::vector<PHSolidIf*> soBlock;
-
-// 材質の設定
-GRMaterialDesc matFloor(Vec4f(0.2, 0.2, 0.2, 1.0),		// ambient
-					Vec4f(0.6, 0.6, 0.6, 1.0),		// diffuse
-					Vec4f(0.2, 0.2, 0.2, 1.0),		// specular
-					Vec4f(0.0, 0.0, 0.0, 1.0),		// emissive
-					10.0);							// power
-GRMaterialDesc matBlock(Vec4f(0.5, 0.5, 0.7, 0.9),		
-					Vec4f(0.1, 0.5, 0.8, 0.9),		
-					Vec4f(1.0, 1.0, 1.0, 0.9),			
-					Vec4f(0.0, 0.0, 0.0, 1.0),		
-					10.0);
-GRMaterialDesc matLine(Vec4f(1.0, 1.0, 1.0, 1.0),		
-					Vec4f(1.0, 1.0, 1.0, 1.0),		
-					Vec4f(1.0, 1.0, 1.0, 1.0),		
-					Vec4f(0.0, 0.0, 0.0, 1.0),		
-					100.0);	
+std::vector<PHSolidIf*> soSphere; 
 
 
 /**
@@ -92,7 +75,6 @@ void reshape(int w, int h){
  */
 void keyboard(unsigned char key, int x, int y){
 	if (key == ESC) exit(0);
-
 }	
 /**
  brief  	glutIdleFuncで指定したコールバック関数
@@ -122,46 +104,35 @@ int main(int argc, char* argv[]){
 	desc.mass = 2.0;
 	desc.inertia *= 2.0;
 
-	unsigned int blockCnt;
-	for (blockCnt=0; blockCnt<NUM_BLOCKS; ++blockCnt){
-		soBlock.push_back(scene->CreateSolid(desc));		// 剛体をdescに基づいて作成
+	// Solidの作成
+	unsigned int sphereCnt;
+	for (sphereCnt=0; sphereCnt<NUM_SPHERES; ++sphereCnt){
+		soSphere.push_back(scene->CreateSolid(desc));		// 剛体をdescに基づいて作成
 	}
 
 	desc.mass = 1e20f;
 	desc.inertia *= 1e20f;
 	soFloor = scene->CreateSolid(desc);		// 剛体をdescに基づいて作成
 	soFloor->SetGravity(false);
-	
+
 	//	形状の作成
-	CDConvexMeshIf* meshFloor=NULL;
-	CDConvexMeshIf* meshBlock=NULL;
+	CDBoxIf* floor=NULL;
+	CDSphereIf* sphere=NULL;
 	{
-		CDConvexMeshDesc md;
-		md.vertices.push_back(Vec3f(-1,-1,-1));
-		md.vertices.push_back(Vec3f(-1,-1, 1));	
-		md.vertices.push_back(Vec3f(-1, 1,-1));	
-		md.vertices.push_back(Vec3f(-1, 1, 1));
-		md.vertices.push_back(Vec3f( 1,-1,-1));	
-		md.vertices.push_back(Vec3f( 1,-1, 1));
-		md.vertices.push_back(Vec3f( 1, 1,-1));
-		md.vertices.push_back(Vec3f( 1, 1, 1));
-		meshBlock = ICAST(CDConvexMeshIf, phSdk->CreateShape(md));
+		CDSphereDesc sd;
+		sphere = ICAST(CDSphereIf, phSdk->CreateShape(sd));
 
-		// soFloor(meshFloor)に対してスケーリング
-		for(unsigned i=0; i<md.vertices.size(); ++i){
-			md.vertices[i].x *= 30;
-			md.vertices[i].z *= 30;
-		}
-		meshFloor = ICAST(CDConvexMeshIf, phSdk->CreateShape(md));
-	}
-	
-	soFloor->AddShape(meshFloor);
+		CDBoxDesc bd;
+		bd.boxsize = Vec3f (30.0, 1.0, 30.0);
+		floor = ICAST(CDBoxIf, phSdk->CreateShape(bd));
+	}	
+
+	soFloor->AddShape(floor);
 	soFloor->SetFramePosition(Vec3f(0,-3,0));
-	for (blockCnt=0; blockCnt<NUM_BLOCKS; ++blockCnt){
-		soBlock[blockCnt]->AddShape(meshBlock);
-		soBlock[blockCnt]->SetFramePosition(Vec3f(3, 15*(blockCnt+1), 3));
+	for (sphereCnt=0; sphereCnt<NUM_SPHERES; ++sphereCnt){
+		soSphere[sphereCnt]->AddShape(sphere);
+		soSphere[sphereCnt]->SetFramePosition(Vec3f(0, 15+5*sphereCnt, 0));
 	}
-
 	scene->SetGravity(Vec3f(0,-9.8f, 0));	// 重力を設定
 
 	glutInit(&argc, argv);
@@ -184,7 +155,7 @@ int main(int argc, char* argv[]){
 
 	// 視点を設定する
 	Affinef view;
-	view.Pos() = Vec3f(5.0, 15.0, 15.0);								// eye
+	view.Pos() = Vec3f(0.0, 15.0, 15.0);								// eye
 	view.LookAtGL(Vec3f(0.0, 0.0, 0.0), Vec3f(0.0, 1.0, 0.0));			// center, up 
 	view = view.inv();	
 	render->SetViewMatrix(view);
