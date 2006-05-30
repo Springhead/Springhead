bf855baa26eaee9020f2b4da20f5f915b779d6e3
shibuya <shibuya@05cee5c3-a2e9-0310-9523-9dfc2f93dbe1>
2006-05-30 12:50:11 +0000

    ユーザーズガイド＆チュートリアル 更新
    git-svn-id: http://springhead.info/spr2/Springhead2/trunk@1699 05cee5c3-a2e9-0310-9523-9dfc2f93dbe1

--- include/docsrc/01UsersGuide/03Concept.h
+++ include/docsrc/01UsersGuide/03Concept.h
@@ -1,9 +1,11 @@
 /** \page pageConcept	Springheadの構成
 
 Springheadは大きく分けて以下の3つのモジュールから構成されています．
-- Springhead Physics \ref pagePhysics
-- Springhead Graphics \ref pageGraphics
-- Springhead File I/O \ref pageFileIO
+- Ｓｐｒｉｎｇｈｅａｄ　Ｐｈｙｓｉｃｓ　　⇒　 \ref pagePhysics
+- Ｓｐｒｉｎｇｈｅａｄ　Ｇｒａｐｈｉｃｓ　⇒　 \ref pageGraphics
+- Ｓｐｒｉｎｇｈｅａｄ　Ｆｉｌｅ　Ｉ／Ｏ　⇒　 \ref pageFileIO
+
+<br>
 
 これらのモジュールは，個別に用いることもできるように設計されています．
 例えば，ユーザの中にはレンダリングは手持ちのライブラリを使い，物理シミュレーションのみを
@@ -11,31 +13,44 @@ Springheadで行いたいと考えている人もいるでしょう．このような場合にも，必要最低限
 コードだけでユーザのライブラリとSpringheadとを連動させることができます．
 
 以下では個別のモジュールについて順に解説します．
+<br><br><br>
 
-\section sec_physics 物理シミュレーションSDK
+
+\section sec_physics 【 物理シミュレーションSDK 】
+\par
 \ref pagePhysics は，物理シミュレータ本体です．接触判定や物体運動のシミュレーションを行います．
 物理シミュレーションのシーン(世界)には，物体の質量や慣性モーメント，関節の定義，摩擦係数や跳ね返り係数，バネダンパなどがあります．
 物体の色や光源など，物体の見た目についてはGraphcisで扱います．Physicsにはありません．
+<br><br><br>
 
-\section sec_graphics グラフィックスSDK
+
+\section sec_graphics 【 グラフィックスSDK 】
+\par
 \ref pageGraphics は，グラフィックスレンダラです．
 Physicsにはまったくシーンを描画する機能が無いため，シーンの様子を目で見ることができません．
 Graphcisは，Physicsのシーンを描画して，様子を見るための，Debugのレンダリング機能を持ちます．
 
+\par
 また，Graphcisは，映像専用のシーンを用意して美しい映像提示を行うこともできます．
 毎ステップ，Physicsで計算した物体運動を映像専用のシーンに反映させてから描画することになります．
 
+\par
 Graphicsを使わずに，Physicsの計算結果を自分の描画プログラムから参照して，
 独自の描画プログラムを作ることもできます．
 また，他のグラフィックスやシーングラフのライブラリを使って描画し，Physicsの計算結果を
 ライブラリ伝えるプログラムだけを自分で用意する手もあります．
+<br><br><br>
 
-\section sec_fileio ファイル入出力SDK
+
+\section sec_fileio 【 ファイル入出力SDK 】
+\par
 \ref pageFileIO は，ファイルローダとセーバです．シーングラフのロードとセーブを行います．
 FileIOを使うと，シーングラフ(＝オブジェクトのツリー)をデータファイルからロードしたり，
 データファイルにセーブしたりできます．
 FileIOは他のモジュールのAPIを利用してロードしたデータからオブジェクトを生成したり，
 他のモジュールのAPIを利用してシーングラフからデータを取り出して，ファイルに保存したりしています．
 
+\par
 FileIOを使わずに，自分で1からデータファイルをロードする仕組みを作ることもできます．
+<br><br><br>
 */
--- include/docsrc/02Tutorial/08MoreTutoFISdk.h
+++ include/docsrc/02Tutorial/08MoreTutoFISdk.h
@@ -1,10 +1,13 @@
 /** \page pageMoreTutoFISdk	ファイル入出力SDK チュートリアル
 
 <br>
-\section contentsMoreTutoFISdk
+\section contentsMoreTutoFISdk	◇ Contents ◇
 　　 \ref moreTutoFISdk						<br>
 　　　　 \ref moreTutoCreateFISdk			<br>
-
+　　 \ref moreTutoFileIO					<br>
+　　　　 \ref moreTutoCreateFileX			<br>
+　　　　 \ref moreTutoLoadFileX				<br>
+　　　　 \ref moreTutoSaveFileX				<br>
 <br><br>
 <hr>
 
@@ -21,8 +24,39 @@
 <div align="right">
 <a href="#pageMoreTutoFISdk">top of page</a>
 </div> 
+<hr>
 
 
+<!-------------------- 2．　Xファイル ローダ＆セーバ -------------------------------------------->
+\section  moreTutoFileIO		2．　ローダ＆セーバ
+任意のXファイルの読み込み、書き出しが可能な、ローダ＆セーバがSpringhead2には用意されています．
+ローダ機能を用いれば、オブジェクトの頂点座標や物理パラメータなどを気にせず、\ref pageGraphics の
+DrawScene()メソッドなどを呼び出すだけで描画できます．
+
+\subsection moreTutoCreateFileX		2.1．　ファイルハンドルの作成
+ファイルの入出力にはファイルハンドルを使います．
+\verbatim 
+    FIFileXIf* fileX = fiSdk->CreateFileX();     // ファイルハンドルの作成
+\endverbatim
+<br>
+
+\subsection moreTutoLoadFileX		2.2．　Xファイルローダー
+指定されたXファイルをロードします．
+\verbatim 
+　　ObjectIfs objs;　　　　　　　　　　　　　　　// インタフェースクラスへのポインタ配列を用意
+　　fileX->Load(objs, "test.x");                 // Xファイルをロード
+\endverbatim
+<br>
+
+\subsection moreTutoSaveFileX		2.3．　Xファイルセーバー
+指定した名前で、Xファイルのセーブを行います．
+\verbatim 
+　　fileX->Save(objs, "out.x");                  // Xファイルのセーブ
+\endverbatim
+<br><br>
+<div align="right">
+<a href="#pageMoreTutoFISdk">top of page</a>
+</div> 
 
 
 */
