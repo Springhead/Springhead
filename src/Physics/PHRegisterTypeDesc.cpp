f8364f2db1342649759526dc18df5a9ddc80e7a9
hase <hase@05cee5c3-a2e9-0310-9523-9dfc2f93dbe1>
2006-06-06 07:33:12 +0000

    git-svn-id: http://springhead.info/spr2/Springhead2/trunk@1719 05cee5c3-a2e9-0310-9523-9dfc2f93dbe1

---
+++ src/Physics/PHRegisterTypeDesc.cpp
@@ -0,0 +1,25 @@
+#include <Foundation/Foundation.h>
+#ifdef USE_HDRSTOP
+#pragma hdrstop
+#endif
+
+#include "TypeDescDump.h"
+
+
+namespace Spr{ 
+static UTTypeDesc* desc; 
+static UTTypeDesc::Field* field; 
+
+void PHRegisterTypeDescs(){
+	static bool bFirst=true;
+	if (!bFirst) return;
+	bFirst = false;
+
+	UTRegisterTypeDescs();
+
+	UTTypeDescDb* db = UTTypeDescDb::theTypeDescDb;
+	#include "TypeDescDumpImp.h"
+	db->RegisterAlias("PHConstraintDesc", "PHJointDesc");
+	db->Link();
+}
+}
