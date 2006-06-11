915ec14ea1c00acdc57dd9ef5cfd0ab35fc884f1
hase <hase@05cee5c3-a2e9-0310-9523-9dfc2f93dbe1>
2006-06-11 16:53:47 +0000

    git-svn-id: http://springhead.info/spr2/Springhead2/trunk@1756 05cee5c3-a2e9-0310-9523-9dfc2f93dbe1

--- src/Foundation/MakeTypeDesc.bat
+++ src/Foundation/MakeTypeDesc.bat
@@ -1,15 +1,39 @@
+@echo off
 rem include/*/*.h から， struct の定義を抜き出して，
 rem FIDesc.cppにTypeDescの定義を作る．．
 rem bin/typedesc.exe が抜き出しを行う．
+if "%1"=="LISTSRC" goto ListSrc
+if "%1"=="EXECUTE" goto Execute
+
 set OUT=TypeDescDumpImp.h
 set HEADER=TypeDescDump.h
 set TARGET=%1 %2 %3 %4 %5 %6 %7 %8 %9
+set MAKEFILE=MakeTypeDescMAK.txt
+cmd /v:on /c%0 LISTSRC
+goto end
 
+:ListSrc
+rem makefileの作成とMakeの実行
 set PATH=%SPRINGHEAD2%\bin
-del %OUT%
-del %HEADER%
+set SRC=
+for %%p in (%TARGET%) do for %%f in (../../include/%%p/*.h) do set SRC=!SRC! ../../include/%%p/%%f
+echo source files:%SRC%
+
+echo #	Do not edit. MakeTypeDesc.bat will update this file.> %MAKEFILE%
+echo all: %OUT%>>%MAKEFILE%
+echo %OUT%: %SRC%>>%MAKEFILE%
+echo 	MakeTypeDesc.bat EXECUTE>>%MAKEFILE%
+make -f%MAKEFILE%
+goto end
+
+
+:Execute
+rem Makeが呼び出す．
+
 echo //	Do not edit. MakeTypeDesc.bat will update this file.> %OUT%
+echo //	Do not edit. MakeTypeDesc.bat will update this file.> %HEADER%
 for %%p in (%TARGET%) do for %%f in (../../include/%%p/*.h) do echo #include "../../include/%%p/%%f" >> %HEADER%
 
 for %%p in (%TARGET%) do for %%f in (../../include/%%p/*.h) do typedesc ../../include/%%p/%%f >> %OUT%
 if exist UTBaseType.h typedesc UTBaseType.h >> %OUT%
+:end
--- src/Foundation/TypeDescDump.h
+++ src/Foundation/TypeDescDump.h
@@ -1,2 +1,3 @@
+//	Do not edit. MakeTypeDesc.bat will update this file.
 #include "../../include/Foundation/SprObject.h" 
 #include "../../include/Foundation/SprScene.h" 
