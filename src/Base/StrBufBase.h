867f46c91123355a347bf52c1f022a7b18959a9b
shibuya <shibuya@05cee5c3-a2e9-0310-9523-9dfc2f93dbe1>
2006-06-20 10:00:08 +0000

    ソースコードに coppyright 追加
    git-svn-id: http://springhead.info/spr2/Springhead2/trunk@1797 05cee5c3-a2e9-0310-9523-9dfc2f93dbe1

--- src/Base/StrBufBase.h
+++ src/Base/StrBufBase.h
@@ -1,3 +1,10 @@
+/*
+ *  Copyright (c) 2003-2006, Shoichi Hasegawa and Springhead development team 
+ *  All rights reserved.
+ *  This software is free software. You can freely use, distribute and modify this 
+ *  software. Please deal with this software under one of the following licenses: 
+ *  This license itself, Boost Software License, The MIT License, The BSD License.   
+ */
 #ifndef STRBUFBASE_H
 #define STRBUFBASE_H
 #if defined _MSC_VER && _MSC_VER >= 1000
--- src/Foundation/UTBaseType.h
+++ src/Foundation/UTBaseType.h
@@ -1,3 +1,10 @@
+/*
+ *  Copyright (c) 2003-2006, Shoichi Hasegawa and Springhead development team 
+ *  All rights reserved.
+ *  This software is free software. You can freely use, distribute and modify this 
+ *  software. Please deal with this software under one of the following licenses: 
+ *  This license itself, Boost Software License, The MIT License, The BSD License.   
+ */
 #if 0	//	this file is not for compile but for bin/typedesc.exe
 struct Vec2f{ float x; float y; };
 struct Vec2d{ double x; double y; };
