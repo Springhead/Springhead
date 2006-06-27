d45a84f3b7ec42641948debebb2f492ceab9985f
shibuya <shibuya@05cee5c3-a2e9-0310-9523-9dfc2f93dbe1>
2006-06-27 13:28:51 +0000

    マニュアル更新
    git-svn-id: http://springhead.info/spr2/Springhead2/trunk@1832 05cee5c3-a2e9-0310-9523-9dfc2f93dbe1

--- include/docsrc/01UsersGuide/07Graphics.h
+++ include/docsrc/01UsersGuide/07Graphics.h
@@ -6,10 +6,10 @@
 
 <!-------------------- グラフィックスSDKのオブジェクトのイメージ ----------------------------------------->
 \section sec_GRSdkImage グラフィックスSDKのオブジェクトのイメージ
-1つのグラフィックス SDK（GRSdk）は，いくつかのシーン（GRScene）を持つことができ，
-シーンには座標系（Frame）のツリーの中に，メッシュ（GRMesh），光源（GRLight），材質（GRMaterial）
-などがあります．
-グラフィックス SDK 内のオブジェクトの参照関係の例を図に示します．
+\c 1 つのグラフィックス<tt>SDK(GRSdk)</tt>は，いくつかのシーン<tt>(GRScene)</tt>を持つことができ，
+シーンには座標系<tt>(Frame)</tt>のツリーの中に，メッシュ<tt>(GRMesh)</tt>，
+光源<tt>(GRLight)</tt>，材質<tt>(GRMaterial)</tt>などがあります．
+グラフィックス\c SDK 内のオブジェクトの参照関係の例を図に示します．
 <br><br>
 	\image html GRSdkScene.png
 <br><br>
@@ -20,10 +20,10 @@
 
 <!-------------------- グラフィックスSDK ----------------------------------------------------------------->
 \section sec_GRSdk	グラフィックスSDK
-はじめに，グラフィックスに必要となるSDKを用意します．
+はじめに，グラフィックスに必要となる\c SDKを用意します．
 
 \subsection subsec_CreateGRSdk	グラフィックスSDKの作成
-グラフィックスSDKを作成します．
+グラフィックス\c SDKを作成します．
 \verbatim
     GRSdkIf* grSdk = CreateGRSdk();
 \endverbatim
@@ -37,7 +37,7 @@
 
 <!-------------------- デバッグ情報レンダラー ------------------------------------------------------------>
 \section sec_GRRender デバッグ情報レンダラー
-グラフィックスSDKから，デバッグ情報レンダラーを作成します．
+グラフィックス\c SDK から，デバッグ情報レンダラーを作成します．
 
 \subsection subsec_CreateGRRender	デバッグ情報レンダラーの作成
 デバッグ情報レンダラーを作成します．
@@ -52,12 +52,12 @@
 
 <!-------------------- レンダラー用デバイス -------------------------------------------------------------->
 \section sec_GRDevice	レンダラー用デバイス
-レンダリング用デバイスとして，OpenGL または Direct 3D を選択できます．
-ただし現時点では Direct 3D の API が実装されていませんので描画されません．
-今後対応する予定です．デバイスは，OpenGL を選択して下さい．
+レンダリング用デバイスとして， \c OpenGL または <tt>Direct 3D</tt> を選択できます．
+ただし現時点では<tt> Direct 3D の API </tt>が実装されていませんので描画されません．
+今後対応する予定です．デバイスは， \c OpenGL を選択して下さい．
 
 \subsection subsec_CreateGRDevice	レンダラー用デバイスの作成
-OpenGLレンダラー用デバイスを作成します．
+\c OpenGL レンダラー用デバイスを作成します．
 \verbatim
     GRDeviceGLIf* grDevice = grSdk->CreateDeviceGL(window);
 \endverbatim
@@ -85,17 +85,17 @@ OpenGLレンダラー用デバイスを作成します．
 
 <!-------------------- 座標変換 -------------------------------------------------------------------------->
 \section sec_TransMatrix	変換行列
-Springhead2 では，視点行列，モデル行列，射影行列を記憶する行列を内部で持っています．<br>
+\c Springhead2 では，視点行列，モデル行列，射影行列を記憶する行列を内部で持っています．<br>
 これらの行列を定義することによって，意図する変換を行うことができます．
 
 \subsection subsec_LookAt	視点・参照点の位置の指定
 視点と参照点の位置を設定できます．以下の手順に従って下さい．
 \verbatim
-    Affinef view;                                                            // アフィン行列
-	view.Pos() = Vec3f(0.0, 15.0, 15.0);                                // 視点
-	view.LookAtGL(Vec3f(0.0, 0.0, 0.0), Vec3f(0.0, 1.0, 0.0));          // 参照点，上方向ベクトル
-	view = view.inv();                                                  // 逆行列を求める
-	render->SetViewMatrix(view);                                        // 視点行列へ登録
+    Affinef view;                                                       // アフィン行列
+    view.Pos() = Vec3f(0.0, 15.0, 15.0);                                // 視点
+    view.LookAtGL(Vec3f(0.0, 0.0, 0.0), Vec3f(0.0, 1.0, 0.0));          // 参照点，上方向ベクトル
+    view = view.inv();                                                  // 逆行列を求める
+    render->SetViewMatrix(view);                                        // 視点行列へ登録
 \endverbatim
 <br>
 
