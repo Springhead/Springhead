c70da4a5106393f7ab6690de9373e43e769c063d
hase <hase@05cee5c3-a2e9-0310-9523-9dfc2f93dbe1>
2006-06-29 18:34:44 +0000

    git-svn-id: http://springhead.info/spr2/Springhead2/trunk@1858 05cee5c3-a2e9-0310-9523-9dfc2f93dbe1

--- include/SpringheadDoc.bat
+++ include/SpringheadDoc.bat
@@ -1,7 +1,6 @@
 cd Base&call CopyBase.Bat&cd..
 set PATHORG=%PATH%
 set PATH=%SPRINGHEAD2%\BIN;%SPRINGHEAD2%\BIN\Graphviz\bin;%PATH%
-:for %%f in (docsrc Base Foundation Collision Physics Graphics FileIO) do doxygen %%f\%%f.doxy 2> %%f\%%f.log
 doxygen springhead.doxy 2> springhead.log
 set PATH=%PATHORG%
 :END
