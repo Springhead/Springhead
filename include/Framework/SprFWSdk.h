26534b44235f216468fd0b3c99aa5a1be7408bc4
shibuya <shibuya@05cee5c3-a2e9-0310-9523-9dfc2f93dbe1>
2006-08-31 03:22:24 +0000

    FWSdkIf::Regsiter() -> FWSdkIf::ResigterSdk()  修正
    git-svn-id: http://springhead.info/spr2/Springhead2/trunk@2014 05cee5c3-a2e9-0310-9523-9dfc2f93dbe1

--- include/Framework/SprFWSdk.h
+++ include/Framework/SprFWSdk.h
@@ -14,7 +14,7 @@ namespace Spr{
 		virtual int NScene() const=0;
 		virtual FWSceneIf* GetScene(size_t i)=0;
 
-		static void SPR_CDECL Register();
+		static void SPR_CDECL RegisterSdk();
 		static FWSdkIf* SPR_CDECL FWSdkIf::CreateSdk();
 	};
 }
