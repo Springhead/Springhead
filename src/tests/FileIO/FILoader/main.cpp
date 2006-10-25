8e98284a77e0150135b353bcf9131d42986aa501
shibuya <shibuya@05cee5c3-a2e9-0310-9523-9dfc2f93dbe1>
2006-10-25 05:30:14 +0000

    Xファイル修正、コメント修正
    git-svn-id: http://springhead.info/spr2/Springhead2/trunk@2154 05cee5c3-a2e9-0310-9523-9dfc2f93dbe1

--- src/tests/FileIO/FILoader/main.cpp
+++ src/tests/FileIO/FILoader/main.cpp
@@ -22,7 +22,7 @@
 【テストパターン】
   ※ 本ファイルの28行目 " #define TEST_FILEX " にて、入力ファイル名を指定する。
 　  test1.x  : 凸形状(mesh)のテスト
-    test2.x  : 凸形状(mesh)のテスト（お互い衝突させない剛体として、"soBlock2"と"soFloor"を設定）
+    test2.x  : 凸形状(mesh)のテスト（青いBoxと床を、互いに衝突させない剛体として登録）
     test3.x  : 凸形状(mesh)と球(sphere)のテスト
 	test4.x  : 凸形状(mesh)と球(sphere)と直方体(box)のテスト
 	test5.x  : timeStep=0.001におけるテスト（直方体１つを自由落下）
