3a1d18a21a060550deb7e25f9c5a3b245a7ec80d
hase <hase@05cee5c3-a2e9-0310-9523-9dfc2f93dbe1>
2007-01-10 03:07:31 +0000

    git-svn-id: http://springhead.info/spr2/Springhead2/trunk@2344 05cee5c3-a2e9-0310-9523-9dfc2f93dbe1

--- src/Collision/CDGJK.cpp
+++ src/Collision/CDGJK.cpp
@@ -30,151 +30,6 @@ static Vec3d p[4];			// Aのサポートポイント(ローカル系)
 static Vec3d q[4];			// Bのサポートポイント(ローカル系)
 static Vec3d p_q[4];		// ミンコスキー和上でのサポートポイント(ワールド系)
 
-//	過去のSupportPointが張る形状(点・線分・3角形，4面体)．最大4点まで．
-static int usedBits;		//	下位4ビットが，どの4点で現在の形状が構成されるかを表す．
-static int lastId;			//	最後に見つけたSupportPointをしまった場所(0～3)
-static int lastBit;			//	lastBit = 1<<lastId (ビットで表したもの）
-static int allUsedBits;		//	usedBitsにlastBitを加えたもの
-static double det[16][4];	//	係数
-//	det[3][0] * p[0] + det[3][1]*p[1] + det[3][2]*p[2] / sum で最近傍点が求まる
-//	p_qベクトルたちが張る形状の体積(or長さor面積)．4点から作れる16通りについて
-
-/*
-      a   |
-     /    | v = (1-k) a + k b
-    /     | va = (1-k)aa + kba = vb = (1-k)ab + kbb
-   /v     | -kaa + kba + kab - kbb = -aa + ab
-  b       | k = a^2-ab / (a^2+b^2-2ab), 1-k = b^2-ab / (a^2+b^2-2ab)
-----------+ なので， k = a^2-ab を と (k-1) = b^2-abを記録しておく	*/
-
-///	det(長さ・面積・体積)の計算．垂線の足が頂点を何:何に内分するかも計算
-inline void CalcDet() {
-	static double dotp[4][4];	//	p_q[i] * p_q[j] 
-
-	//	新しく増えた点(lastId)について，内積を計算
-	for (int i = 0, curPoint = 1; i < 4; ++i, curPoint <<=1){
-		if (usedBits & curPoint) 
-			dotp[i][lastId] = dotp[lastId][i] = p_q[i] * p_q[lastId];
-	}
-	dotp[lastId][lastId] = p_q[lastId] * p_q[lastId];
-
-	det[lastBit][lastId] = 1;
-	for (int j = 0, sj = 1; j < 4; ++j, sj <<= 1) {
-		if (usedBits & sj) {		
-			int s2 = sj|lastBit;	//	新しく増えた点について係数の計算
-			det[s2][j] = dotp[lastId][lastId] - dotp[lastId][j];	//	a^2-ab
-			det[s2][lastId] = dotp[j][j] - dotp[j][lastId];			//	b^2-ab
-			for (int k = 0, sk = 1; k < j; ++k, sk <<= 1) {	//	3点の場合
-				if (usedBits & sk) {
-					int s3 = sk|s2;
-					det[s3][k] = det[s2][j] * (dotp[j][j] - dotp[j][k]) + 
-						det[s2][lastId] * (dotp[lastId][j] - dotp[lastId][k]);
-					det[s3][j] = det[sk|lastBit][k] * (dotp[k][k] - dotp[k][j]) + 
-						det[sk|lastBit][lastId] * (dotp[lastId][k] - dotp[lastId][j]);
-					det[s3][lastId] = det[sk|sj][k] * (dotp[k][k] - dotp[k][lastId]) + 
-						det[sk|sj][j] * (dotp[j][k] - dotp[j][lastId]);
-				}
-			}
-		}
-	}
-	if (allUsedBits == 15) {	//	4点の場合
-		det[15][0] =	det[14][1] * (dotp[1][1] - dotp[1][0]) + 
-						det[14][2] * (dotp[2][1] - dotp[2][0]) + 
-						det[14][3] * (dotp[3][1] - dotp[3][0]);
-		det[15][1] =	det[13][0] * (dotp[0][0] - dotp[0][1]) + 
-						det[13][2] * (dotp[2][0] - dotp[2][1]) + 
-						det[13][3] * (dotp[3][0] - dotp[3][1]);
-		det[15][2] =	det[11][0] * (dotp[0][0] - dotp[0][2]) + 
-						det[11][1] * (dotp[1][0] - dotp[1][2]) +	
-						det[11][3] * (dotp[3][0] - dotp[3][2]);
-		det[15][3] =	det[ 7][0] * (dotp[0][0] - dotp[0][3]) + 
-						det[ 7][1] * (dotp[1][0] - dotp[1][3]) + 
-						det[ 7][2] * (dotp[2][0] - dotp[2][3]);
-	}	
-}
-
-
-//	係数から，最近傍点 v を計算
-inline void CalcVector(int usedBits, Vec3d& v) {
-	double sum = 0;
-	v.clear();
-	for (int i = 0; i < 4; ++i) {
-		if (usedBits & (1<<i)) {
-			sum += det[usedBits][i];
-			v += p_q[i] * det[usedBits][i];
-		}
-	}
-	v *= 1 / sum;
-}
-
-//	係数から，最近傍点と，元の2つの図形上での，その点の位置を計算
-inline void CalcPoints(int usedBits, Vec3d& p1, Vec3d& p2) {
-	double sum = 0;
-	p1.clear();
-	p2.clear();
-	for (int i = 0, curPoint = 1; i < 4; ++i, curPoint <<= 1) {
-		if (usedBits & curPoint) {
-			sum += det[usedBits][i];
-			p1 += p[i] * det[usedBits][i];
-			p2 += q[i] * det[usedBits][i];
-		}
-	}
-	if (sum){
-		double s = 1 / sum;
-		p1 *= s;
-		p2 *= s;
-	}else{
-		p1 = p[0];
-		p2 = q[0];
-	}
-}
-
-//	最近傍点を返す．
-inline bool CalcClosest(Vec3d& v) {
-	CalcDet();
-	if (!usedBits){
-		usedBits = lastBit;
-		v = p_q[lastId];
-		return true;
-	}
-	int simplex[5][4];
-	int nSimplex[5];
-	const char numVertices[] = {
-		0, 1, 1, 2, 1, 2, 2, 3,
-		1, 2, 2, 3, 2, 3, 3, 4
-	};
-	int nVtx = numVertices[allUsedBits];
-	simplex[nVtx][0] = allUsedBits;
-	nSimplex[nVtx] = 1;
-	
-	for(; nVtx>1; --nVtx){
-		for(int sid=0; sid<nSimplex[nVtx]; ++sid){
-			int s = simplex[nVtx][sid];
-			nSimplex[nVtx-1]=0;
-			for(int i = 0; i!=4; ++i){
-				int bit = 1<<i;
-				if ((s&bit) && det[s][i] <= 0){
-					simplex[nVtx-1][nSimplex[nVtx-1]] = s & ~bit;
-					++ nSimplex[nVtx-1];
-				}
-			}
-			if (nSimplex[nVtx-1] == 0){
-				usedBits = s;
-				CalcVector(usedBits, v);	//	最近傍点を計算して返す．			
-				return true;
-			}
-		}
-	}
-	return false;
-}
-
-//	新しい点wが，今までの点と等しい場合
-inline bool HasSame(const Vec3d& w) {
-	for (int i = 0; i < 4; ++i){
-		if ((allUsedBits & (1<<i)) && (p_q[i]-w).square() < epsilon2) return true;
-	}
-	return false;
-}
 const char vacants[] = {
 	0, 1, 0, 2, 0, 1, 0, 3,
 	0, 1, 0, 2, 0, 1, 0, 4,
@@ -186,75 +41,6 @@ inline char VacantIdFromId(char a, char b){
 inline char VacantIdFromId(char a, char b, char c){
 	char bits = (1<<a) | (1<<b) | (1<<c);
 	return vacants[(int)bits];
-}
-inline char VacantIdFromBits(char bits){
-	return vacants[(int)bits];
-}
-
-bool FindCommonPoint(const CDConvex* a, const CDConvex* b,
-	const Posed& a2w, const Posed& b2w, Vec3d& v, Vec3d& pa, Vec3d& pb) {
-	Vec3d w;
-
-	usedBits = 0;
-	allUsedBits = 0;
-	int count = 0;
-	do {
-		lastId = VacantIdFromBits(usedBits);
-		lastBit = 1 << lastId;
-
-		p[lastId] = a->Support(a2w.Ori().Conjugated() * (-v));
-		q[lastId] = b->Support(b2w.Ori().Conjugated() * v);
-		w = a2w * p[lastId]  -  b2w * q[lastId];
-		if (v*w > 0) return false;			//	原点がsupport面の外側
-		if (HasSame(w)) return false;		//	supportが1点に集中＝原点は外にある．
-		p_q[lastId] = w;					//	新しい点を代入
-		allUsedBits = usedBits|lastBit;//	使用中頂点リストに追加
-		if (!CalcClosest(v)) return false;
-
-		count ++;
-		if (count > 100){
-			Vec3d vtxs[3];
-			Vec3d notUsed[3];
-			int nUsed=0, nNotUsed=0;
-			for(int i=0; i<4; ++i){
-				if (usedBits & (1<<i)){
-					vtxs[nUsed] = p_q[i];
-					nUsed++;
-				}else{
-					notUsed[nNotUsed] = p_q[i];
-					nNotUsed++;
-				}
-			}
-			if (nUsed == 3 && allUsedBits == 15){
-				Vec3d n = (vtxs[2]-vtxs[0]) ^ (vtxs[1]-vtxs[0]);
-				n.unitize();
-				if (n * (notUsed[0] - vtxs[0]) > 0){
-					n *= -1;
-				}
-				double dist = n * (-vtxs[0]);
-				DSTR << "n:\t" << nUsed << "Dist:\t" << dist << std::endl;
-				DSTR << "v:\t" << v.x << "\t" << v.y << "\t" << v.z << std::endl; 
-//				DSTR << "Normal:\t" << n.x << "\t" << n.y << "\t" << n.z << std::endl;
-				for(int i=0; i<nUsed; ++i){
-					DSTR << "Use:\t" << vtxs[i].x << "\t" << vtxs[i].y << "\t" << vtxs[i].z << std::endl;
-				}
-				for(int i=0; i<nNotUsed; ++i){
-					DSTR << "Not:\t" << notUsed[i].x << "\t" << notUsed[i].y << "\t" << notUsed[i].z << std::endl;
-				}
-			}else{
-				DSTR << "n:\t" << nUsed << std::endl;
-				DSTR << "v:\t" << v.x << "\t" << v.y << "\t" << v.z << std::endl;
-				for(int i=0; i<nUsed; ++i){
-					DSTR << "Use:\t" << vtxs[i].x << "\t" << vtxs[i].y << "\t" << vtxs[i].z << std::endl;
-				}
-				for(int i=0; i<nNotUsed; ++i){
-					DSTR << "Not:\t" << notUsed[i].x << "\t" << notUsed[i].y << "\t" << notUsed[i].z << std::endl;
-				}
-			}
-		}
-	} while ( usedBits < 15 && !(v.square() < epsilon2) ) ;
-	CalcPoints(usedBits, pa, pb);
-	return true;
 }
 
 struct CDGJKIds{
@@ -493,7 +279,236 @@ int ContFindCommonPoint(const CDConvex* a, const CDConvex* b,
 	return last->nVtx;
 }
 
-static int lastUsed;
+
+#define USE_NON_CONTINUOUS_DETECTORS
+#ifdef USE_NON_CONTINUOUS_DETECTORS
+/*	GJKアルゴリズム(FindCommonPoint(), FindClosestPoint())の実装．
+	SOLIDのソースを参考にして書いた部分です．
+	FindClosestPointは使っていません．
+	
+	FindCommonPointは，
+		CDShapePair::Detect()
+		CDContactAnalysis::CalcNormal(CDShapePair* cp)
+	で使っています．ペナルティ法を使うと呼び出されます．	*/
+
+//	過去のSupportPointが張る形状(点・線分・3角形，4面体)．最大4点まで．
+static int usedBits;		//	下位4ビットが，どの4点で現在の形状が構成されるかを表す．
+static int lastId;			//	最後に見つけたSupportPointをしまった場所(0～3)
+static int lastBit;			//	lastBit = 1<<lastId (ビットで表したもの）
+static int allUsedBits;		//	usedBitsにlastBitを加えたもの
+static double det[16][4];	//	係数
+//	det[3][0] * p[0] + det[3][1]*p[1] + det[3][2]*p[2] / sum で最近傍点が求まる
+//	p_qベクトルたちが張る形状の体積(or長さor面積)．4点から作れる16通りについて
+
+/*
+      a   |
+     /    | v = (1-k) a + k b
+    /     | va = (1-k)aa + kba = vb = (1-k)ab + kbb
+   /v     | -kaa + kba + kab - kbb = -aa + ab
+  b       | k = a^2-ab / (a^2+b^2-2ab), 1-k = b^2-ab / (a^2+b^2-2ab)
+----------+ なので， k = a^2-ab を と (k-1) = b^2-abを記録しておく	*/
+
+///	det(長さ・面積・体積)の計算．垂線の足が頂点を何:何に内分するかも計算
+inline void CalcDet() {
+	static double dotp[4][4];	//	p_q[i] * p_q[j] 
+
+	//	新しく増えた点(lastId)について，内積を計算
+	for (int i = 0, curPoint = 1; i < 4; ++i, curPoint <<=1){
+		if (usedBits & curPoint) 
+			dotp[i][lastId] = dotp[lastId][i] = p_q[i] * p_q[lastId];
+	}
+	dotp[lastId][lastId] = p_q[lastId] * p_q[lastId];
+
+	det[lastBit][lastId] = 1;
+	for (int j = 0, sj = 1; j < 4; ++j, sj <<= 1) {
+		if (usedBits & sj) {		
+			int s2 = sj|lastBit;	//	新しく増えた点について係数の計算
+			det[s2][j] = dotp[lastId][lastId] - dotp[lastId][j];	//	a^2-ab
+			det[s2][lastId] = dotp[j][j] - dotp[j][lastId];			//	b^2-ab
+			for (int k = 0, sk = 1; k < j; ++k, sk <<= 1) {	//	3点の場合
+				if (usedBits & sk) {
+					int s3 = sk|s2;
+					det[s3][k] = det[s2][j] * (dotp[j][j] - dotp[j][k]) + 
+						det[s2][lastId] * (dotp[lastId][j] - dotp[lastId][k]);
+					det[s3][j] = det[sk|lastBit][k] * (dotp[k][k] - dotp[k][j]) + 
+						det[sk|lastBit][lastId] * (dotp[lastId][k] - dotp[lastId][j]);
+					det[s3][lastId] = det[sk|sj][k] * (dotp[k][k] - dotp[k][lastId]) + 
+						det[sk|sj][j] * (dotp[j][k] - dotp[j][lastId]);
+				}
+			}
+		}
+	}
+	if (allUsedBits == 15) {	//	4点の場合
+		det[15][0] =	det[14][1] * (dotp[1][1] - dotp[1][0]) + 
+						det[14][2] * (dotp[2][1] - dotp[2][0]) + 
+						det[14][3] * (dotp[3][1] - dotp[3][0]);
+		det[15][1] =	det[13][0] * (dotp[0][0] - dotp[0][1]) + 
+						det[13][2] * (dotp[2][0] - dotp[2][1]) + 
+						det[13][3] * (dotp[3][0] - dotp[3][1]);
+		det[15][2] =	det[11][0] * (dotp[0][0] - dotp[0][2]) + 
+						det[11][1] * (dotp[1][0] - dotp[1][2]) +	
+						det[11][3] * (dotp[3][0] - dotp[3][2]);
+		det[15][3] =	det[ 7][0] * (dotp[0][0] - dotp[0][3]) + 
+						det[ 7][1] * (dotp[1][0] - dotp[1][3]) + 
+						det[ 7][2] * (dotp[2][0] - dotp[2][3]);
+	}	
+}
+
+
+//	係数から，最近傍点 v を計算
+inline void CalcVector(int usedBits, Vec3d& v) {
+	double sum = 0;
+	v.clear();
+	for (int i = 0; i < 4; ++i) {
+		if (usedBits & (1<<i)) {
+			sum += det[usedBits][i];
+			v += p_q[i] * det[usedBits][i];
+		}
+	}
+	v *= 1 / sum;
+}
+
+//	係数から，最近傍点と，元の2つの図形上での，その点の位置を計算
+inline void CalcPoints(int usedBits, Vec3d& p1, Vec3d& p2) {
+	double sum = 0;
+	p1.clear();
+	p2.clear();
+	for (int i = 0, curPoint = 1; i < 4; ++i, curPoint <<= 1) {
+		if (usedBits & curPoint) {
+			sum += det[usedBits][i];
+			p1 += p[i] * det[usedBits][i];
+			p2 += q[i] * det[usedBits][i];
+		}
+	}
+	if (sum){
+		double s = 1 / sum;
+		p1 *= s;
+		p2 *= s;
+	}else{
+		p1 = p[0];
+		p2 = q[0];
+	}
+}
+
+//	最近傍点を返す．
+inline bool CalcClosest(Vec3d& v) {
+	CalcDet();
+	if (!usedBits){
+		usedBits = lastBit;
+		v = p_q[lastId];
+		return true;
+	}
+	int simplex[5][4];
+	int nSimplex[5];
+	const char numVertices[] = {
+		0, 1, 1, 2, 1, 2, 2, 3,
+		1, 2, 2, 3, 2, 3, 3, 4
+	};
+	int nVtx = numVertices[allUsedBits];
+	simplex[nVtx][0] = allUsedBits;
+	nSimplex[nVtx] = 1;
+	
+	for(; nVtx>1; --nVtx){
+		for(int sid=0; sid<nSimplex[nVtx]; ++sid){
+			int s = simplex[nVtx][sid];
+			nSimplex[nVtx-1]=0;
+			for(int i = 0; i!=4; ++i){
+				int bit = 1<<i;
+				if ((s&bit) && det[s][i] <= 0){
+					simplex[nVtx-1][nSimplex[nVtx-1]] = s & ~bit;
+					++ nSimplex[nVtx-1];
+				}
+			}
+			if (nSimplex[nVtx-1] == 0){
+				usedBits = s;
+				CalcVector(usedBits, v);	//	最近傍点を計算して返す．			
+				return true;
+			}
+		}
+	}
+	return false;
+}
+
+//	新しい点wが，今までの点と等しい場合
+inline bool HasSame(const Vec3d& w) {
+	for (int i = 0; i < 4; ++i){
+		if ((allUsedBits & (1<<i)) && (p_q[i]-w).square() < epsilon2) return true;
+	}
+	return false;
+}
+inline char VacantIdFromBits(char bits){
+	return vacants[(int)bits];
+}
+
+bool FindCommonPoint(const CDConvex* a, const CDConvex* b,
+	const Posed& a2w, const Posed& b2w, Vec3d& v, Vec3d& pa, Vec3d& pb) {
+	Vec3d w;
+
+	usedBits = 0;
+	allUsedBits = 0;
+	int count = 0;
+	do {
+		lastId = VacantIdFromBits(usedBits);
+		lastBit = 1 << lastId;
+
+		p[lastId] = a->Support(a2w.Ori().Conjugated() * (-v));
+		q[lastId] = b->Support(b2w.Ori().Conjugated() * v);
+		w = a2w * p[lastId]  -  b2w * q[lastId];
+		if (v*w > 0) return false;			//	原点がsupport面の外側
+		if (HasSame(w)) return false;		//	supportが1点に集中＝原点は外にある．
+		p_q[lastId] = w;					//	新しい点を代入
+		allUsedBits = usedBits|lastBit;//	使用中頂点リストに追加
+		if (!CalcClosest(v)) return false;
+
+		count ++;
+		if (count > 100){
+			Vec3d vtxs[3];
+			Vec3d notUsed[3];
+			int nUsed=0, nNotUsed=0;
+			for(int i=0; i<4; ++i){
+				if (usedBits & (1<<i)){
+					vtxs[nUsed] = p_q[i];
+					nUsed++;
+				}else{
+					notUsed[nNotUsed] = p_q[i];
+					nNotUsed++;
+				}
+			}
+			if (nUsed == 3 && allUsedBits == 15){
+				Vec3d n = (vtxs[2]-vtxs[0]) ^ (vtxs[1]-vtxs[0]);
+				n.unitize();
+				if (n * (notUsed[0] - vtxs[0]) > 0){
+					n *= -1;
+				}
+				double dist = n * (-vtxs[0]);
+				DSTR << "n:\t" << nUsed << "Dist:\t" << dist << std::endl;
+				DSTR << "v:\t" << v.x << "\t" << v.y << "\t" << v.z << std::endl; 
+//				DSTR << "Normal:\t" << n.x << "\t" << n.y << "\t" << n.z << std::endl;
+				for(int i=0; i<nUsed; ++i){
+					DSTR << "Use:\t" << vtxs[i].x << "\t" << vtxs[i].y << "\t" << vtxs[i].z << std::endl;
+				}
+				for(int i=0; i<nNotUsed; ++i){
+					DSTR << "Not:\t" << notUsed[i].x << "\t" << notUsed[i].y << "\t" << notUsed[i].z << std::endl;
+				}
+			}else{
+				DSTR << "n:\t" << nUsed << std::endl;
+				DSTR << "v:\t" << v.x << "\t" << v.y << "\t" << v.z << std::endl;
+				for(int i=0; i<nUsed; ++i){
+					DSTR << "Use:\t" << vtxs[i].x << "\t" << vtxs[i].y << "\t" << vtxs[i].z << std::endl;
+				}
+				for(int i=0; i<nNotUsed; ++i){
+					DSTR << "Not:\t" << notUsed[i].x << "\t" << notUsed[i].y << "\t" << notUsed[i].z << std::endl;
+				}
+			}
+		}
+	} while ( usedBits < 15 && !(v.square() < epsilon2) ) ;
+	CalcPoints(usedBits, pa, pb);
+	return true;
+}
+
+
+#define USE_FIND_CLOSEST_POINT
+#ifdef USE_FIND_CLOSEST_POINT
 
 inline bool IsDegenerate(const Vec3d& w) {
 	for (int i = 0, curPoint = 1; i < 4; ++i, curPoint <<= 1){
@@ -518,8 +533,8 @@ void FindClosestPoints(const CDConvex* a, const CDConvex* b,
 
 	while (usedBits < 15 && dist > epsilon) {
 		lastId = 0;
-		lastUsed = 1;
-		while (usedBits & lastUsed) { ++lastId; lastUsed <<= 1; }
+		lastBit = 1;
+		while (usedBits & lastBit) { ++lastId; lastBit <<= 1; }
 		p[lastId] = a->Support(a2w.Ori().Conjugated() * (-v));
 		q[lastId] = b->Support(b2w.Ori().Conjugated() * v);
 		w = a2w * p[lastId]  -  b2w * q[lastId];
@@ -528,10 +543,13 @@ void FindClosestPoints(const CDConvex* a, const CDConvex* b,
 		if (dist - maxSupportDist <= dist * epsilon) break;
 		if (IsDegenerate(w)) break;
 		p_q[lastId] = w;
-		allUsedBits = usedBits|lastUsed;
+		allUsedBits = usedBits|lastBit;
 		if (!CalcClosest(v)) break;
 		dist = v.norm();
 	}
 	CalcPoints(usedBits, pa, pb);
 }
+#endif
+#endif
+
 }
