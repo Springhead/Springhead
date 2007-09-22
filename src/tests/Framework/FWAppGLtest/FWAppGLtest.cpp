9ffceda544cca9f694940cbc58d8fa2305688bf4
hase <hase@05cee5c3-a2e9-0310-9523-9dfc2f93dbe1>
2007-09-22 10:02:16 +0000

    アニメーションのテストを始める前の本来のプロジェクトに戻した。
    git-svn-id: http://springhead.info/spr2/Springhead2/trunk@2638 05cee5c3-a2e9-0310-9523-9dfc2f93dbe1

--- src/tests/Framework/FWAppGLtest/FWAppGLtest.cpp
+++ src/tests/Framework/FWAppGLtest/FWAppGLtest.cpp
@@ -5,8 +5,7 @@
 #include <SprFramework.h>
 #include <Framework/SprFWAppGLUT.h>
 #include <HumanInterface/HIBase.h>
-#include <GL/glut.h>
-#include <windows.h>
+
 using namespace Spr;
 
 class MyApp;
@@ -14,92 +13,24 @@ MyApp* app;
 
 class MyApp: public FWAppGLUT{
 public:
-	void Init(int argc, char* argv[]){
-		glutInit(&argc, argv);
-		glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA | GLUT_DEPTH);
-		FWAppGL::Init(argc, argv);
-		atexit(FWAppGLUT::AtExit);
+	void Step(){
+		FWAppGLUT::Step();
+		PHSolidIf* s = DCAST(PHSolidIf,  GetSdk()->GetScene()->FindObject("soBlock1"));
 	}
-/*	void Start(){
-		instance = this;
-		glutGameModeString("800x600:32@60");
-		Sleep(500);
-		windowID = glutEnterGameMode();
-		CreateRender();
-		glutDisplayFunc(FWAppGLUT::GlutDisplayFunc);
-		glutReshapeFunc(FWAppGLUT::GlutReshapeFunc);
-		glutKeyboardFunc(FWAppGLUT::GlutKeyboardFunc);
-		glutTimerFunc(1, FWAppGLUT::GlutTimerFunc, 0);
-		glutMainLoop();
-	}
-*/
-
 	void Keyboard(unsigned char key, int x, int y){
 		if (key==0x1b){
+			delete app;
 			exit(0);
-		}
-		Affinef af;
-		GRCameraIf* cam;
-		switch(key){
-		
-			case('a'):	app->GetSdk()->GetScene()->FindObject(cam, "cam");
- 						af = cam->GetFrame()->GetTransform();
-						af =  Affinef::Rot(Rad(5), 'y') * af;
-						cam->GetFrame()->SetTransform(af);
-						break;
-		
-			case('s'):  app->GetSdk()->GetScene()->FindObject(cam, "cam");
- 						af = cam->GetFrame()->GetTransform();
-						af =  Affinef::Rot(Rad(-5), 'y') * af;
-						cam->GetFrame()->SetTransform(af);
-						break;
-		
-			case('w'):  app->GetSdk()->GetScene()->FindObject(cam, "cam");
- 						af = cam->GetFrame()->GetTransform();
-						af =  Affinef::Rot(Rad(5), 'x') * af;
-						cam->GetFrame()->SetTransform(af);
-						break;
-
-			case('z'):  app->GetSdk()->GetScene()->FindObject(cam, "cam");
- 						af = cam->GetFrame()->GetTransform();
-						af =  Affinef::Rot(Rad(-5), 'x') * af;
-						cam->GetFrame()->SetTransform(af);
-						break;
-
-			case('e'):  app->GetSdk()->GetScene()->FindObject(cam, "cam");
- 						af = cam->GetFrame()->GetTransform();
-						af =  Affinef::Rot(Rad(5), 'z') * af;
-						cam->GetFrame()->SetTransform(af);
-						break;
-
-			case('x'):  app->GetSdk()->GetScene()->FindObject(cam, "cam");
- 						af = cam->GetFrame()->GetTransform();
-						af =  Affinef::Rot(Rad(-5), 'z') * af;
-						cam->GetFrame()->SetTransform(af);
-						break;
-
-			case('d'):  app->GetSdk()->GetScene()->FindObject(cam, "cam");
- 						af = cam->GetFrame()->GetTransform();
-						af = af * Affinef::Trn(0,0,0.1);
-						cam->GetFrame()->SetTransform(af);
-						break;
-
-			case('f'):  app->GetSdk()->GetScene()->FindObject(cam, "cam");
- 						af = cam->GetFrame()->GetTransform();
-						af = af * Affinef::Trn(0,0,-0.1);
-						cam->GetFrame()->SetTransform(af);
-						break;
-
 		}
 	}
 };
-MyApp app_;
+
 
 int SPR_CDECL main(int argc, char* argv[]){
-	app = &app_;
+	app = new MyApp;
 	app->Init(argc, argv);
 #ifdef _DEBUG
-//	app->GetSdk()->SetDebugMode(true);
+	app->GetSdk()->SetDebugMode(true);
 #endif
 	app->GetSdk()->Clear();
 	app->GetSdk()->CreateScene(PHSceneDesc(), GRSceneDesc());
@@ -110,7 +41,7 @@ int SPR_CDECL main(int argc, char* argv[]){
 	boxdesc.boxsize = Vec3d(0.1, 0.1, 0.1);
 	floor->AddShape(app->GetSdk()->GetPHSdk()->CreateShape(boxdesc));
 
-	app->GetSdk()->LoadScene("test2.x");
+	app->GetSdk()->LoadScene("block.x");
 //	app->GetFWScene()->AddHumanInterface(new HIMouse);
 
 	app->Start();
