103bf926877786cb7dc5e711fc44f52a14767aec
hase <hase@05cee5c3-a2e9-0310-9523-9dfc2f93dbe1>
2008-05-22 04:03:45 +0000

    git-svn-id: http://springhead.info/spr2/Springhead2/trunk@3290 05cee5c3-a2e9-0310-9523-9dfc2f93dbe1

--- src/HumanInterface/HISpidarG6.cpp
+++ src/HumanInterface/HISpidarG6.cpp
@@ -8,13 +8,90 @@ namespace Spr {;
 
 
 HISpidarG6Desc::HISpidarG6Desc(){
-	assert(0);
+
+
 }
 void HISpidarG6Desc::Init(int nMotor, Vec3f* motorPos, Vec3f* knotPos, float vpn, float lpp, float minF, float maxF){
-	assert(0);
+	motors.resize(nMotor);
+	for(int i=0; i<nMotor; ++i){
+		motors[i].pos = motorPos[i];
+		motors[i].jointPos = knotPos[i];
+		motors[i].lengthPerPulse = lpp;
+		motors[i].voltPerNewton = vpn;		//	volt per newton
+		motors[i].currentPerVolt = 1;		//	抵抗値の逆数
+		motors[i].minForce = minF;
+		motors[i].maxForce = maxF;
+	}
+	VVector<float> minForce, maxForce;
+	minForce.resize(nMotor);
+	maxForce.resize(nMotor);
+	for(int i=0; i<nMotor; ++i){
+		minForce[i] = minF;
+		maxForce[i] = maxF;
+	}
 }
 void HISpidarG6Desc::Init(char* type){
-	assert(0);
+	if (stricmp(type, "SpidarG6X3L")==0 || stricmp(type, "SpidarG6X3R")==0){
+		bool bLeft = stricmp(type, "SpidarG6X3L")==0;
+		//	モータの取り付け位置. モータが直方体に取り付けられている場合は，
+		const float PX = 0.12f/2;		//	x方向の辺の長さ/2
+		const float PY = 0.14f/2;		//	y方向の辺の長さ/2
+		const float PZ = 0.12f/2;		//	z方向の辺の長さ/2
+		//	糸のグリップへの取り付け位置．グリップはピンポン玉を採用しています．
+		const float GX = 0.045f/2;		//	x方向の辺の長さ/2
+		const float GY = 0.045f/2;		//	y方向の辺の長さ/2
+		Matrix3f rotR = Matrix3f::Rot((float)Rad(-45), 'y');
+		Matrix3f rotL = Matrix3f::Rot((float)Rad(-45), 'y');
+
+		Vec3f motorPos[2][8][2] = {		//	モータの取り付け位置(中心を原点とするDirectX座標系（右がX,上がY,奥がZ）)
+			{
+				{rotR*Vec3f(-PX,-PY, PZ), rotR*Vec3f( -GX, 0.0f, 0.0f)},
+				{rotR*Vec3f( PX,-PY, PZ), rotR*Vec3f(  GX, 0.0f, 0.0f)},
+				{rotR*Vec3f( PX,-PY,-PZ), rotR*Vec3f(0.0f,  -GY, 0.0f)},
+				{rotR*Vec3f(-PX,-PY,-PZ), rotR*Vec3f(0.0f,  -GY, 0.0f)},
+				{rotR*Vec3f(-PX, PY, PZ), rotR*Vec3f( -GX, 0.0f, 0.0f)},
+				{rotR*Vec3f( PX, PY, PZ), rotR*Vec3f(  GX, 0.0f, 0.0f)},
+				{rotR*Vec3f( PX, PY,-PZ), rotR*Vec3f(0.0f,   GY, 0.0f)},
+				{rotR*Vec3f(-PX, PY,-PZ), rotR*Vec3f(0.0f,   GY, 0.0f)}
+			},
+			{
+				{rotL*Vec3f( PX, PY,-PZ), rotL*Vec3f(0, 0.0f,  -GX)},
+				{rotL*Vec3f( PX, PY, PZ), rotL*Vec3f(0, 0.0f,   GX)},
+				{rotL*Vec3f(-PX, PY, PZ), rotL*Vec3f(0,   GY, 0.0f)},
+				{rotL*Vec3f(-PX, PY,-PZ), rotL*Vec3f(0,   GY, 0.0f)},
+				{rotL*Vec3f( PX,-PY,-PZ), rotL*Vec3f(0, 0.0f,  -GX)},
+				{rotL*Vec3f( PX,-PY, PZ), rotL*Vec3f(0, 0.0f,   GX)},
+				{rotL*Vec3f(-PX,-PY, PZ), rotL*Vec3f(0,  -GY, 0.0f)},
+				{rotL*Vec3f(-PX,-PY,-PZ), rotL*Vec3f(0,  -GY, 0.0f)},
+			}
+		};
+		Vec3f mp[8];
+		Vec3f kp[8];
+		if (bLeft){
+			for(int i=0; i<8; ++i){
+				mp[i] = motorPos[1][i][0];
+				kp[i] = motorPos[1][i][1];
+			}
+		}else{
+			for(int i=0; i<8; ++i){
+				mp[i] = motorPos[0][i][0];
+				kp[i] = motorPos[0][i][1];
+			}
+		}
+		Init(8, mp, kp, 0.365296803653f, 1.66555e-5f, 0.3f, 20.0f);
+		if (bLeft){
+			motors[2].lengthPerPulse *= -1;
+			motors[3].lengthPerPulse *= -1;
+			motors[4].lengthPerPulse *= -1;
+			motors[5].lengthPerPulse *= -1;
+
+		}else{
+			motors[2].lengthPerPulse *= -1;
+			motors[3].lengthPerPulse *= -1;
+			motors[4].lengthPerPulse *= -1;
+			motors[5].lengthPerPulse *= -1;
+		}
+	}
 }
 
 //----------------------------------------------------------------------------
