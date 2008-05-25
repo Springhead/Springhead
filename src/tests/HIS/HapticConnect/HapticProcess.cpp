dbe9858944cba943bd0a3c60983e33d98e9eda24
susa <susa@05cee5c3-a2e9-0310-9523-9dfc2f93dbe1>
2008-05-25 18:01:23 +0000

    PHDetectorで近傍物体探索をしていたが，止めたので消しました． src/tests/HIS/HapticConnectを追加しました．
    git-svn-id: http://springhead.info/spr2/Springhead2/trunk@3308 05cee5c3-a2e9-0310-9523-9dfc2f93dbe1

---
+++ src/tests/HIS/HapticConnect/HapticProcess.cpp
@@ -0,0 +1,123 @@
+#include "HapticProcess.h"
+#include <iostream>
+#include <sstream>
+#include <cmath>
+#include <ctime>
+#include <string>
+#include <GL/glut.h>
+#include <vector>
+#include <windows.h>
+#include <fstream>
+
+void HapticProcess::Init(){
+	InitDevice();
+}
+
+void HapticProcess::InitDevice(){
+	// デバイスマネージャの初期化
+	devMan.RPool().Register(new DRUsb20Sh4(0));	    // USB2.0版コントローラ 8モータ
+	devMan.RPool().Register(new DRUsb20Sh4(1));	    // USB2.0版コントローラ 8モータ
+	devMan.Init();														// デバイスの初期化
+	cout << devMan;												// 初期化の結果を表示
+
+	// SPIDARの初期化
+	spidarG6.Init(devMan, false);
+	for(int i = 0; i < 8; ++i){
+		spidarG6.motor[i].maxForce = 5.0f;
+	}
+	spidarG6.Calib();
+}
+
+void HapticProcess::Step(){
+	UpdateSpidar();
+	HapticRendering();
+	LocalDynamics();
+}
+
+void HapticProcess::UpdateSpidar(){
+	spidarG6.Update(dt);
+	hpointer.SetFramePosition(spidarG6.GetPos() * 200);
+	hpointer.SetOrientation(spidarG6.GetOri());
+	hpointer.SetVelocity(spidarG6.GetVel());
+	hpointer.SetAngularVelocity(spidarG6.GetAngVel());
+}
+
+void HapticProcess::HapticRendering(){
+	addforce = Vec3d(0.0, 0.0, 0.0); 
+	for(unsigned i = 0; i < neighborObjects.size(); i++){
+		if(!neighborObjects[i].blocal) continue;
+		Vec3d cPoint = neighborObjects[i].phSolid.GetPose() * neighborObjects[i].closestPoint;
+		Vec3d pPoint = hpointer.GetPose() * neighborObjects[i].pointerPoint;
+		float	f = (pPoint-cPoint) * neighborObjects[i].direction;
+		Vec3d ortho = f * neighborObjects[i].direction;
+		if(f < 0.0){
+			CalcForce(ortho);
+			neighborObjects[i].phSolid.AddForce(-addforce * 10);
+		}
+	}
+	if(bDisplayforce) spidarG6.SetForce(addforce);		// 力覚提示
+}
+
+void HapticProcess::CalcForce(Vec3d dis){
+	addforce += -K * dis;
+};
+
+void HapticProcess::LocalDynamics(){
+	GenerateForce();
+	Integrate();
+	ClearForce();
+}
+
+void HapticProcess::GenerateForce(){}
+void HapticProcess::Integrate(){
+	// ツリーに属さない剛体の更新
+	for(unsigned i = 0; i < neighborObjects.size(); i++){
+		if(!neighborObjects[i].blocal) continue;
+		double minv  = neighborObjects[i].phSolid.GetMassInv();
+		//Matrix3d inv = neighborObjects[i].phSolid.GetInertiaInv();
+		//Vec3d accel = minv * neighborObjects[i].phSolid.nextForce;
+		//Vec3d angaccel = inv * cross((neighborObjects[i].pointerPoint - neighborObjects[i].phSolid.GetCenterPosition()), neighborObjects[i].phSolid.nextForce); 
+		//neighborObjects[i].phSolid.dv.v() += accel * dt;
+		//neighborObjects[i].phSolid.dv.w() += angaccel * dt;
+		//cout << neighborObjects[i].phSolid.dv.v() << endl;
+		Vec3d vel = neighborObjects[i].phSolid.GetVelocity() + (minv * neighborObjects[i].phSolid.nextForce) * dt;
+		neighborObjects[i].phSolid.SetVelocity(vel);
+		neighborObjects[i].phSolid.SetCenterPosition(neighborObjects[i].phSolid.GetCenterPosition() + vel * dt);
+		neighborObjects[i].phSolid.SetUpdated(true);
+		neighborObjects[i].phSolid.Step();
+	}
+}
+
+void HapticProcess::ClearForce(){}
+
+// ベクトルのノルムを返す
+// ベクトルの方向はaからb
+double HapticProcess::CalcDistance(Vec3d a2, Vec3d b){
+	Vec3d c = b - a2;
+	Vec3d d = a2 - b;
+//	if(c.unit ==  )
+	return c.norm();
+}
+
+void HapticProcess::Keyboard(unsigned char key){
+	switch(key){
+		case 'f':
+			if(bDisplayforce){
+				bDisplayforce = false;
+				spidarG6.SetForce(Vec3d(0, 0, 0));
+				cout << "Force OFF" << endl;
+			}else{
+				bDisplayforce = true;
+				spidarG6.SetForce(Vec3d(0, 0, 0));
+				cout << "Force ON" << endl;
+			}
+			break;
+		case 'c':
+			spidarG6.SetForce(Vec3d(0, 0, 0));
+			spidarG6.Calib();
+			DSTR << "Calibration" << endl;
+			break;
+		default:
+			break;
+	}
+}
---
+++ src/tests/HIS/HapticConnect/main.cpp
@@ -0,0 +1,132 @@
+#include "BoxStack.h"
+#include "HapticProcess.h"
+#include <GL/glut.h>
+#include <iostream>
+#include <time.h>
+#include "Foundation/UTMMTimer.h"
+
+#ifdef USE_HDRSTOP
+#pragma hdrstop
+#endif
+
+#define ESC 27
+
+using namespace std;
+
+BoxStack bstack;
+HapticProcess hprocess;	
+UTMMTimer timer;
+bool bsync = false;
+int stepcount = 0;
+void _cdecl CallBackPhysiclaProcess();
+void AppStart();
+void CallBackHapticProcess(void* arg);
+void Synchronize();
+void _cdecl Keyboard(unsigned char key, int x, int y);
+
+void _cdecl CallBackPhysiclaProcess(){
+	while(bsync){
+		Sleep(1);
+	}
+	bstack.Display();
+	bsync = true;
+};
+
+void CallBackHapticProcess(void* arg){
+	hprocess.Step();
+	Synchronize();
+};
+
+void StartTimer(){
+	timer.Resolution(1);
+	timer.Interval(1);
+	timer.Set(CallBackHapticProcess, NULL);
+	timer.Create();
+};
+
+void AppStart(){
+	bstack.Start();
+	glutDisplayFunc(CallBackPhysiclaProcess);
+	glutKeyboardFunc(Keyboard);
+	glutMainLoop();
+};
+
+void Synchronize(){
+	if(bsync){
+		//static int count;
+		//count++;
+		//DSTR << "count" << count << endl;
+		//DSTR << bstack.phscene->GetCount() << endl;
+		// 力覚ポインタの同期
+		bstack.phpointer = hprocess.hpointer;
+		bstack.UpdateHapticPointer();
+
+		// 力覚プロセス->物理プロセス
+		// 力覚プロセスでの近傍物体のシミュレーション結果を物理プロセスに反映させる
+		for(unsigned i = 0; i < hprocess.neighborObjects.size(); i++){
+			// blocalがtrueな場合は結果を反映させる
+			if(hprocess.neighborObjects[i].blocal){
+				hprocess.neighborObjects[i].phSolidIf->SetVelocity(hprocess.neighborObjects[i].phSolid.GetVelocity());
+				//hprocess.neighborObjects[i].phSolidIf->SetAngularVelocity(hprocess.neighborObjects[i].phSolid.GetAngularVelocity());
+				hprocess.neighborObjects[i].phSolidIf->SetCenterPosition(hprocess.neighborObjects[i].phSolid.GetCenterPosition());
+				//hprocess.neighborObjects[i].phSolidIf->SetOrientation(hprocess.neighborObjects[i].phSolid.GetOrientation());	
+			}
+			hprocess.neighborObjects[i].phSolidIf->SetDynamical(true);
+			hprocess.neighborObjects[i].phSolidIf->SetFrozen(false);
+		}
+
+		// 物理プロセス->力覚プロセス
+		// シーンで新しく生成された剛体を格納
+		for(unsigned i = hprocess.neighborObjects.size(); i < bstack.neighborObjects.size(); i++){
+			hprocess.neighborObjects.resize(hprocess.neighborObjects.size() + 1);
+			hprocess.neighborObjects.back() = bstack.neighborObjects[i];
+		}
+		for(unsigned i = 0; i < hprocess.neighborObjects.size(); i++){
+			// 初めて取得した近傍物体のみ行う
+			if(bstack.neighborObjects[i].bfirstlocal){
+				hprocess.neighborObjects[i].phSolid = bstack.neighborObjects[i].phSolid;
+				bstack.neighborObjects[i].bfirstlocal = false;
+				hprocess.neighborObjects[i].phSolidIf->SetDynamical(false);
+				hprocess.neighborObjects[i].phSolidIf->SetFrozen(true);
+				hprocess.neighborObjects[i].phSolid.SetDynamical(true);
+				hprocess.neighborObjects[i].phSolid.SetFrozen(false);
+			}
+			hprocess.neighborObjects[i].closestPoint = bstack.neighborObjects[i].closestPoint;
+			hprocess.neighborObjects[i].pointerPoint = bstack.neighborObjects[i].pointerPoint;
+			hprocess.neighborObjects[i].direction = bstack.neighborObjects[i].direction;
+			hprocess.neighborObjects[i].blocal = bstack.neighborObjects[i].blocal;
+		}
+
+		// 物理プロセスで使用する刻み時間
+		bstack.phscene->SetTimeStep(0.001f * (float)stepcount);
+		stepcount = 0;
+
+		// 同期終了のフラグ
+		bsync = false;
+	}
+//	cout << stepcount << endl;
+	stepcount++;
+};
+
+void _cdecl Keyboard(unsigned char key, int x, int y){
+		switch (key) {
+		case ESC:		
+		case 'q':
+			timer.Release();
+			exit(0);
+			break;
+		default:
+			bstack.Keyboard(key);
+			hprocess.Keyboard(key);
+			break;
+	}
+};
+
+int _cdecl main(int argc, char* argv[]){
+	Sleep(100);
+	hprocess.Init();
+	bstack.Init(argc, argv);
+	StartTimer();
+	AppStart();
+	return 0;
+}
