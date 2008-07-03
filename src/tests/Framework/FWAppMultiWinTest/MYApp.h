9b7eec5f2bb62df77599bd0e1985f1da1bb1da5c
tokizaki <tokizaki@05cee5c3-a2e9-0310-9523-9dfc2f93dbe1>
2008-07-03 05:23:27 +0000

    git-svn-id: http://springhead.info/spr2/Springhead2/trunk@3413 05cee5c3-a2e9-0310-9523-9dfc2f93dbe1

--- src/tests/Framework/FWAppMultiWinTest/MYApp.h
+++ src/tests/Framework/FWAppMultiWinTest/MYApp.h
@@ -4,23 +4,25 @@
 #include <Framework/SprFWAppGLUT.h>
 #include <Springhead.h>
 #include <cstdlib>
+#include <vector>
 
 using namespace Spr;
+using namespace std;
 
 class MYApp : public FWAppGLUT, public UTRefCount{
 	
 private:
-	double			dt;					//< 積分幅
-	double			nIter;				//< LCP反復回数
+	double					dt;				//< 積分幅
+	double					nIter;			//< LCP反復回数
 
-	int				numWindow;			//< ウィンドウの数
-	FWWin*			window[2];			//< 作ったウィンドウ[0]:上画面，[1]:下画面
-	FWSceneIf*		fwScene[2];			//< Physicsのシーングラフ[0]:上画面，[1]:下画面
-	const char*		winName[2];			//< Windowの名前[0]:上画面，[1]:下画面
+	int						numWindow;		//< ウィンドウの数
+	vector< FWWin* >		windows;		//< 作ったウィンドウ[0]:上画面，[1]:下画面
+	vector< FWSceneIf* >	fwScenes;		//< Physicsのシーングラフ[0]:上画面，[1]:下画面
+	vector< string >		winNames;		//< Windowの名前[0]:上画面，[1]:下画面
 
-	float			camAngle[2];		//< カメラの角度[0]:上画面，[1]:下画面
-	float			camZoom[2];			//< カメラの倍率[0]:上画面，[1]:下画面
-	Affinef			view[2];			//< カメラ位置の回転・ズームを行う時の行列用の変数[0]:上画面，[1]:下画面
+	vector< float >			camAngles;		//< カメラの角度[0]:上画面，[1]:下画面
+	vector< float >			camZooms;		//< カメラの倍率[0]:上画面，[1]:下画面
+	vector< Affinef	>		views;			//< カメラ位置の回転・ズームを行う時の行列用の変数[0]:上画面，[1]:下画面
 
 
 public:
@@ -29,7 +31,7 @@ public:
 	MYApp();
 
 	// このクラス内で新しく宣言する関数
-	void MyRenderInit(FWWin* window, int winNumber);
+	void MyRenderInit(FWWin* window, int num);
 
 	// インタフェース関数
 	double	GetTimeStep()					{return dt;}
@@ -40,7 +42,7 @@ public:
 	// 基底のクラスで宣言された関数のうちオーバーロードする関数
 	void Init(int argc, char* argv[]);
 	void Keyboard(int key, int x, int y);
-	//void Display();
+	void Display();
 	
 };
 
