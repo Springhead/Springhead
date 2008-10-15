08fc59e49a75dabbcd2896e8cd7b6fa579a3c05b
tokizaki <tokizaki@05cee5c3-a2e9-0310-9523-9dfc2f93dbe1>
2008-10-15 07:42:16 +0000

    git-svn-id: http://springhead.info/spr2/Springhead2/trunk@3561 05cee5c3-a2e9-0310-9523-9dfc2f93dbe1

--- src/Base/TQuaternion.h
+++ src/Base/TQuaternion.h
@@ -364,14 +364,14 @@ inline T1 dot(const TQuaternion<T1>& q1, const TQuaternion<T2>& q2) {
 /// 回転操作をせず，普通にQuaternionとベクトルの積を求める
 template<class ET, class BD>
 inline TQuaternion<ET> mult(const TQuaternion<ET>& q, const PTM::TVectorBase<3, BD>& v){
-	TQuaternion<ET> qv(0, ET(v[0]), ET(v[1]), ET (v[2]));
+	TQuaternion<ET> qv(1, ET(v[0]), ET(v[1]), ET (v[2]));
 	return q*qv;
 }
 
 /// 回転操作をせず，普通にベクトルとQuaternionの積を求める
 template<class BD, class ET>
 inline TQuaternion<ET> mult(const PTM::TVectorBase<3, BD>& v, const TQuaternion<ET>& q){
-	TQuaternion<ET> qv(0, ET(v[0]), ET(v[1]), ET (v[2]));
+	TQuaternion<ET> qv(1, ET(v[0]), ET(v[1]), ET (v[2]));
 	return qv*q;
 }
 
