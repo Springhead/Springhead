5b8ba394b3c0fbf8d6bf255a5952c8b07d127169
tokizaki <tokizaki@05cee5c3-a2e9-0310-9523-9dfc2f93dbe1>
2008-10-18 10:27:45 +0000

    SetMotorTorqueが物理シミュレーションに反映されていないバグを修正いたしました．
    git-svn-id: http://springhead.info/spr2/Springhead2/trunk@3575 05cee5c3-a2e9-0310-9523-9dfc2f93dbe1

--- src/Physics/PHHingeJoint.cpp
+++ src/Physics/PHHingeJoint.cpp
@@ -49,13 +49,13 @@ void PHHingeJoint::CompBias(){
 		db *= engine->velCorrectionRate;
 	}
 
-	if(mode == MODE_TRAJECTORY_TRACKING){
-		preQd		= qd;
-		qd			= origin;
-		preQdDot	= desiredVelocity;
-		qdDot		= (qd - preQd) / GetScene()->GetTimeStep();
-		qdWDot		= (qdDot - preQdDot) / GetScene()->GetTimeStep();
-	}
+	//if(mode == MODE_TRAJECTORY_TRACKING){
+	//	preQd		= qd;
+	//	qd			= origin;
+	//	preQdDot	= desiredVelocity;
+	//	qdDot		= (qd - preQd) / GetScene()->GetTimeStep();
+	//	qdWDot		= (qdDot - preQdDot) / GetScene()->GetTimeStep();
+	//}
 	if(mode == MODE_VELOCITY){
 		db.w().z = -desiredVelocity;
 	}else if(spring != 0.0 || damper != 0.0){
@@ -85,14 +85,14 @@ void PHHingeJoint::CompBias(){
 			double tmp = 1.0 / (damper_ + spring * GetScene()->GetTimeStep());
 			dA.w().z = tmp * dtinv;
 			//‹O“¹’Ç]§Œä‚ÌLCP‚ÍˆÈ‰º‚Ì‚æ‚¤‚É‚È‚é
-			if(mode == MODE_TRAJECTORY_TRACKING){
+			/*if(mode == MODE_TRAJECTORY_TRACKING){
 				db.w().z = - tmp * (spring * (qd - GetPosition())
 						 + (solid[0]->GetInertia()[2][2] * qdWDot)
 						 + (solid[1]->GetInertia()[2][2] * -qdWDot)
 						 + (damper_ * -qdDot));
-			}
+			}*/
 			// •’Ê‚ÌˆÊ’u§Œä‚Ìê‡
-			else db.w().z = tmp * (spring*diff + springLim*diffLim);
+			db.w().z = tmp * (spring*diff + springLim*diffLim);
 		}
 	}
 }
