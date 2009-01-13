36fdd3a6648fc63438dfa00f7dfbf77ccde13df0
matunaga <matunaga@05cee5c3-a2e9-0310-9523-9dfc2f93dbe1>
2009-01-13 03:21:26 +0000

    3要素モデルに降伏応力と硬さの倍率に関する係数を追加
    git-svn-id: http://springhead.info/spr2/Springhead2/trunk@3655 05cee5c3-a2e9-0310-9523-9dfc2f93dbe1

--- src/Physics/PH3Element.cpp
+++ src/Physics/PH3Element.cpp
@@ -17,6 +17,9 @@ namespace Spr{;
 
 PH3ElementDesc::PH3ElementDesc(){
 	springOri = damperOri = 0.0;
+	secondDamper		  = Vec3d(0.0, 0.0, 0.0);
+	yieldStress			  = 0.0;			// 降伏応力
+	hardnessRate		  = 1.0;		// 降伏応力以下の場合に二個目のダンパ係数に掛ける比率
 }
 //----------------------------------------------------------------------------
 // PH3Element
@@ -33,6 +36,8 @@ void PH3Element::SetDesc(const void* desc){
 	secondDamper = desc3Element.secondDamper;
 	springOri = desc3Element.springOri;
 	damperOri = desc3Element.damperOri;
+	yieldStress	= desc3Element.yieldStress;			// 降伏応力
+	hardnessRate = desc3Element.hardnessRate;		// 降伏応力以下の場合に二個目のダンパ係数に掛ける比率
 }
 
 void PH3Element::SetConstrainedIndex(bool* con){
@@ -51,54 +56,56 @@ void PH3Element::CompBias(){
 	Vec3d D2 = secondDamper;
 	double h = GetScene()->GetTimeStep();
 
-	//3要素モデル	
-	ws=vjrel;
-	//cout<<fabs(f.v().y)<<endl;
-	//cout<<Xjrel.r.y<<endl;
-	cout<<"Xjrel"<<Xjrel.r.y<<endl;
-	cout<<"xss"<<xss[0].v().y<<endl;
-	cout<<"Xjrel-xss"<<Xjrel.r.y-xss[0].v().y<<endl;
-	cout<<"xs"<<xs[0].v().y<<endl;
-	cout<<"xd"<<xd[0].v().y<<endl;
-
-	if(f.v().y<-30){
-		for(int i = 0; i < 3; i++){
-			if (!constr[i]) continue;
-			//ばねの長さを更新
-			tmp = D2[i]-D1[i]+K[i]*h;
-			tmp2=D2[i]*K[i]*h*(2*D1[i]-D2[i])*(D2[i]-D1[i])-tmp*D1[i]*D2[i];
-
-			xs[1][i] = ((D2[i]-D1[i])/tmp)*xs[0][i] + (D2[i]*h/tmp)*ws[i];		
-			dA[i] = -tmp*(D2[i]-D1[i])/tmp2* dtinv;
-			db[i] = K[i]*(2*D1[i]-D2[i])*(D2[i]-D1[i])/tmp2*xs[0][i];
-		}
-	xd[0].v()=Xjrel.r-xss[0].v()-xs[0].v();
-	xs[0]=xs[1];
+	//降伏応力以下ではジョイントを固くする
+	if(f.v().norm()>=yieldStress ){
+		K = spring;
+		D1 = damper;
+		D2=secondDamper;
 	}else{
-		for(int i = 0; i < 3; i++){
-			if (!constr[i]) continue;
-			tmp = 1.0 / (damper[i] + spring[i] * GetScene()->GetTimeStep());
-			dA[i] = tmp * dtinv;
-			db[i] = spring[i] *( Xjrel.r[i] -xd[0].v()[i])* tmp;
-		}
-		xss[0].v()=Xjrel.r;
+		K = spring;
+		D1 = damper;
+		D2= secondDamper*hardnessRate;
 	}
 
+	//3要素モデル	
+	ws=vjrel;
+	for(int i = 0; i < 3; i++){
+		if (!constr[i]) continue;
+		//ばねの長さを更新
+		tmp = D2[i]-D1[i]+K[i]*h;
+		tmp2=D2[i]*K[i]*h*(2*D1[i]-D2[i])*(D2[i]-D1[i])-tmp*D1[i]*D2[i];
 
-	////3要素モデル	
+		xs[1][i] = ((D2[i]-D1[i])/tmp)*xs[0][i] + (D2[i]*h/tmp)*ws[i];		
+		dA[i] = -tmp*(D2[i]-D1[i])/tmp2* dtinv;
+		db[i] = K[i]*(2*D1[i]-D2[i])*(D2[i]-D1[i])/tmp2*xs[0][i];
+	}
+	xs[0]=xs[1];
+
+
+	////3要素モデルとフォークトモデルの切り替え方式	
 	//ws=vjrel;
-	//for(int i = 0; i < 3; i++){
-	//	if (!constr[i]) continue;
-	//	//ばねの長さを更新
-	//	tmp = D2[i]-D1[i]+K[i]*h;
-	//	tmp2=D2[i]*K[i]*h*(2*D1[i]-D2[i])*(D2[i]-D1[i])-tmp*D1[i]*D2[i];
+	//if(f.v().y<-30){
+	//	for(int i = 0; i < 3; i++){
+	//		if (!constr[i]) continue;
+	//		//ばねの長さを更新
+	//		tmp = D2[i]-D1[i]+K[i]*h;
+	//		tmp2=D2[i]*K[i]*h*(2*D1[i]-D2[i])*(D2[i]-D1[i])-tmp*D1[i]*D2[i];
 
-	//	xs[1][i] = ((D2[i]-D1[i])/tmp)*xs[0][i] + (D2[i]*h/tmp)*ws[i];		
-	//	dA[i] = -tmp*(D2[i]-D1[i])/tmp2* dtinv;
-	//	db[i] = K[i]*(2*D1[i]-D2[i])*(D2[i]-D1[i])/tmp2*xs[0][i];
-	//}
+	//		xs[1][i] = ((D2[i]-D1[i])/tmp)*xs[0][i] + (D2[i]*h/tmp)*ws[i];		
+	//		dA[i] = -tmp*(D2[i]-D1[i])/tmp2* dtinv;
+	//		db[i] = K[i]*(2*D1[i]-D2[i])*(D2[i]-D1[i])/tmp2*xs[0][i];
+	//	}
+	//xd[0].v()=Xjrel.r-xss[0].v()-xs[0].v();
 	//xs[0]=xs[1];
-
+	//}else{
+	//	for(int i = 0; i < 3; i++){
+	//		if (!constr[i]) continue;
+	//		tmp = 1.0 / (damper[i] + spring[i] * GetScene()->GetTimeStep());
+	//		dA[i] = tmp * dtinv;
+	//		db[i] = spring[i] *( Xjrel.r[i] -xd[0].v()[i])* tmp;
+	//	}
+	//	xss[0].v()=Xjrel.r;
+	//}
 
 	//if(secondDamper[0]!=0.0||secondDamper[1]!=0.0||secondDamper[2]!=0.0){
 	//	//3要素モデル
