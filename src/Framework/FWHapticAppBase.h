b3b74114a2b8d4378ade82f75e2eeeba1d6affcc
susa <susa@05cee5c3-a2e9-0310-9523-9dfc2f93dbe1>
2009-02-10 15:26:19 +0000

    git-svn-id: http://springhead.info/spr2/Springhead2/trunk@3713 05cee5c3-a2e9-0310-9523-9dfc2f93dbe1

--- src/Framework/FWHapticAppBase.h
+++ src/Framework/FWHapticAppBase.h
@@ -11,27 +11,48 @@
 
 #include <Springhead.h>
 #include <Framework/SprFWAppGLUT.h>
+#include <Framework/FWExpandedPHSolid.h>
+#include <vector>
 
 namespace Spr{;
 
-class FWHapticAppBase : public FWAppGLUT, public UTRefCount{
-public:
 
+class FWHapticAppBase : public FWAppGLUT, public UTRefCount{
+protected:
+	// 変数
+	std::vector<PHSolidIf*> hapticPointers;
+//	std::vector<ExpandedPHSolid> eSolids;
+	std::vector<ExpandedPHSolid> expandedPHSolids;
+	// フラグ
+	bool bDebug;
+public:
+	// 
 	FWHapticAppBase();
-	virtual void Init(int argc, char* argv[]);
-	virtual void InitCameraView();
-	virtual void ResetScene();
-	virtual void BuildScene();
-	virtual void CallBack();
-	virtual void Start();
-	virtual void Idle();
-	virtual void Step();
-	virtual void Display();
-	virtual void SyncHapticProcess();
-	virtual void ExpandPHSolidInfo();
-	virtual void FindNearestObjectFromHapticPointer();
-	virtual void PredictSimulation();
-	virtual void Keyboard(int key, int x, int y);
+	virtual void Init(int argc, char* argv[]);							///< 初期化，はじめにこれを呼ぶ
+	virtual void InitCameraView();										///< 初期カメラ位置
+	virtual void ResetScene() = 0;										///< シーンをリセット
+	virtual void BuildScene() = 0;										///< シーンをビルド
+	virtual void CallBack() = 0;											///< MultiMediaTimerが呼ぶコールバック関数
+	virtual void Start();														///< アプリケーション開始
+	virtual void Idle() = 0;													///< glutIdelFuncが呼ぶ関数
+	virtual void Step() = 0;												///< シミュレーションステップ
+	virtual void Display();													///< 描画関数(glutPostRedisplay()で呼ばれる
+	virtual void DebugDisplay(GRDebugRenderIf* render);		///< デバック表示にしたときに呼ばれる関数
+	virtual void SyncHapticProcess() = 0;								///< HapticProcessと同期する関数
+	virtual void ExpandPHSolidInfo() = 0;							///< シーンが持つPHSolidに力覚提示に必要な情報を付加する
+	virtual void FindNearestObjectFromHapticPointer() = 0;	///< 力覚ポインタ近傍の物体を見つける
+//	virtual void TestSimulation();											///< テストシミュレーション
+	virtual void Keyboard(int key, int x, int y) = 0;					///< glutKeyboarcFunc()が呼ぶ関数
+
+	// protected変数へのアクセス
+	void AddHapticPointer(PHSolidIf* ps);
+	std::vector<PHSolidIf*> GetHapticPointers();
+	void AddExpandedPHSolids(ExpandedPHSolid es);
+	std::vector<ExpandedPHSolid>* GetExpandedPHSolids();
+
+	// フラグを切り替えるための関数
+	void SetDebugMode(bool bD);
+	bool GetDebugMode();
 };
 
 }
--- src/Framework/FWMultiRateHapticApp.h
+++ src/Framework/FWMultiRateHapticApp.h
@@ -1,12 +1,18 @@
 #ifndef FWMULTIRATE_HAPTIC_APP_H
 #define FWMULTIRATE_HAPTIC_APP_H
 
-#include <Framework/FWHapticAppBase>
+#include <Framework/FWHapticAppBase.h>
 
 namespace Spr{;
 class FWMultiRateHapticApp : public FWHapticAppBase{
 public:
 	FWMultiRateHapticApp();
+	virtual void InitCameraView();
+	virtual void Idle();
+	virtual void SyncHapticProcess();
+	virtual void ExpandPHSolidInfo();
+	virtual void FindNearestObjectFromHapticPointer();
 };
 
 }
+#endif
