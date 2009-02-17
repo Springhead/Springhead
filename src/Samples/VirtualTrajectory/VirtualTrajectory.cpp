83dc00447d7fc6361b71f19df3e1815662b17752
tokizaki <tokizaki@05cee5c3-a2e9-0310-9523-9dfc2f93dbe1>
2009-02-17 12:43:58 +0000

    git-svn-id: http://springhead.info/spr2/Springhead2/trunk@3735 05cee5c3-a2e9-0310-9523-9dfc2f93dbe1

--- src/Samples/VirtualTrajectory/VirtualTrajectory.cpp
+++ src/Samples/VirtualTrajectory/VirtualTrajectory.cpp
@@ -14,7 +14,9 @@ int quitCount = 0;
 
 PHSceneIf* phScene;
 PHBallJointIf* ballJoint;
+PHHingeJointIf* hingeJoint;
 PHSolidIf* bar;
+PHSolidIf* flr;
 PHSolidIf* ball;
 
 UTRef<ObjectStatesIf> state = ObjectStatesIf::Create();
@@ -22,38 +24,77 @@ UTRef<ObjectStatesIf> state = ObjectStatesIf::Create();
 class MyApp: public FWAppGLUT{
 protected:
 	bool bRun;
+	
+
 public:
-	MyApp():bRun(true){
+	bool isBall;
+
+	MyApp():bRun(true), isBall(true){
+	}
+	void CalcBallJoint(){			//	トルクの計算
+		state->SaveState(phScene);
+		phScene->SetContactMode(PHSceneDesc::MODE_NONE);
+		ballJoint->SetMode(PHJointDesc::MODE_VELOCITY);
+		Vec3d v(0,0,10);
+		ballJoint->SetDesiredVelocity(v);
+		phScene->Step();
+		Vec3d f,t;
+		ballJoint->GetConstraintForce(f, t);
+		state->LoadState(phScene);
+
+		//	実シミュレーション
+		phScene->SetContactMode(PHSceneDesc::MODE_LCP);
+		ballJoint->SetMode(PHJointDesc::MODE_POSITION);
+		static Quaterniond q;
+		q = Quaterniond::Rot(v * phScene->GetTimeStep()) * q;
+		ballJoint->SetGoal(q);
+		ballJoint->SetDesiredVelocity(v);
+		ballJoint->SetOffsetForce(t);
+	}
+	void CalcHingeJoint(){			//	トルクの計算
+		state->SaveState(phScene);
+		phScene->SetContactMode(PHSceneDesc::MODE_NONE);
+		hingeJoint->SetMode(PHJointDesc::MODE_VELOCITY);
+		double v = 10;
+		hingeJoint->SetDesiredVelocity(v);
+		phScene->Step();
+		Vec3d f,t;
+		hingeJoint->GetConstraintForce(f, t);
+		Vec3d w;
+		hingeJoint->GetRelativeVelocity(f, w);
+		DSTR << hingeJoint->GetVelocity() << ", " << t << std::endl;
+		state->LoadState(phScene);
+
+		//	実シミュレーション
+		phScene->SetContactMode(PHSceneDesc::MODE_LCP);
+		hingeJoint->SetMode(PHJointDesc::MODE_POSITION);
+		static double ori = 0;
+		ori = v * phScene->GetTimeStep() + ori;
+		hingeJoint->SetSpringOrigin(ori);
+		hingeJoint->SetDesiredVelocity(v);
+		hingeJoint->SetOffsetForce(t.Z());
 	}
 	void Step(){
 		if (bRun){
-			//	トルクの計算
-			state->SaveState(phScene);
-			phScene->SetContactMode(PHSceneDesc::MODE_NONE);
-			ballJoint->SetMode(PHJointDesc::MODE_VELOCITY);
-			Vec3d v(0,0,10);
-			ballJoint->SetDesiredVelocity(v);
-			phScene->Step();
-			Vec3d f,t;
-			ballJoint->GetConstraintForce(f, t);
-			state->LoadState(phScene);
 
-			//	実シミュレーション
-			phScene->SetContactMode(PHSceneDesc::MODE_LCP);
-			ballJoint->SetMode(PHJointDesc::MODE_POSITION);
-			static Quaterniond q;
-			q = Quaterniond::Rot(v * phScene->GetTimeStep()) * q;
-			ballJoint->SetGoal(q);
-			ballJoint->SetDesiredVelocity(v);
-			ballJoint->SetOffsetForce(t);
-			phScene->Step();
-
-
-			if(ballJoint->GetPosition().Rotation().norm() < 0.3){
-				ball->SetFramePosition(Vec3d(0.2, 0.6 ,0));
-				ball->SetVelocity(Vec3d());
+			if(isBall){
+				CalcBallJoint();
+			}else{
+				CalcHingeJoint();
 			}
+			phScene->Step();
 
+			if(isBall){
+				if(ballJoint->GetPosition().Rotation().norm() < 0.3){
+					ball->SetFramePosition(Vec3d(0.2, 0.6 ,0));
+					ball->SetVelocity(Vec3d());
+				}
+			}else{
+				if(hingeJoint->GetPosition() < 0.3){
+					ball->SetFramePosition(Vec3d(0.2, 0.6 ,0));
+					ball->SetVelocity(Vec3d());
+				}
+			}
 			SetCurrentWin(GetWin(0));
 			glutPostRedisplay();
 			if (GetWin(1)){
@@ -72,14 +113,24 @@ public:
 		static double stiff = 1.0;
 		if (key == 's'){
 			stiff /= 2;
-			ballJoint->SetSpring(stiff*10);
-			ballJoint->SetDamper(stiff);
+			if(isBall){
+				ballJoint->SetSpring(stiff*10);
+				ballJoint->SetDamper(stiff);
+			}else {
+				hingeJoint->SetSpring(stiff*10);
+				hingeJoint->SetDamper(stiff);
+			}
 			DSTR << "stiff:" << stiff << std::endl;
 		}
 		if (key == 'h'){
 			stiff *= 2;
-			ballJoint->SetSpring(stiff*10);
-			ballJoint->SetDamper(stiff);
+			if(isBall){
+				ballJoint->SetSpring(stiff*10);
+				ballJoint->SetDamper(stiff);
+			}else {
+				hingeJoint->SetSpring(stiff*10);
+				hingeJoint->SetDamper(stiff);
+			}
 			DSTR << "stiff:" << stiff << std::endl;
 		}
 		if (key=='p'){
@@ -113,22 +164,40 @@ public:
 		FWAppGLUT::Display();
 	}
 };
+void SetUpBallJoint(){
+	PHBallJointDesc jdesc;
+	//jdesc.limit[1].upper =  0.2;	// 最大スイング角
+	//jdesc.limit[2].lower= -0.2;	// ツイスト角範囲
+	//jdesc.limit[2].upper =  0.2;
+	jdesc.poseSocket.Pos() = Vec3d(0, 0.2, 0);
+	jdesc.posePlug.Pos() = Vec3d(0, -0.2, 0);
+	ballJoint = phScene->CreateJoint(flr, bar, PHBallJointIf::GetIfInfoStatic(), jdesc)->Cast();
+}
 
+void SetUpHingeJoint(){
+	PHHingeJointDesc jdesc;
+	jdesc.spring = 0;
+	jdesc.damper = 0;
+	jdesc.poseSocket.Pos() = Vec3d(0, 0.2, 0);
+	jdesc.posePlug.Pos() = Vec3d(0, -0.2, 0);
+	hingeJoint = phScene->CreateJoint(flr, bar, jdesc)->Cast();
+}
 
 int SPR_CDECL main(int argc, char* argv[]){
 	app = new MyApp;
+	app->isBall = false;
 	app->Init(argc, argv);
 	app->GetSdk()->Clear();	//	SDK全体を初期化
 
 	//	シーンの構築
 	app->GetSdk()->CreateScene(PHSceneDesc(), GRSceneDesc());
 	phScene = app->GetSdk()->GetScene()->GetPHScene();
-	PHSolidIf* floor = phScene->CreateSolid(PHSolidDesc());
-	floor->SetDynamical(false);
+	flr = phScene->CreateSolid(PHSolidDesc());
+	flr->SetDynamical(false);
 	bar = phScene->CreateSolid(PHSolidDesc());
 	CDBoxDesc boxdesc;
 	boxdesc.boxsize = Vec3d(0.1, 0.3, 0.1);
-//	floor->AddShape(app->GetSdk()->GetPHSdk()->CreateShape(boxdesc));
+//	flr->AddShape(app->GetSdk()->GetPHSdk()->CreateShape(boxdesc));
 	bar->AddShape(app->GetSdk()->GetPHSdk()->CreateShape(boxdesc));
 	
 	ball = phScene->CreateSolid(PHSolidDesc());
@@ -137,13 +206,11 @@ int SPR_CDECL main(int argc, char* argv[]){
 	sd.radius = 0.05;
 	ball->AddShape(app->GetSdk()->GetPHSdk()->CreateShape(sd));
 
-	PHBallJointDesc jdesc;
-	//jdesc.limit[1].upper =  0.2;	// 最大スイング角
-	//jdesc.limit[2].lower= -0.2;	// ツイスト角範囲
-	//jdesc.limit[2].upper =  0.2;
-	jdesc.poseSocket.Pos() = Vec3d(0, 0.2, 0);
-	jdesc.posePlug.Pos() = Vec3d(0, -0.2, 0);
-	ballJoint = phScene->CreateJoint(floor, bar, PHBallJointIf::GetIfInfoStatic(), jdesc)->Cast();
+	if(app->isBall){
+		SetUpBallJoint();
+	}else{
+		SetUpHingeJoint();
+	}
 
 	//	ウィンドウ1を作成
 	FWAppGLUTDesc wd;
@@ -152,3 +219,5 @@ int SPR_CDECL main(int argc, char* argv[]){
 	app->Start();
 	return 0;
 }
+
+
