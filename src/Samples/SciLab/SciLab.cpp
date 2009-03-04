70f3fa29e846b80c6d323e8265d63713c293f8f6
hase <hase@05cee5c3-a2e9-0310-9523-9dfc2f93dbe1>
2009-03-04 03:03:42 +0000

    ScilabAPIの整理＆サンプルの整理
    git-svn-id: http://springhead.info/spr2/Springhead2/trunk@3741 05cee5c3-a2e9-0310-9523-9dfc2f93dbe1

--- src/Samples/SciLab/SciLab.cpp
+++ src/Samples/SciLab/SciLab.cpp
@@ -1,78 +1,40 @@
 #include <SciLab/SprSciLab.h>
 #include <Base/Affine.h>
 using namespace Spr;
-using namespace Scilab;
-
-bool WriteMatrix(char* n, int* mA, int* nA, double* A){
-	return cwritemat_(n, mA, nA, A, (long)strlen(n)) != 0;
-}
-double* GetMatrixptr(char* s, int* m, int* n, int* lp){
-	cmatptr_(s, m, n, lp, (long)strlen(s));
-	return stk(*lp);
-}
-bool ReadMatrix(char* s, int* m, int* n, double* buf){
-	return creadmat_(s, m, n, buf, (long)strlen(s)) != 0;
-}
-
 
 int main(){
-#ifdef WIN32
-	if ( StartScilab(NULL,NULL,NULL) == false ) DSTR << "Error : StartScilab \n";
-#else
-    if ( StartScilab(SCI,NULL,NULL) == false ) DSTR << "Error : StartScilab \n";
-#endif
+	if (!ScilabStart()) std::cout << "Error : ScilabStart \n";
+	Matrix2f A;
+	A.Ex() = Vec2f(1,2);
+	A.Ey() = Vec2f(3,4);
+	std::cout << "A:  " << A ;
+	std::cout << "A00:" << A[0][0] << std::endl;
+	std::cout << "A01:" << A[0][1] << std::endl;
+	std::cout << "A10:" << A[1][0] << std::endl;
+	std::cout << "A11:" << A[1][1] << std::endl;
+	
+	ScilabSetMatrix("A", A);
+	ScilabJob("b=[4;5]");
+	std::cout << "A=";
+	ScilabJob("disp(A);");
+	std::cout << "b=";
+	ScilabJob("disp(b);");
+	std::cout << "x=A\\b" << std::endl;
+	ScilabJob("A,b,x=A\\b;");
+	
+	ScilabGetMatrix(A, "A");
+	SCMatrix b = ScilabMatrix("b");
+	SCMatrix x = ScilabMatrix("x");
+	std::cout << "x:" << x << std::endl;
 
-	static double A[]={1,2,3,4};  int mA=2,nA=2;
-	static double b[]={4,5};  int mb=2,nb=1;
-
-
-	/* Create Scilab matrices A and b */
-	WriteMatrix("A", &mA, &nA, A);
-	WriteMatrix("b", &mb, &nb, b);
-
-	SendScilabJob("disp(''A='');");
-	SendScilabJob("disp(A);");
-	SendScilabJob("disp(''b='');");
-	SendScilabJob("disp(b);");
-	SendScilabJob("disp(''x=A\\b'');");
-
-	if ( SendScilabJob("A,b,x=A\\b;") != 0) 
-	{
-		fprintf(stdout,"Error occured during scilab execution (SendScilabJob)\n");
-	}
-	else 
-	{
-		double *cxtmp=NULL;
-		int m,n,lp,i;
-
-		/* Get m and n */
-		GetMatrixptr("x", &m, &n, &lp);
-
-		cxtmp=(double*)malloc((m*n)*sizeof(double));
-
-		ReadMatrix("x", &m, &n, cxtmp);
-
-		for(i=0;i<m*n;i++)
-		{
-			fprintf(stdout,"x[%d] = %5.2f\n",i,cxtmp[i]);
-		}
-
-		if (cxtmp) 
-		{
-			free(cxtmp);
-			cxtmp=NULL;
-		}
-	}
-	{
-		SCMatrix A = ScilabMatrix("A");
-		SCMatrix b = ScilabMatrix("b");
-		SCMatrix x = ScilabMatrix("x");
-		std::cout << A << x << " = " << b;
-
-		SendScilabJob("y=A+A;");
-		SCMatrix y = ScilabMatrix("y");
-		std::cout << "y=" << y;
-		A = y;
-		SendScilabJob("disp(A);");
-	}
+	ScilabJob("y = A;");
+	SCMatrix y = ScilabMatrix("y");
+	std::cout << "y=" << y;
+	y = 2*A;
+	std::cout << "y = 2*A is done by C++ code" << std::endl;
+	std::cout << "y=";
+	ScilabJob("disp(y);");
+	ScilabEnd();
+	std::cout << A;
+	std::cout << y;
 }
