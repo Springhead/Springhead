45e083e80ec17c27d7c118e169b129df06d860f9
matunaga <matunaga@05cee5c3-a2e9-0310-9523-9dfc2f93dbe1>
2009-03-11 03:58:48 +0000

    ボーン付きXファイルによるPHBoneの自動生成を枝別れのないボーンにおいて行えるようになった
    git-svn-id: http://springhead.info/spr2/Springhead2/trunk@3747 05cee5c3-a2e9-0310-9523-9dfc2f93dbe1

--- src/Framework/FWObject.cpp
+++ src/Framework/FWObject.cpp
@@ -27,26 +27,42 @@ void FWObject::Sync(){
 				//ボーン１　（並行行列のみのアフィン行列更新）
 				Affinef af;
 				Matrix3d matrix;
-				//phSolid->GetPose().ToAffine(af);
-				//af.Rot()=matrix;									//回転成分の初期化
+				phSolid->GetPose().ToAffine(af);
+				af.Rot()=matrix;									//回転成分の初期化
 				//af.PosZ()-=solidLength/2;							//剛体中心の位置から剛体の半長分だけずらし，ジョイント部分の位置にする
 				DCAST(GRFrame, grFrame)->SetTransform(af);
 			}else if(bonePositionFlag==2){
 				//ボーン２　（回転行列のみのアフィン行列更新）
-				Affinef af,afParent,afd;
+				Affinef af,afParent,afd,afl,AF;
+				phSolid->GetPose().ToAffine(af);
+				afParent=grFrame->GetParent()->GetWorldTransform();
+					/*DSTR<<"phSolid"<<std::endl<<af<<std::endl;
+					DSTR<<"afParent"<<std::endl<<afParent<<std::endl;*/
+				afd=afParent.inv()*af;
+					DSTR<<"afd"<<std::endl<<afd<<std::endl;
+				afl.PosZ()+=solidLength/2;							//剛体中心の位置から剛体の半長分だけずらし，ジョイント部分の位置にする
+				AF=afd*afl;
+					//DSTR<<"solidLength"<<std::endl<<solidLength/2<<std::endl;
+					//DSTR<<"afdz"<<std::endl<<afd<<std::endl;
+				DCAST(GRFrame, grFrame)->SetTransform(AF);
+			}else if(bonePositionFlag==3){
+				//ボーン３
+				Affinef af,afParent,afd,afl,AF;
 				phSolid->GetPose().ToAffine(af);
 				afParent=grFrame->GetParent()->GetWorldTransform();
 				afd=afParent.inv()*af;
-				afd.PosZ()-=solidLength/2;
-				DCAST(GRFrame, grFrame)->SetTransform(afd);
+				afl.PosZ()+=solidLength/2;							//剛体中心の位置から剛体の半長分だけずらし，ジョイント部分の位置にする
+				AF=afd*afl;
+				DCAST(GRFrame, grFrame)->SetTransform(AF);
 			}else if(bonePositionFlag==0){
 				//ボーン１，２以外のボーン（アフィン行列の更新）
-				Affinef af,afParent,afd;
-				phSolid->GetPose().ToAffine(af);						
+				Affinef af,afParent,afd,afl,AF;
+				phSolid->GetPose().ToAffine(af);	
 				afParent=grFrame->GetParent()->GetWorldTransform();	//親のWorld座標からみたFrameを取得
 				afd=afParent.inv()*af;								//階層構造下のAffin行列に変換する
-				afd.PosZ()+=solidLength/2;							//剛体中心の位置から剛体の半長分だけずらし，ジョイント部分の位置にする
-				DCAST(GRFrame, grFrame)->SetTransform(afd);
+				afl.PosZ()+=solidLength/2;							//剛体中心の位置から剛体の半長分だけずらし，ジョイント部分の位置にする
+				AF=afd*afl;
+				DCAST(GRFrame, grFrame)->SetTransform(AF);
 			}
 		}else{
 			Affinef af;
