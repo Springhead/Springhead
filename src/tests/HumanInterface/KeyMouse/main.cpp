6c600199be80752763f90ad23bdd9639547f1d2c
hase <hase@05cee5c3-a2e9-0310-9523-9dfc2f93dbe1>
2009-04-14 12:53:44 +0000

    git-svn-id: http://springhead.info/spr2/Springhead2/trunk@3776 05cee5c3-a2e9-0310-9523-9dfc2f93dbe1

---
+++ src/tests/HumanInterface/KeyMouse/main.cpp
@@ -0,0 +1,28 @@
+#include <conio.h>
+#include <Springhead.h>
+#include <HumanInterface/SprHIKeyMouse.h>
+#include <iomanip>
+using namespace Spr;
+
+
+int __cdecl main(){
+	UTRef<HISdkIf> sdk = HISdkIf::CreateSdk();
+	sdk->AddRealDevice(DRKeyMouseWin32If::GetIfInfoStatic());
+
+	sdk->Init();
+	sdk->Print(DSTR);
+
+	DRKeyMouseWin32If* wif = sdk->FindRealDevice("KeyMouseWin32")->Cast();
+	wif->Update();
+	UTRef<HIMouse6DIf> mouse6D = sdk->CreateHumanInterface("HIMouse6D")->Cast();
+	mouse6D->Init(NULL);
+	for(int i=0; i<100000; ++i){
+		if (mouse6D->GetKeyMouse()->GetKeyState('Q') & DVKeyMouseIf::PRESS) return 0;
+		for(int i=0; i<200; ++i){
+			if (mouse6D->GetKeyMouse()->GetKeyState(i) & DVKeyMouseIf::PRESS){
+				std::cout << i << " '"<< (char)i << "' " << std::endl;
+			}
+		}
+		std::cout << "pose:" << mouse6D->GetPose() << std::endl;
+	}
+}
