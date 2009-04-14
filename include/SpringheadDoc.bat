309f5c85aeb634b699c2ec6ea20283798d7f9218
hase <hase@05cee5c3-a2e9-0310-9523-9dfc2f93dbe1>
2009-04-14 17:34:24 +0000

    git-svn-id: http://springhead.info/spr2/Springhead2/trunk@3778 05cee5c3-a2e9-0310-9523-9dfc2f93dbe1

--- include/SpringheadDoc.bat
+++ include/SpringheadDoc.bat
@@ -3,5 +3,3 @@ set PATHORG=%PATH%
 set PATH=%SPRINGHEAD2%\BIN;%SPRINGHEAD2%\BIN\Graphviz\bin;%PATH%
 doxygen springhead.doxy 2> springhead.log
 set PATH=%PATHORG%
-
-%call ..\src\Samples\SamplesDoc.bat
