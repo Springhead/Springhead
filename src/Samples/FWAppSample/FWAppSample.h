65f5b41ac48370a0f9f01b0f1d68949be98e3dac
susa <susa@05cee5c3-a2e9-0310-9523-9dfc2f93dbe1>
2009-04-16 05:37:09 +0000

    git-svn-id: http://springhead.info/spr2/Springhead2/trunk@3781 05cee5c3-a2e9-0310-9523-9dfc2f93dbe1

--- src/Samples/FWAppSample/FWAppSample.h
+++ src/Samples/FWAppSample/FWAppSample.h
@@ -9,12 +9,13 @@ using namespace Spr;								// Springheadの名前空間
 class FWAppSample : public FWAppGLUT, public UTRefCount{
 public:
 	FWAppSample();									// コンストラクタ（このクラスのインスタンスが生成された時に呼ばれる関数）
-	void Init(int argc, char* argv[]);				// GLUTの初期化
-	void InitCameraView();							// カメラ座標				
+	void Init(int argc, char* argv[]);			// GLUTの初期化
+	void InitCameraView();							// カメラ座標		
 	void BuildObject();								// 物体を作成
-	void Step();									// シミュレーションを1ステップ進める
+	void Step();										// シミュレーションを1ステップ進める
 	void Display();									// 描画
-	void Keyboard(int key, int x, int y);			// キーボード関数
+	void Reset();										// シーンのリセット
+	void Keyboard(int key, int x, int y);		// キーボード関数
 	bool bDrawInfo;									// 接触力などを表示するか
 }; 
 
