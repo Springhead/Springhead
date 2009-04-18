79b903f5c21b45bc00b64384ca57f53abb9110ff
hase <hase@05cee5c3-a2e9-0310-9523-9dfc2f93dbe1>
2009-04-18 13:20:13 +0000

    git-svn-id: http://springhead.info/spr2/Springhead2/trunk@3789 05cee5c3-a2e9-0310-9523-9dfc2f93dbe1

--- src/docsrc/01APIClassImp.h
+++ src/docsrc/01APIClassImp.h
@@ -6,8 +6,8 @@
 APIクラスの概要，使い方については，\ref pageApiBasic をご参照ください．
 
 \subsection secDefAPIClass APIクラスの宣言
-APIクラスのヘッダファイルは，Springhead2/include/SDKNAME に作ります．
-(SDKNAMEはSDKの名前 Graphics，Physicsなど)<br>
+APIクラスのヘッダファイルは，Springhead2/include/Sdk名 に作ります．
+(Sdk名はSDKの名前 Graphics，Physicsなど)<br>
 ヘッダファイルの名前は，Sprオブジェクト名.h (SprPHSolid.h など)とします．
 APIクラスを宣言するには，
 <pre>
@@ -26,7 +26,7 @@ APIクラスを宣言するには，
 をします．
 SPR_IFDEF()は，いくつかのメンバの宣言をまとめたものです．
 SPR_IFDEF()に対応する実装は，Springhead2/bin/swig/swig.exe が自動生成します．
-Springhead2/src/SDKNAME/SDKNAMEStub.cpp
+Springhead2/src/Sdk名/Sdk名Stub.cpp
 に，
 <pre>
 SPR_IFIMP?(クラス名, 基本クラス名);
@@ -61,7 +61,7 @@ APIクラスを作ったら，それを実装するクラスを作ります．
 この場合，SPR_OBJECTDEF1(cls, base); のように，マクロで直接基本クラスを指定します．
 
 SPR_OBJECTDEF()の実装も，SPR_IFDEF()の場合と同様に，
- Springhead2/src/SDKNAME/SDKNAMEStub.cpp に，
+ Springhead2/src/Sdk名/Sdk名Stub.cpp に，
 <pre>
 SPR_OBJECTIMP?(クラス名, 基本クラス名);
 </pre>
@@ -69,17 +69,18 @@ SPR_OBJECTIMP?(クラス名, 基本クラス名);
 Cast()メンバ関数，DCAST()マクロが利用する型情報もここに入ります．
 
 \subsubsection secGetAPI APIの取得
-Object::GetObjectIf() は thisをObjectIf*にキャストして返します．
-APIクラスは thisを
+インタフェースとオブジェクトを指すポインタは型が異なりますが，同じアドレスを指すポインタです．
+Object::GetObjectIf() は ObjectのthisポインタをObjectIf*にキャストして返します．
+APIクラスは thisポインタを
 <pre>
 size_t ObjectIf::NChildObject() const {
 	return ((Object*)this)->NChildObject();
 }
 </pre>
-のように，thisの型を実装クラスに戻して関数を呼び出しています．
+のように，thisポインタの型を実装クラスに戻して関数を呼び出しています．
 
 \subsection secImpAPIClass 宣言したAPIの実装
-まず，Springhead/src/SDKNAME/SceneObject.h で，
+まず，Springhead/src/Sdk名/SceneObject.h で，
 <pre>
 class SceenObject: public NamedObject{
 	SPR_OBJECTDEF(SceneObject);
@@ -87,7 +88,7 @@ class SceenObject: public NamedObject{
 };
 </pre>
 のように，関数を宣言し，
-まず，Springhead/src/SDKNAME/SceneObject.cpp で，
+まず，Springhead/src/Sdk名/SceneObject.cpp で，
 <pre>
  void SceneObject::SetScene(SceneIf* s){
  	SetNameManager(s->GetObj<NameManager>());
@@ -96,17 +97,16 @@ class SceenObject: public NamedObject{
 </pre>
 のように，宣言したAPIを実装します．
 APIクラスだけにあり，実装クラスにない関数があると，
-Springhead2/src/SDKNAME/SDKNAMEStub.cpp をコンパイルするときに，
+Springhead2/src/Sdk名/Sdk名Stub.cpp をコンパイルするときに，
 エラーになります．
 
 \section secFileLoadSave ファイルからのロード・ファイルへのセーブ
-FileIO SDK(\ref pageFileIO) でロード・セーブができるようにするためには，APIクラスの定義に
-若干の細工をする必要があります．
-FileIO SDK の詳細は，\ref pageFileIOImp を参照してください
+FileIO SDK(\ref pageFileIO) でロード・セーブができるようにするためには，
+APIクラスの定義に若干の細工をする必要があります．
+FileIO SDK の詳細は，\ref pageFileIOImp を参照してください．
 
-自動ロード・セーブ可能なオブジェクトを定義するには，
+\subsection secCreateLoadableObject ロード・セーブ可能なオブジェクトの作り方
 
-以下若干古い． SPR_DECLMEMBEROFクラス名に言及すべき
 <ol>
  <li>ロードしたいデータを含んだディスクリプタ (例:PHSolidDesc)を作ります．
  <li>インタフェースクラス (例：PHSolidIf) を作ります．
@@ -119,12 +119,16 @@ FileIO SDK の詳細は，\ref pageFileIOImp を参照してください
   	///	デスクリプタのサイズ
   	virtual size_t GetDescSize() const { return 0; };
   </pre>
-  をオーバーロードしてください．ディスクリプタを継承しているならば，
-  LoadableがLoadableDescを継承しているなら，
-  <pre>
-	ACCESS_DESC(実装クラス名);
+  をオーバーロードしてください．
   </pre>
-  マクロを実装クラスの宣言の中に置けば，オーバーライドしてくれます．
+  virtual const void* GetDescAddress() const { return NULL; }
+  </pre>
+  をオーバーロードせず、代わりに
+  </pre>
+  	///	ディスクリプタの読み出し(コピー版)
+  	virtual bool GetDesc(void* d) const;
+  </pre>
+  をオーバーロードしてもOKです．
  <li> シーングラフ上で，実装クラス(例：PHSolid)の先祖になるクラスのなかから，
       実装クラス(例：PHSolid)を生成するクラス(例：PHScene)を決めます．
  <li> 生成クラス(例：PHScene)のCreateObject()が実装クラス(PHSolid)を作れるように，
@@ -137,9 +141,30 @@ FileIO SDK の詳細は，\ref pageFileIOImp を参照してください
       詳しくは \ref secFactory を参照ください．
 </ol>
 
+\subsection secCreateLoadableObjectAutoOverload オーバーロードの自動化
+前小節での手順3の3つの関数のオーバーロードは、オブジェクト(例:PHSolid)が、
+デスクリプタ(例:PHSolidDesc)を継承しているならば，
+  <pre>
+	ACCESS_DESC(実装クラス名);
+  </pre>
+マクロを実装クラス(例:PHSolid)の宣言の中に置けば，オーバーライドしてくれます．
+
+多重継承の都合でデスクリプタ(例:PHBallJointDesc)を継承できない場合でも，
+SPR_DECLMEMBEROF_デスクリプタ名 マクロ(例:SPR_DECLMEMBEROF_PHBallJointDesc)
+を使えば自動化ができます。デスクリプタを定義すると、
+それに対応するマクロSPR_DECLMEMBEROF_デスクリプタ名マクロ
+(例:SPR_DECLMEMBEROF_PHBallJointDesc)が自動的に定義されます。
+オブジェクト(例:PHBallJoint)の定義の中で、
+<pre>
+	SPR_DECLMEMBEROF_デスクリプタ名;
+</pre>
+を定義すると、3つの関数がオーバーライドされます。
+
 \section secStateLoadSave 状態の保存・再現
-ファイルへのロードセーブでは，何も無い状態からオブジェクトを生成してシーングラフを作りますが，
-シミュレーション中に，10ステップ前の状態に戻したいなどということも良くあるでしょう．
+ファイルへのロードセーブでは，何も無い状態からオブジェクトを生成して
+シーングラフを作ります．これに対して，
+シーンのオブジェクトの構造は変わらないけれども，シミュレーション中に
+10ステップ前の状態に戻したいなどということも良くあるでしょう．
 そんな用途に使うのが状態の保存・再現です．
 
 状態の保存・再現ができるクラスを作るには，
