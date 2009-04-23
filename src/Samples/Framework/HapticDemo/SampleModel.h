b3d714da4a3d985b58f303cb6d6e0d2e4bbc304b
susa <susa@05cee5c3-a2e9-0310-9523-9dfc2f93dbe1>
2009-04-23 19:53:43 +0000

    git-svn-id: http://springhead.info/spr2/Springhead2/trunk@3805 05cee5c3-a2e9-0310-9523-9dfc2f93dbe1

---
+++ src/Samples/Framework/HapticDemo/SampleModel.h
@@ -0,0 +1,19 @@
+#ifndef SAMPLE_MODEL_H
+#define SAMPLE_MODEL_H
+
+#include <Springhead.h>
+
+using namespace Spr;
+
+CDShapeIf* CreateShapeBox(FWSdkIf* fwSdk);
+PHSolidIf* CreateBox(FWSdkIf* fwSdk);
+PHSolidIf* CreateSphere(FWSdkIf* fwSdk);
+PHSolidIf* CreateCapsule(FWSdkIf* fwSdk);
+PHSolidIf* CreateRoundCone(FWSdkIf* fwSdk);
+PHSolidIf* CreatePolyhedron(FWSdkIf* fwSdk);
+PHSolidIf* CreateLumpBox(FWSdkIf* fwSdk);
+void CreateWall(FWSdkIf* fwSdk);
+void CreateTower(FWSdkIf* fwSdk);
+
+
+#endif
