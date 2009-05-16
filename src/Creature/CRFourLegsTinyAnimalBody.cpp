d21a48d67c9b8fffdfc6dd989026b4248c27ff49
tokizaki <tokizaki@05cee5c3-a2e9-0310-9523-9dfc2f93dbe1>
2009-05-16 08:23:57 +0000

    git-svn-id: http://springhead.info/spr2/Springhead2/trunk@3864 05cee5c3-a2e9-0310-9523-9dfc2f93dbe1

--- src/Creature/CRFourLegsTinyAnimalBody.cpp
+++ src/Creature/CRFourLegsTinyAnimalBody.cpp
@@ -64,13 +64,14 @@ void CRFourLegsTinyAnimalBody::CreateBody(){
 	PHSolidDesc        solidDesc;
 
 	// Solid
-	solidDesc.mass		= 25.0;
+	solidDesc.mass		= 100.0;
 	solids[SO_BODY]		= phScene->CreateSolid(solidDesc);
 	boxDesc.boxsize		= Vec3f(bodyBreadth, bodyHeight, bodyThickness);
 	solids[SO_BODY]->AddShape(phSdk->CreateShape(boxDesc));
 	solids[SO_BODY]->SetFramePosition(Vec3f(0,0,0));
 	solids[SO_BODY]->SetOrientation(Quaterniond::Rot(Rad(-90), 'x'));
 	solids[SO_BODY]->SetName("soWaist");
+	solids[SO_BODY]->SetInertia(Matrix3d(100000, 10000, 10000, 10000, 100000, 10000, 10000, 10000, 100000));
 	solids[SO_BODY]->SetDynamical(false);
 }
 void CRFourLegsTinyAnimalBody::InitFrontLeg0(LREnum lr){	
