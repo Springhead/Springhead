bc0e6d93d663c75dcda633d928cb2d5efe95b0c0
susa <susa@05cee5c3-a2e9-0310-9523-9dfc2f93dbe1>
2009-05-24 07:23:10 +0000

    git-svn-id: http://springhead.info/spr2/Springhead2/trunk@3890 05cee5c3-a2e9-0310-9523-9dfc2f93dbe1

---
+++ src/Samples/Foundation/UTMMTimerSample/main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <GL/glut.h>						/// UTMMtimer.hを使うのに必要
+#include <windows.h>
+#include <Springhead.h>	
+#include "Foundation/UTMMTimer.h"
+
+void CallBackHoge(void* arg){
+	std::cout << "hogehoge" << std::endl;
+}
+
+void CallBackFuga(void* arg){
+	std::cout << "         " << "fugafuga" << std::endl;
+}
+
+int _cdecl main(int argc, char* argv[]){
+	Spr::UTMMTimer timer1;				/// マルチメディアタイマの宣言
+	timer1.Resolution(1);					///	 呼びだし分解能
+	timer1.Interval(1000);					/// 呼びだし頻度
+	timer1.Set(CallBackHoge, NULL);	/// 呼びだす関数
+
+	Spr::UTMMTimer timer2;
+	timer2.Resolution(1);
+	timer2.Interval(5000);
+	timer2.Set(CallBackFuga, NULL);
+
+	timer1.Create();							/// マルチメディアタイマスタート
+	timer2.Create();
+
+	if(getchar()){
+		return 0;
+	}
+}
