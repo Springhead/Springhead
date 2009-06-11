74303872562d2111ace7efdf708b53f9720dadbc
tokizaki <tokizaki@05cee5c3-a2e9-0310-9523-9dfc2f93dbe1>
2009-06-11 11:39:50 +0000

    git-svn-id: http://springhead.info/spr2/Springhead2/trunk@3980 05cee5c3-a2e9-0310-9523-9dfc2f93dbe1

---
+++ src/Samples/Framework/FWGLUISample/FWAppSample.h
@@ -0,0 +1,32 @@
+#ifndef FWAPPSAMPLE_H								// ヘッダファイルを作る時のおまじない
+#define FWAPPSAMPLE_H								// 同上
+
+#include <Springhead.h>								// SpringheadのIf型を使うためのヘッダファイル
+#include <Framework/SprFWAppGLUI.h>					// Frameworkクラスが宣言されているヘッダファイル
+#include <GL/glui.h>
+using namespace Spr;								// Springheadの名前空間
+
+class FWAppSample : public FWAppGLUI, public UTRefCount{
+private:
+	GLUI*			glui;
+	GLUI_Panel*		panel;
+	GLUI_Button*	button1;
+public:
+	FWAppSample();									// コンストラクタ（このクラスのインスタンスが生成された時に呼ばれる関数）
+	void Init(int argc, char* argv[]);			// GLUTの初期化
+	void InitCameraView();							// カメラ座標		
+	void BuildObject();								// 物体を作成
+	void Step();										// シミュレーションを1ステップ進める
+	void Display();									// 描画
+	void Reset();										// シーンのリセット
+	void Keyboard(int key, int x, int y);		// キーボード関数
+	bool bDrawInfo;									// 接触力などを表示するか
+
+	void SPR_CDECL DesignGUI();
+
+private:
+	static void CallButton1(int control);
+	void		Button1(int control);
+}; 
+
+#endif
