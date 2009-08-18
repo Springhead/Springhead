ff55b9a8f7724758a940e54054cd6b437f7c6245
susa <susa@05cee5c3-a2e9-0310-9523-9dfc2f93dbe1>
2009-08-18 08:37:24 +0000

    git-svn-id: http://springhead.info/spr2/Springhead2/trunk@4260 05cee5c3-a2e9-0310-9523-9dfc2f93dbe1

--- src/Samples/Framework/FWAppSample/FWAppSample.h
+++ src/Samples/Framework/FWAppSample/FWAppSample.h
@@ -2,21 +2,25 @@
 #define FWAPPSAMPLE_H								// 同上
 
 #include <Springhead.h>								// SpringheadのIf型を使うためのヘッダファイル
-#include <Framework/SprFWAppGLUT.h>					// Frameworkクラスが宣言されているヘッダファイル
+#include <Framework/SprFWApp.h>						// Frameworkクラスが宣言されているヘッダファイル
 
 using namespace Spr;								// Springheadの名前空間
-
-class FWAppSample : public FWAppGLUT, public UTRefCount{
+class FWAppSample;
+class FWAppSample : public FWApp{
 public:
 	FWAppSample();									// コンストラクタ（このクラスのインスタンスが生成された時に呼ばれる関数）
-	void Init(int argc, char* argv[]);			// GLUTの初期化
+	virtual void Init(int argc, char* argv[]);		// GLUTの初期化
+	virtual void Timer();
+	virtual void IdleFunc();
 	void InitCameraView();							// カメラ座標		
 	void BuildObject();								// 物体を作成
-	void Step();										// シミュレーションを1ステップ進める
+	void Step();									// シミュレーションを1ステップ進める
 	void Display();									// 描画
-	void Reset();										// シーンのリセット
-	void Keyboard(int key, int x, int y);		// キーボード関数
+	void Reset();									// シーンのリセット
+	void Keyboard(int key, int x, int y);			// キーボード関数
 	bool bDrawInfo;									// 接触力などを表示するか
+	static void _cdecl TimerFunc(int id);
+	void CallStep();
 }; 
 
 #endif
