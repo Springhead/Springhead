6d918e21aa658e68803cb1dddbf5e3ea774ab685
susa <susa@05cee5c3-a2e9-0310-9523-9dfc2f93dbe1>
2009-08-19 07:05:38 +0000

    git-svn-id: http://springhead.info/spr2/Springhead2/trunk@4272 05cee5c3-a2e9-0310-9523-9dfc2f93dbe1

--- src/Samples/Framework/FWLDHapticSample/FWLDHapticSample.h
+++ src/Samples/Framework/FWLDHapticSample/FWLDHapticSample.h
@@ -2,16 +2,26 @@
 #define FWLDHAPTIC_SAMPLE_H
 
 #include <Springhead.h>
-#include <Framework/SprFWAppLDHaptic.h>
+#include <Framework/SprFWApp.h>
 
 using namespace Spr;
 
-class FWLDHapticSample : public FWAppLDHaptic{
+class FWLDHapticSample : public FWApp{
 public:
 	FWLDHapticSample();									
-	void InitCameraView();									
-	void BuildScene();																			
+	virtual void Init(int argc, char* argv[]);
+	void InitCameraView();	
+	void InitHumanInterface();
+	void IdleFunc();
+	static void CallBackPhysicsLoop();
+	static void CallBackHapticLoop(void* arg);	
+	void Display();									
+	void BuildScene();																	
+	void Reset();			
 	void Keyboard(int key, int x, int y);		
+	bool bDrawInfo;									
+	void DisplayContactPlane();
+	void DisplayLineToNearestPoint();
 }; 
 
 #endif
