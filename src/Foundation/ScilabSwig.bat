e860c76f3e58f09eeaca585dcaaeccab0ca78e04
hase <hase@05cee5c3-a2e9-0310-9523-9dfc2f93dbe1>
2009-08-31 06:43:04 +0000

    git-svn-id: http://springhead.info/spr2/Springhead2/trunk@4322 05cee5c3-a2e9-0310-9523-9dfc2f93dbe1

--- src/Foundation/ScilabSwig.bat
+++ src/Foundation/ScilabSwig.bat
@@ -6,8 +6,8 @@ set PATH=%SPRINGHEAD2%\bin\swig;%SPRINGHEAD2%\bin
 set MODULE=Scilab
 set MAKEFILE=%MODULE%Stub.mak.txt
 echo #	Do not edit. ScilabSwig.bat will update this file.> %MAKEFILE%
-echo all: %MODULE%Stub.cpp>>%MAKEFILE%
-echo %MODULE%Stub.cpp: Scilab.i>>%MAKEFILE%
+echo all: ..\..\include\Scilab\ScilabStub.hpp>>%MAKEFILE%
+echo ..\..\include\Scilab\ScilabStub.hpp: Scilab.i>>%MAKEFILE%
 echo 	swig -dll -c++ Scilab.i>>%MAKEFILE%
 echo 	copy ScilabStub.hpp ..\..\include\Scilab>>%MAKEFILE%
 echo 	del ScilabStub.hpp>>%MAKEFILE%
