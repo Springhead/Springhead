e39c1b50ae56cfa0d13c9621ccaca6659f6e9934
hase <hase@05cee5c3-a2e9-0310-9523-9dfc2f93dbe1>
2009-09-07 13:28:37 +0000

    git-svn-id: http://springhead.info/spr2/Springhead2/trunk@4368 05cee5c3-a2e9-0310-9523-9dfc2f93dbe1

--- src/Samples/Physics/ColTest/colTest.cpp
+++ src/Samples/Physics/ColTest/colTest.cpp
@@ -17,10 +17,35 @@ int main(){
 	file.open("ContFindCommonPointSaveParam.txt");
 	PHSdkIf* sdk = PHSdkIf::CreateSdk();
 	ContFindCommonPointCall(file, sdk);
-#else
+#elif 0
 	file.open("DetectContinuouslySaveParam.txt");
 	PHSdkIf* sdk = PHSdkIf::CreateSdk();
 	CallDetectContinuously(file, sdk);
+#else
+	extern bool bGJKDebug;
+	bGJKDebug = true;
+	CDSphereDesc sdA;
+	sdA.radius = 1;
+	CDSphereDesc sdB;
+	sdB.radius = 2;
+	UTRef<CDConvex> cA = new CDSphere(sdA);
+	UTRef<CDConvex> cB = new CDSphere(sdB);
+	Posed poseA, poseB;
+	double rr = sdA.radius + sdB.radius;
+	poseA.pz = sqrt(rr)-0.1;
+	poseA.px = sqrt(rr);
+	Vec3d dir(0,0,1);
+	Vec3d normal, pa, pb;
+	double dist = 0;
+	for(int i=0; i<10; ++i){
+		int rt = ContFindCommonPoint(cA, cB, poseA, poseB, dir, -10, 10, normal, pa, pb, dist);
+		std::cout << rt;
+		std::cout << " normal:" << normal;
+		std::cout << " dist:" << dist;
+		std::cout << " pa pb:" << pa << pb << std::endl;
+		dir = -normal;
+	}
+	getchar();
 #endif
 	return 0;
 }
