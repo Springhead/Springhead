621d283c45bd3bc033c1fcbc7cc82d79f12b4b8f
susa <susa@05cee5c3-a2e9-0310-9523-9dfc2f93dbe1>
2009-10-02 09:55:14 +0000

    SprFWAppHapticとFWAppHapticを削除
    git-svn-id: http://springhead.info/spr2/Springhead2/trunk@4403 05cee5c3-a2e9-0310-9523-9dfc2f93dbe1

--- src/Samples/Framework/FWLDHapticSample/FWLDHapticSample.h
+++ src/Samples/Framework/FWLDHapticSample/FWLDHapticSample.h
@@ -2,18 +2,29 @@
 #define FWLDHAPTIC_SAMPLE_H
 
 #include <Springhead.h>
-#include <Framework/SprFWAppHaptic.h>
+#include <Framework/SprFWApp.h>
 
 using namespace Spr;
 
-class FWLDHapticSample : public FWAppHaptic{
+class FWLDHapticSample : public FWApp{
 public:
-	FWLDHapticSample();									
+	FWLDHapticSample();
+	virtual void Init(int argc, char* argv[]);			///< アプリケーションの初期化
 	virtual void InitCameraView();						///< カメラ行列の初期化
 	virtual void InitHumanInterface();					///< HapticInterfaceの初期化
-	virtual void BuildScene();							///< 剛体の作成	
-	virtual void BuildPointer();							///< 剛体の作成	
-	void Keyboard(int key, int x, int y);		///< キーボード関数
+	virtual void Reset();
+	virtual void TimerFunc(int id);
+	virtual void IdleFunc();							
+	virtual void Display();								///< 描画関数	
+	virtual void BuildScene();							///< 剛体の作成
+	virtual void BuildPointer();						///< 剛体の作成
+								
+	virtual void Keyboard(int key, int x, int y);		///< キーボード関数
+
+	/// オプション
+	bool bStep;									///< シミュレーションを進めるかどうか
+	bool bOneStep;								///< ステップ実行
+	bool bDrawInfo;								///< デバック表示するかどうか
 }; 
 
 #endif
