12b0a759dd48555a2cd112079609d416793c7313
susa <susa@05cee5c3-a2e9-0310-9523-9dfc2f93dbe1>
2009-10-02 09:55:14 +0000

    SprFWAppHapticとFWAppHapticを削除
    git-svn-id: http://springhead.info/spr2/Springhead2/trunk@4403 05cee5c3-a2e9-0310-9523-9dfc2f93dbe1

--- src/Samples/Framework/FWLDHapticSample/FWLDHapticSample.cpp
+++ src/Samples/Framework/FWLDHapticSample/FWLDHapticSample.cpp
@@ -6,7 +6,48 @@
 using namespace std;
 
 FWLDHapticSample::FWLDHapticSample(){
+	bStep = true;
+	bDrawInfo = false;
 }
+
+void FWLDHapticSample::Init(int argc, char* argv[]){
+	/// Sdkの初期化，シーンの作成
+	CreateSdk();
+	GetSdk()->Clear();										// SDKの初期化
+	GetSdk()->CreateScene(PHSceneDesc(), GRSceneDesc());	// Sceneの作成
+	GetSdk()->GetScene()->GetPHScene()->SetTimeStep(0.02);	// 刻みの設定
+
+	/// 描画モードの設定
+	SetGRAdaptee(TypeGLUT);									// GLUTモードに設定
+	GetGRAdaptee()->Init(argc, argv);						// Sdkの作成
+
+	/// 描画Windowの作成，初期化
+	FWWinDesc windowDesc;									// GLのウィンドウディスクリプタ
+	windowDesc.title = "FWLDHapticSample";					// ウィンドウのタイトル
+	CreateWin(windowDesc);									// ウィンドウの作成
+	InitWindow();											// ウィンドウの初期化
+	InitCameraView();										// カメラビューの初期化
+
+	/// HumanInterfaceの初期化
+	InitHumanInterface();
+
+	/// InteractSceneの作成
+	FWInteractSceneDesc desc;
+	desc.fwScene = GetSdk()->GetScene();					// fwSceneに対するinteractsceneを作る
+	desc.mode = LOCAL_DYNAMICS;								// humaninterfaceのレンダリングモードの設定
+	desc.hdt = 0.001;										// マルチレートの場合の更新[s]
+	CreateINScene(desc);									// interactSceneの作成
+
+	/// 物理シミュレーションする剛体を作成
+	BuildScene();
+	BuildPointer();
+
+	/// タイマの作成，設定
+	FWTimer* timer = CreateTimer(MMTimer);
+	timer->SetInterval(1);
+	timer->SetResolution(1);
+}
+
 void FWLDHapticSample::InitCameraView(){
 	std::istringstream issView(
 		"((0.9996 0.0107463 -0.0261432 -0.389004)"
@@ -52,34 +93,66 @@ void FWLDHapticSample::InitHumanInterface(){
 #endif
 }
 
+void FWLDHapticSample::Reset(){
+	ReleaseAllTimer();
+	GetSdk()->Clear();
+	INClear();
+	GetSdk()->CreateScene(PHSceneDesc(), GRSceneDesc());	// Sceneの作成
+	GetSdk()->GetScene()->GetPHScene()->SetTimeStep(0.02);	// 刻みの設定
+	FWInteractSceneDesc desc;
+	desc.fwScene = GetSdk()->GetScene();					// fwSceneに対するinteractsceneを作る
+	desc.mode = LOCAL_DYNAMICS;								// humaninterfaceのレンダリングモードの設定
+	desc.hdt = 0.001;										// マルチレートの場合の更新[s]
+	CreateINScene(desc);									// interactSceneの作成
+	BuildScene();
+	BuildPointer();
+	GetCurrentWin()->SetScene(GetSdk()->GetScene());
+	CreateAllTimer();
+}
 
-void FWLDHapticSample::BuildPointer(){
-	PHSceneIf* phscene = GetSdk()->GetScene()->GetPHScene();
-	PHSolidDesc desc;
-	/// ポインタ
-	{	
-		for(int i= 0; i < 2; i++){
-			PHSolidIf* soPointer = phscene->CreateSolid(desc);
-			CDSphereDesc sd;
-			sd.radius = 0.5;//1.0;
-			CDSphereIf* shapePointer = DCAST(CDSphereIf,  GetSdk()->GetPHSdk()->CreateShape(sd));
-			soPointer->AddShape(shapePointer);
-			soPointer->SetDynamical(false);
-			soPointer->GetShape(0)->SetStaticFriction(1.0);
-			soPointer->GetShape(0)->SetDynamicFriction(1.0);
-			GetSdk()->GetScene()->GetPHScene()->SetContactMode(soPointer, PHSceneDesc::MODE_NONE);
-			FWInteractPointerDesc idesc;			// interactpointerのディスクリプタ
-			idesc.pointerSolid = soPointer;			// soPointerを設定
-			idesc.humanInterface = GetHI(i);		// humaninterfaceを設定
-			idesc.springK = 10;//0.8						// haptic renderingのバネ係数
-			idesc.damperD = 0.01;//0.01					// haptic renderingのダンパ係数
-			idesc.posScale = 300;					// soPointerの可動域の設定(～倍)
-			idesc.localRange = 1.0;					// LocalDynamicsを使う場合の近傍範囲
-			if(i==0) idesc.defaultPosition =Posed(1,0,0,0,5,0,0);	// 初期位置の設定
-			if(i==1) idesc.defaultPosition =Posed(1,0,0,0,-5,0,0);
-			GetINScene()->CreateINPointer(idesc);	// interactpointerの作成
-		}
+void FWLDHapticSample::TimerFunc(int id){	
+	switch(id){
+		case 0:{
+			GetINScene()->CallBackHapticLoop();
+			break;
+			   }
 	}
+}
+
+void FWLDHapticSample::IdleFunc(){
+	/// シミュレーションを進める(interactsceneがある場合はそっちを呼ぶ)
+	if(bStep) 	GetINScene()->Step();
+	else if (bOneStep){
+			GetINScene()->Step();
+			bOneStep = false;
+	}
+	GetGRAdaptee()->PostRedisplay();
+}
+
+void FWLDHapticSample::Display(){
+	/// 描画モードの設定
+	GetSdk()->SetDebugMode(true);
+	GRDebugRenderIf* render = GetCurrentWin()->render->Cast();
+	render->SetRenderMode(true, false);
+	render->EnableRenderAxis(bDrawInfo);
+	render->EnableRenderForce(bDrawInfo);
+	render->EnableRenderContact(bDrawInfo);
+
+	/// カメラ座標の指定
+	GRCameraIf* cam = GetCurrentWin()->scene->GetGRScene()->GetCamera();
+	if (cam && cam->GetFrame()){
+		cam->GetFrame()->SetTransform(cameraInfo.view);
+	}else{
+		GetCurrentWin()->render->SetViewMatrix(cameraInfo.view.inv());
+	}
+
+	/// 描画の実行
+	if(!GetCurrentWin()) return;
+	GetSdk()->SwitchScene(GetCurrentWin()->GetScene());
+	GetSdk()->SwitchRender(GetCurrentWin()->GetRender());
+	GetSdk()->Draw();
+
+	glutSwapBuffers();
 }
 
 void FWLDHapticSample::BuildScene(){
@@ -122,7 +195,125 @@ void FWLDHapticSample::BuildScene(){
 	}
 }
 
+void FWLDHapticSample::BuildPointer(){
+	PHSceneIf* phscene = GetSdk()->GetScene()->GetPHScene();
+	PHSolidDesc desc;
+	/// ポインタ
+	{	
+		for(int i= 0; i < 2; i++){
+			PHSolidIf* soPointer = phscene->CreateSolid(desc);
+			CDSphereDesc sd;
+			sd.radius = 0.5;//1.0;
+			CDSphereIf* shapePointer = DCAST(CDSphereIf,  GetSdk()->GetPHSdk()->CreateShape(sd));
+			soPointer->AddShape(shapePointer);
+			soPointer->SetDynamical(false);
+			soPointer->GetShape(0)->SetStaticFriction(1.0);
+			soPointer->GetShape(0)->SetDynamicFriction(1.0);
+			GetSdk()->GetScene()->GetPHScene()->SetContactMode(soPointer, PHSceneDesc::MODE_NONE);
+			FWInteractPointerDesc idesc;			// interactpointerのディスクリプタ
+			idesc.pointerSolid = soPointer;			// soPointerを設定
+			idesc.humanInterface = GetHI(i);		// humaninterfaceを設定
+			idesc.springK = 10;//0.8						// haptic renderingのバネ係数
+			idesc.damperD = 0.01;//0.01					// haptic renderingのダンパ係数
+			idesc.posScale = 300;					// soPointerの可動域の設定(～倍)
+			idesc.localRange = 1.0;					// LocalDynamicsを使う場合の近傍範囲
+			if(i==0) idesc.defaultPosition =Posed(1,0,0,0,5,0,0);	// 初期位置の設定
+			if(i==1) idesc.defaultPosition =Posed(1,0,0,0,-5,0,0);
+			GetINScene()->CreateINPointer(idesc);	// interactpointerの作成
+		}
+	}
+}
 
 void FWLDHapticSample::Keyboard(int key, int x, int y){
-	FWAppHaptic::Keyboard(key , x, y);
+	BeginKeyboard();
+	switch (key) {
+		case  27: //ESC
+		case 'q':
+			exit(0);
+			break;
+		case 'r':
+			Reset();
+			break;
+		case 'd':
+			bDrawInfo = !bDrawInfo;
+			break;
+		case 's':
+			bStep = false;
+			bOneStep = true;
+			DSTR << "Stepwise Execution" << endl;
+			break;
+		case 'a':
+			bStep = true;
+			DSTR << "Play" << endl;
+			break;
+		case 'c':
+			{
+				ReleaseAllTimer();
+				for(int i = 0; i < GetINScene()->NINPointers(); i++){
+					GetINScene()->GetINPointer(i)->Calibration();
+				}
+				CreateAllTimer();
+			}
+			break;
+		case 'f':
+			{
+				static bool bf = false;
+				bf = !bf;
+				for(int i = 0; i < GetINScene()->NINPointers(); i++){
+					GetINScene()->GetINPointer(i)->EnableForce(bf);
+					if(!bf){
+						HIBaseIf* hib = GetINScene()->GetINPointer(i)->GetHI();
+						if(DCAST(HIForceInterface6DIf, hib)){
+							HIForceInterface6DIf* hif = hib->Cast();
+							hif->SetForce(Vec3d(), Vec3d());
+						}
+						if(DCAST(HIForceInterface3DIf, hib)){
+							HIForceInterface3DIf* hif = hib->Cast();
+							hif->SetForce(Vec3d());
+						}
+					}
+				}
+				if(bf){
+					DSTR << "Enable Force Feedback" << std::endl;
+				}else{
+					DSTR << "Disable Force Feedback" << std::endl;
+				}
+			}
+			break;
+		case 'o':
+			{
+				static bool bv = false;
+				bv = !bv;
+				for(int i = 0; i < GetINScene()->NINPointers(); i++){
+					GetINScene()->GetINPointer(i)->EnableVibration(bv);
+				}
+				if(bv){
+					DSTR << "Enable Vibration Feedback" << std::endl;
+				}else{
+					DSTR << "Disable Vibration Feedback" << std::endl;
+				}
+			}
+			break;
+		case 'j':
+			{
+					GetINScene()->SetHMode(PENALTY);
+					DSTR << "PENALTY MODE" << std::endl;
+					break;
+			}
+		case 'k':
+			{
+					GetINScene()->SetHMode(PROXY);
+					DSTR << "PROXY MODE" << std::endl;
+					break;
+			}
+		case 'l':
+			{
+					GetINScene()->SetHMode(PROXYSIMULATION);
+					DSTR << "PROXYSIMULATION MODE" << std::endl;
+					break;
+			}
+		default:
+			break;
+	}
+	EndKeyboard();
 }
