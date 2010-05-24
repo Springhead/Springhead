e1fcf178211d7248040d00ecc0348b046d8df038
hase <hase@05cee5c3-a2e9-0310-9523-9dfc2f93dbe1>
2010-05-24 00:35:46 +0000

    git-svn-id: http://springhead.info/spr2/Springhead2/trunk@4673 05cee5c3-a2e9-0310-9523-9dfc2f93dbe1

--- src/Graphics/GRLoadBmp.h
+++ src/Graphics/GRLoadBmp.h
@@ -8,8 +8,11 @@ int SPR_CDECL LoadBmpRelease(int hBmp);
 int SPR_CDECL LoadBmpGetBmp(int hBmp, char* bmp);
 int SPR_CDECL LoadBmpGetWidth(int hBmp);
 int SPR_CDECL LoadBmpGetHeight(int hBmp);
+int SPR_CDECL LoadBmpGetBitPerPixel(int hBmp);
 int SPR_CDECL LoadBmpGetBytePerPixel(int hBmp);
 int SPR_CDECL LoadBmpGetSize(int hBmp);
+int SPR_CDECL LoadBmpHasAlpha(int hBmp);
+int SPR_CDECL LoadBmpIsGrayscale(int hBmp);
 
 
 };
