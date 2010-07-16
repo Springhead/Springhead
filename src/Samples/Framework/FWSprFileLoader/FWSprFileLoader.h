2b4c8d442ceddff2b966871828a71be8d1232068
hase <hase@05cee5c3-a2e9-0310-9523-9dfc2f93dbe1>
2010-07-16 02:14:46 +0000

    git-svn-id: http://springhead.info/spr2/Springhead2/trunk@4713 05cee5c3-a2e9-0310-9523-9dfc2f93dbe1

---
+++ src/Samples/Framework/FWSprFileLoader/FWSprFileLoader.h
@@ -0,0 +1,25 @@
+#ifndef FWSPRFILE_LOADER_H							// ヘッダファイルを作る時のおまじない
+#define FWSPRFILE_LOADER_H							// 同上
+
+#include <Springhead.h>								// SpringheadのIf型を使うためのヘッダファイル
+#include <Framework/SprFWApp.h>						// Frameworkクラスが宣言されているヘッダファイル
+
+using namespace Spr;								// Springheadの名前空間
+
+class FWSprfileLoader : public FWApp{
+public:
+	std::string fileName;
+	bool bDebug;									///< デバックモードの切り替え
+	FWSprfileLoader();								///< コンストラクタ（このクラスのインスタンスが生成された時に呼ばれる関数）
+	virtual void Init(int argc, char* argv[]);		///< GLUTの初期化
+	virtual void TimerFunc(int id);
+	virtual void IdleFunc();
+	virtual void Display();							///< 描画
+	virtual void Keyboard(int key, int x, int y);	///< キーボード関数
+
+	void InitCameraView();							///< カメラ座標				
+	void Reset();
+	void Step();									///< シミュレーションを1ステップ進める
+}; 
+
+#endif
