4ddd930aa299cccacf8c55ed94bfba696ad04ba2
hase <hase@05cee5c3-a2e9-0310-9523-9dfc2f93dbe1>
2011-05-17 06:54:35 +0000

    git-svn-id: http://springhead.info/spr2/Springhead2/trunk@4971 05cee5c3-a2e9-0310-9523-9dfc2f93dbe1

---
+++ src/Framework/FWFemMesh.cpp
@@ -0,0 +1,16 @@
+#include "Framework.h"
+#include <Framework/SprFWFemMesh.h>
+#include <Graphics/GRFrame.h>
+#include <Physics/PHConstraint.h>
+#include "FWFemMesh.h"
+
+#ifdef USE_HDRSTOP
+#pragma hdrstop
+#endif
+
+namespace Spr{;
+
+FWFemMesh::FWFemMesh(const FWFemMeshDesc& d):FWFemMeshDesc(d){
+}
+
+}
