c597cadd5a0fec05130cd6a9496bb0c6ae9dfbab
hase <hase@05cee5c3-a2e9-0310-9523-9dfc2f93dbe1>
2011-05-17 06:02:40 +0000

    FWFEMMeshを追加
    git-svn-id: http://springhead.info/spr2/Springhead2/trunk@4967 05cee5c3-a2e9-0310-9523-9dfc2f93dbe1

---
+++ src/Framework/FWFemMesh.h
@@ -0,0 +1,17 @@
+/*
+ *  Copyright (c) 2003-2008, Shoichi Hasegawa and Springhead development team 
+ *  All rights reserved.
+ *  This software is free software. You can freely use, distribute and modify this 
+ *  software. Please deal with this software under one of the following licenses: 
+ *  This license itself, Boost Software License, The MIT License, The BSD License.   
+ */
+#ifndef FWFEMMESH_H
+#define FWFEMMESH_H
+
+#include <Springhead.h>
+#include <Framework/FWObject.h>
+
+namespace Spr{;
+}
+
+#endif
