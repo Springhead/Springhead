b943f5839008ee2d920d117f81a4831b649d8a95
hase <hase@05cee5c3-a2e9-0310-9523-9dfc2f93dbe1>
2011-05-17 06:45:35 +0000

    git-svn-id: http://springhead.info/spr2/Springhead2/trunk@4970 05cee5c3-a2e9-0310-9523-9dfc2f93dbe1

---
+++ src/Samples/Physics/FEMThermo/main.cpp
@@ -0,0 +1,94 @@
+/*
+ *  Copyright (c) 2003-2006, Shoichi Hasegawa and Springhead development team 
+ *  All rights reserved.
+ *  This software is free software. You can freely use, distribute and modify this 
+ *  software. Please deal with this software under one of the following licenses: 
+ *  This license itself, Boost Software License, The MIT License, The BSD License.   
+ */
+/**
+\page pageFEMThermo FEMによる温度シミュレーションのサンプル
+Springhead2/src/Samples/FEMThermo
+
+\section secQuitBoxStack 終了基準
+- Escか'q'で強制終了。
+	
+*/
+
+#include "../../SampleApp.h"
+
+#pragma hdrstop
+using namespace Spr;
+using namespace std;
+
+
+class MyApp : public SampleApp{
+public:
+	/// ページID
+	enum {
+		MENU_MAIN = MENU_SCENE,
+	};
+	/// アクションID
+	enum {
+		ID_BOX,
+	};
+
+public:
+	MyApp(){
+		appName = "FEMThermo";
+
+		AddAction(MENU_MAIN, ID_BOX, "drop object");
+		AddHotKey(MENU_MAIN, ID_BOX, 'd');
+	}
+	~MyApp(){}
+
+	virtual void BuildScene(){
+		GetSdk()->Clear();						// SDKの初期化
+		UTRef<ImportIf> import = GetSdk()->GetFISdk()->CreateImport();
+		GetSdk()->LoadScene("scene.spr", import);			// ファイルのロード
+	}
+
+	// タイマコールバック関数．タイマ周期で呼ばれる
+	virtual void OnStep() {
+		SampleApp::OnStep();
+	}
+
+	// 描画関数．描画要求が来たときに呼ばれる
+	virtual void OnDraw(GRRenderIf* render) {
+		SampleApp::OnDraw(render);
+
+		std::ostringstream sstr;
+		sstr << "NObj = " << phScene->NSolids();
+		render->DrawFont(Vec2f(-21, 23), sstr.str());
+	}
+
+	virtual void OnAction(int menu, int id){
+		if(menu == MENU_MAIN){
+			Vec3d v, w(0.0, 0.0, 0.2), p(0.5, 20.0, 0.0);
+			Quaterniond q = Quaterniond::Rot(Rad(30.0), 'y');
+
+			if(id == ID_BOX){
+				Drop(SHAPE_BOX, GRRenderIf::RED, v, w, p, q);
+				message = "box dropped.";
+			}
+		}
+		SampleApp::OnAction(menu, id);
+	}
+
+};
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+MyApp app;
+
+/**
+ brief		メイン関数
+ param		<in/--> argc　　コマンドライン入力の個数
+ param		<in/--> argv　　コマンドライン入力
+ return		0 (正常終了)
+ */
+int main(int argc, char *argv[]) {
+	app.Init(argc, argv);
+	app.StartMainLoop();
+	return 0;
+}
+
