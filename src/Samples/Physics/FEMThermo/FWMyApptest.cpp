eb01dffd43bf23bbea62b226a97eee6b033c007a
fumihiro <fumihiro@05cee5c3-a2e9-0310-9523-9dfc2f93dbe1>
2011-05-19 13:19:07 +0000

    git-svn-id: http://springhead.info/spr2/Springhead2/trunk@4999 05cee5c3-a2e9-0310-9523-9dfc2f93dbe1

---
+++ src/Samples/Physics/FEMThermo/FWMyApptest.cpp
@@ -0,0 +1,12 @@
+//#include "FWMyApptest.h"
+
+//#define ESC 27
+
+//constructor
+//FWMyApptest::FWMyApptest(){
+//		appName = "FEMThermo";
+//
+//		AddAction(MENU_MAIN, ID_BOX, "drop object");
+//		AddHotKey(MENU_MAIN, ID_BOX, 'd');
+//	}
+
