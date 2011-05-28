5bb5ea1caf608af1ffe1562337dd5574fd7bc548
hase <hase@05cee5c3-a2e9-0310-9523-9dfc2f93dbe1>
2011-05-28 04:49:26 +0000

    git-svn-id: http://springhead.info/spr2/Springhead2/trunk@5065 05cee5c3-a2e9-0310-9523-9dfc2f93dbe1

--- include/docsrc/01UsersGuide/10EmbPython.h
+++ include/docsrc/01UsersGuide/10EmbPython.h
@@ -8,8 +8,12 @@
 PythonインタプリタからSpringheadを触れるようにするモジュールです。
 実行するとPythonインタプリタが起動し、Pythonインタプリタからシーンに触ることなどができます。
 
-\section sec_EmbPythonInstall 埋め込みPythonのインストール
-環境変数 SPRPYTHONPATH に
+\section sec_EmbPythonInstall 埋め込みPythonの設定
+Springheadの設定に加えて以下の設定が必要です。
+-# 環境変数 「SPRPYTHONPATH」に Springheadのフォルダ\bin\src\Python\Lib を設定。
+-# PCの再起動（Visual Studioのデバッグ環境に環境変数を反映させるために必要なようです。他の方法が見つかればよいのですが）
+環境変数の設定が悪いと
+バッファオーバランエラーが起こったりしてプログラムが動かなくなります。
 
 \section sec_EmbPythonUsage 埋め込みPythonの使い方
 /src/Samples/EmbPython/BoxStack にサンプルがあります。
