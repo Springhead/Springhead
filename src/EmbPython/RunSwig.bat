a4fa926433f37265bb40fd68c3add48e01161bf2
hase <hase@05cee5c3-a2e9-0310-9523-9dfc2f93dbe1>
2011-05-29 09:54:23 +0000

    git-svn-id: http://springhead.info/spr2/Springhead2/trunk@5073 05cee5c3-a2e9-0310-9523-9dfc2f93dbe1

--- src/EmbPython/RunSwig.bat
+++ src/EmbPython/RunSwig.bat
@@ -6,6 +6,7 @@ call .\EmbPythonSwig.bat Collision Foundation
 call .\EmbPythonSwig.bat Physics Collision Foundation
 call .\EmbPythonSwig.bat Graphics Physics Foundation
 call .\EmbPythonSwig.bat Creature Graphics Physics Foundation
-
+call .\EmbPythonSwig.bat HumanInterface Foundation
+call .\EmbPythonSwig.bat Framework HumanInterface Graphics Physics Foundation
 
 exit 0
