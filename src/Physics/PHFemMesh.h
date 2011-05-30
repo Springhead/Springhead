f8c9d674101e8bfbff54fa5fdf8dfb385b32c29d
fumihiro <fumihiro@05cee5c3-a2e9-0310-9523-9dfc2f93dbe1>
2011-05-30 10:08:52 +0000

    PHFemMesh が　PHFemMeshDescを継承しない形式（GetDescやSetDescを用いる 熱伝導の方程式をガウス・ザイデル法で解く前提で、熱計算に必要なパラメータを追加など
    git-svn-id: http://springhead.info/spr2/Springhead2/trunk@5077 05cee5c3-a2e9-0310-9523-9dfc2f93dbe1

--- src/Physics/PHFemMesh.h
+++ src/Physics/PHFemMesh.h
@@ -15,11 +15,41 @@
 namespace Spr{;
 
 ///	有限要素法シミュレーションのためのメッシュ(4面体メッシュ)
-class PHFemMesh: public SceneObject, public PHFemMeshDesc {
+
+class PHFemMesh: public SceneObject{
 public:
+	struct Convolusion{
+		double volume;		//	\int {N} {N}^T dV
+		double gradient;	//	\int d{N}/dx d{N}^T/dx + d{N}/dy d{N}^T/dy + d{N}/dz d{N}^T/dz  dV
+		double surface;		//	\int {N} {N}^T dS	表面の頂点のみが値を持つ
+	};
+	struct FemVertex{
+		Vec3d pos;
+		std::vector<int> tets;
+		std::vector<int> neighbors;
+		bool surfaceFlag;
+		std::vector<Convolusion> convNeighbors;
+		Convolusion convSelf;
+		FemVertex();
+	};
+	struct Tet{
+		int vertices[4];
+	};
+	std::vector<FemVertex> vertices;
+	std::vector<Tet> tets;
 	SPR_OBJECTDEF(PHFemMesh);
 	PHFemMesh(const PHFemMeshDesc& desc=PHFemMeshDesc(), SceneIf* s=NULL);
+	//	デスクリプタのサイズ
+	virtual size_t GetDescSize() const { return sizeof(PHFemMeshDesc); };
+	//	デスクリプタの読み出し(コピー版)
+	virtual bool GetDesc(void* desc) const ;
+	// デスクリプタの設定
+	virtual void SetDesc(const void* desc);
+	
+
 };
+
+
 
 
 }	//	namespace Spr
