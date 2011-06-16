c4c40a5de8b7f59e1b2f40ce60f53b42a0c857e9
tazaki <tazaki@05cee5c3-a2e9-0310-9523-9dfc2f93dbe1>
2011-06-16 11:33:33 +0000

    git-svn-id: http://springhead.info/spr2/Springhead2/trunk@5128 05cee5c3-a2e9-0310-9523-9dfc2f93dbe1

---
+++ src/Framework/FWWin.cpp
@@ -0,0 +1,50 @@
+/*
+ *  Copyright (c) 2003-2008, Shoichi Hasegawa and Springhead development team 
+ *  All rights reserved.
+ *  This software is free software. You can freely use, distribute and modify this 
+ *  software. Please deal with this software under one of the following licenses: 
+ *  This license itself, Boost Software License, The MIT License, The BSD License.   
+ */
+
+#include <Framework/FWWin.h>
+#include <Framework/FWScene.h>
+#include <Graphics/SprGRScene.h>
+#include <Graphics/SprGRRender.h>
+#ifdef USE_HDRSTOP
+#pragma hdrstop
+#endif
+
+namespace Spr{;
+
+void FWWin::Display(){
+	// GRSceneにカメラフレームが無い場合はトラックボールを直接ビュー変換に反映する
+	if(!scene->GetGRScene() || !scene->GetGRScene()->GetCamera() || !scene->GetGRScene()->GetCamera()->GetFrame()){
+		render->SetViewMatrix(trackball->GetAffine().inv());
+	}
+	scene->Draw(render, renderMode);
+	render->SwapBuffers();
+}
+
+void FWWin::Reshape(int w, int h){
+	render->Reshape(Vec2f(), Vec2f(w,h));
+}
+
+void FWWin::OnUpdatePose(HITrackballIf*, bool dir){
+	// トラックボールとGRSceneのカメラフレームの同期
+	GRCameraIf* cam = GetScene()->GetGRScene()->GetCamera();
+	if(!cam)
+		return;
+	GRFrameIf* camFrame = cam->GetFrame();
+	if(!camFrame)
+		return;
+
+	if(dir){
+		camFrame->SetTransform(trackball->GetAffine());
+	}
+	else{
+		trackball->SetPosition(camFrame->GetTransform().Pos());
+	}
+}
+
+
+}
