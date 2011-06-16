0b16dde60b4cb57806a48fa838f6515b52473991
tazaki <tazaki@05cee5c3-a2e9-0310-9523-9dfc2f93dbe1>
2011-06-16 11:33:33 +0000

    git-svn-id: http://springhead.info/spr2/Springhead2/trunk@5128 05cee5c3-a2e9-0310-9523-9dfc2f93dbe1

---
+++ include/Framework/SprFWWin.h
@@ -0,0 +1,100 @@
+#ifndef SPR_FW_WINDOW_H
+#define SPR_FW_WINDOW_H
+
+#include <Foundation/SprObject.h>
+
+namespace Spr{;
+
+struct GRRenderIf;
+struct DVKeyMouseIf;
+struct DVJoyStickIf;
+struct HITrackballIf;
+struct HIDraggerIf;
+struct FWSceneIf;
+
+struct FWWinDesc{
+	int			width;			///<	幅
+	int			height;			///<	高さ
+	int			left;			///<	左端の座標
+	int			top;			///<	上端の座標
+	int			parentWindow;	///<	子ウィンドウを作る場合は、親のID、そうでなければ0
+	UTString	title;			///<	ウィンドウのタイトル文字列(トップレベルウィンドウのみ)
+	bool		fullscreen;		///<	フルスクリーンにするかどうか
+
+	bool		useKeyMouse;	///<	キーボード・マウスデバイスを使用するか
+	bool		useJoyStick;	///<	ジョイスティックデバイスを使用するか
+	bool		useTrackball;	///<	トラックボールインタフェースを使用するか
+	bool		useDragger;		///<	ドラッガーインタフェースを使用するか
+
+	FWWinDesc(int w=640, int h=480, int l=-1, int t=-1, int p=0, bool f=false):
+		width(w), height(h), left(l), top(t), parentWindow(p), fullscreen(f){
+		title			= "Springhead2";
+		useKeyMouse		= true;
+		useJoyStick		= false;
+		useTrackball	= true;
+		useDragger		= true;
+	}
+};
+
+/**	@brief ウィンドウ
+	window ID, scene, renderを持つ．
+ */
+struct FWWinIf : ObjectIf{
+	SPR_IFDEF(FWWin);
+
+	/// 位置を変更する
+	void	SetPosition(int left, int top);
+	/// サイズを変更する
+	void	SetSize(int width, int height);
+	/// タイトルを変更する
+	void	SetTitle(UTString title);
+	/// タイトルを取得する
+	UTString GetTitle();
+	/// フルスクリーンにする
+	void	SetFullScreen(bool full = true);
+	/// フルスクリーンかどうか
+	bool	GetFullScreen();
+	/// 位置を取得する
+	Vec2i	GetPosition();
+	/// 幅を取得する
+	int		GetWidth();
+	/// 高さを取得する
+	int		GetHeight();
+	/// IDを取得する
+	int		GetID();
+	/// IDを設定する
+	void	SetID(int newID);
+
+	/// 登録されているレンダラを取得する
+	GRRenderIf*	GetRender();
+	/// レンダラを登録する
+	void		SetRender(GRRenderIf* data);
+	/// 登録されているシーンを取得する
+	FWSceneIf*  GetScene();
+	/// シーンを登録する
+	void		SetScene(FWSceneIf* s);
+
+	/// キーボード・マウスデバイス
+	DVKeyMouseIf*	GetKeyMouse();
+	void			SetKeyMouse(DVKeyMouseIf* dv);
+	/// ジョイスティックデバイス
+	DVJoyStickIf*	GetJoyStick();
+	void			SetJoyStick(DVJoyStickIf* dv);
+	/// トラックボール
+	HITrackballIf*	GetTrackball();
+	void			SetTrackball(HITrackballIf* dv);
+	/// ドラッガー
+	HIDraggerIf*	GetDragger();
+	void			SetDragger(HIDraggerIf* dv);
+
+	/// 描画モード設定
+	void	SetRenderMode(bool ph_or_gr);
+	bool	GetRenderMode();
+	/// 描画
+	void	Display();
+	/// サイズ変更時の処理
+	void	Reshape(int w, int h);
+};
+
+}
+#endif 
---
+++ src/Framework/FWWin.h
@@ -0,0 +1,87 @@
+#ifndef FW_WINDOW_H
+#define FW_WINDOW_H
+
+#include <Framework/SprFWWin.h>
+#include <Foundation/Object.h>
+#include <HumanInterface/SprHITrackball.h>
+#include <HumanInterface/SprHIDragger.h>
+
+namespace Spr{;
+
+struct FWSceneIf;
+
+class FWWin : public Object, public FWWinDesc, public HITrackballCallback, public HIDraggerCallback{
+public:
+	SPR_OBJECTDEF(FWWin);
+
+	int					id;				///< ウィンドウID
+	bool				renderMode;		///< デバッグ描画モード
+	
+	GRRenderIf*			render;			///< レンダラ
+	FWSceneIf*			scene;			///< シーン
+
+	DVKeyMouseIf*		keyMouse;		///< キーボード・マウス
+	DVJoyStickIf*		joyStick;		///< ジョイスティック
+	HITrackballIf*		trackball;		///< トラックボール
+	HIDraggerIf*		dragger;		///< ドラッガー
+
+public:
+	// 属性変更API（派生クラスが実装）
+	virtual void SetPosition(int left, int top){}
+	virtual void SetSize(int width, int height){}
+	virtual void SetTitle(UTString title){}
+	// 属性取得API
+	UTString	GetTitle(){ return title; }
+	void		SetFullScreen(bool full = true){ fullscreen = full;}
+	bool		GetFullScreen(){ return fullscreen; }
+	Vec2i		GetPosition(){ return Vec2i(left, top); }
+	int			GetWidth(){ return width; }
+	int			GetHeight(){ return height; }
+	int			GetID() const{ return id; }
+	void		SetID(int newID){id = newID;}
+	// レンダラとシーンの割当てと取得
+	GRRenderIf*	GetRender(){ return render; }
+	void		SetRender(GRRenderIf* data){render = data;}
+	FWSceneIf*  GetScene(){ return scene; }
+	void		SetScene(FWSceneIf* s){ scene = s; }
+	// HumanInterfaceデバイスの登録と取得
+	DVKeyMouseIf*	GetKeyMouse(){ return keyMouse; }
+	void			SetKeyMouse(DVKeyMouseIf* dv){ keyMouse = dv; }
+	DVJoyStickIf*	GetJoyStick(){ return joyStick; }
+	void			SetJoyStick(DVJoyStickIf* dv){ joyStick = dv; }
+	HITrackballIf*	GetTrackball(){ return trackball; }
+	void			SetTrackball(HITrackballIf* dv){
+		trackball = dv;
+		trackball->SetKeyMouse(keyMouse);
+		trackball->SetCallback(this);
+	}
+	HIDraggerIf*	GetDragger(){ return dragger; }
+	void			SetDragger(HIDraggerIf* dv){
+		dragger = dv;
+		dragger->SetKeyMouse(keyMouse);
+		dragger->SetCallback(this);
+	}
+	// 描画モードの設定と取得
+	void	SetRenderMode(bool ph_or_gr){ renderMode = ph_or_gr; }
+	bool	GetRenderMode(){ return renderMode; }
+	// 描画
+	void	Display();
+	// サイズ変更時処理
+	void	Reshape(int w, int h);
+
+	// HITrackballCallbackの関数
+	virtual void OnUpdatePose(HITrackballIf*, bool dir);
+
+	FWWin(int wid = 0, const FWWinDesc& desc = FWWinDesc()):FWWinDesc(desc), id(wid){
+		render		= 0;
+		scene		= 0;
+		keyMouse	= 0;
+		joyStick	= 0;
+		trackball	= 0;
+		dragger		= 0;
+	}
+	virtual ~FWWin(){}
+};
+
+}
+#endif 
