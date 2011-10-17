bfb1acffff4fd7f3cd6013590971016712beb21c
tsuchida <tsuchida@05cee5c3-a2e9-0310-9523-9dfc2f93dbe1>
2011-10-17 14:42:25 +0000

    ・enumの名前変換にあったバグ修正 ・std::vector< UTRef<～> >といったテンプレートの入れ子には現在対応してない ・コード整理 ・EP_AS_BUILTINSのスイッチを無くした (Utility.i) 　・EP_MODULE_NAME　に名前を指定するとパッケージ名が指定できるようにした 　　・これをbuiltinsに設定すると従来のEP_AS_BUILTINSと同じ 　・EP_USE_SUBMODULE　をdefineするとPhysics,Creatureといったモジュールごとにまとめて出力される
    git-svn-id: http://springhead.info/spr2/Springhead2/trunk@5306 05cee5c3-a2e9-0310-9523-9dfc2f93dbe1

--- src/EmbPython/RunSwig.bat
+++ src/EmbPython/RunSwig.bat
@@ -8,7 +8,7 @@ call .\EmbPythonSwig.bat Collision Foundation
 call .\EmbPythonSwig.bat Physics Collision Foundation
 call .\EmbPythonSwig.bat Graphics Physics Foundation
 call .\EmbPythonSwig.bat Creature Graphics Physics Foundation
-call .\EmbPythonSwig.bat HumanInterface Foundation
+call .\EmbPythonSwig.bat HumanInterface Graphics Foundation
 call .\EmbPythonSwig.bat Framework FileIO HumanInterface Graphics Physics Foundation
 
 exit 0
