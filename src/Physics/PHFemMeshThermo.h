efe7b320d0042d595ae5768a42a4a73e593ea930
fumihiro <fumihiro@05cee5c3-a2e9-0310-9523-9dfc2f93dbe1>
2011-10-28 16:40:28 +0000

    S_3：熱伝達境界条件を設定する関数を実装
    git-svn-id: http://springhead.info/spr2/Springhead2/trunk@5336 05cee5c3-a2e9-0310-9523-9dfc2f93dbe1

--- src/Physics/PHFemMeshThermo.h
+++ src/Physics/PHFemMeshThermo.h
@@ -33,9 +33,29 @@ public:
 	virtual bool GetDesc(void* desc) const;
 	///デスクリプタの設定。
 	virtual void SetDesc(const void* desc);
-	///熱伝導シミュレーションでエンジンが用いるステップ
+	///熱伝導シミュレーションでエンジンが用いるステップ		//(オーバーライド)
 	void Step(double dt);
 
+	void SetVerticesTemp(double temp);					//（節点温度の行列を作成する前に）頂点の温度を設定する（単位摂氏℃）
+	void SetVerticesTemp(unsigned i,double temp);		// 節点iの温度をtemp度に設定し、それをTVEcAllに反映
+	void SetLocalFluidTemp(unsigned i,double temp);		//	接点iの周囲の節点温度をtemp度に設定
+
+	//%%%%%%%%		熱伝導境界条件の設定関数の宣言		%%%%%%%%//
+	void SetThermalBoundaryCondition();				//	熱伝導境界条件の設定
+
+	//	使い方
+	//	温度を加えたい節点や節点周りの流体は、その節点のx,y,z座標から特定する。
+	//	その際に以下の関数を用いれば良い。
+	void UsingFixedTempBoundaryCondition(unsigned id,double temp);			//	温度固定境界条件:S_1	指定節点の温度を変える
+	void UsingHeatTransferBoundaryCondition(unsigned id,double temp);		//	熱伝達境界条件:S_3		指定節点の周囲流体温度を変える	
+
+
+
+
+protected:
+	//PHFemMeshThermo内のみで用いる計算
+
+	//%%%%%%%%		行列の宣言・定義		%%%%%%%%//	
 	//行列の生成　小文字は要素剛性行列、大文字は全体剛性行列
 	//節点温度ベクトルの	温度の	TVec:要素（縦）ベクトル(4x1)	TVecall:全体（縦）ベクトル(nx1)
 	//熱伝導マトリクスの	k:k1~k3の加算したもの,	k1:体積分項,	面積分の	k2:熱伝達境界条件,	k3:熱輻射境界条件	積分
@@ -75,38 +95,37 @@ public:
 	PTM::VMatrixCol<double> bVecAll;			//ガウスザイデルの計算に用いる定数行列bの縦ベクトル	Rowである必要はあるのか？⇒Colにした
 	//double *constb;								//ガウスザイデルの係数bを入れる配列のポインタ	後で乗り換える
 
-
-
-
-	void SetVerticesTemp(double temp);					//（節点温度の行列を作成する前に）頂点の温度を設定する（単位摂氏℃）
-	void SetVerticesTemp(unsigned i,double temp);		// 節点iの温度をtemp度に設定し、それをTVEcAllに反映
-	void SetLocalFluidTemp(unsigned i,double temp);		//	接点iの周囲の節点温度をtemp度に設定
-	
-	//熱伝達境界条件の時はすべての引数を満たす　温度固定境界条件を用いたいときには、熱伝達率（最後の引数）を入力しない。また、毎Step実行時に特定節点の温度を一定温度に保つようにする。
+	//%%%		関数の宣言		%%%%%%%%//
+	//%%%%%%		熱伝導計算本体		%%%//
+	//	熱伝達境界条件の時はすべての引数を満たす必要がある。
+	//	温度固定境界条件を用いたいときには、熱伝達率（最後の引数）を入力しない。また、毎Step実行時に特定節点の温度を一定温度に保つようにする。
 	void SetInitThermoConductionParam(double thConduct,double roh,double specificHeat,double heatTrans);		//熱伝導率、密度、比熱、熱伝達率などのパラメーターを設定・代入
 
-	void SetThermalBoundaryCondition();				//	熱伝導境界条件の設定
-
-protected:
-	//熱伝導計算本体
-	//[K]熱伝導マトリクスを作る関数群
+	//	[K]:熱伝導マトリクスを作る関数群
 	void CreateMatk1k(Tet tets);				//kimura方式の計算法
 	void CreateMatk1b(Tet tets);				//yagawa1983の計算法の3次元拡張した計算法 b:book の意味
 	void CreateMatk2(Tet tets);					//四面体ごとに作るので、四面体を引数に取る
 	void CreateMatk2array();
 	void CreateMatkLocal();
 	void CreateMatKall();
-
-	void CreateMatcLocal();
+	//	[C]:熱容量マトリクスを作る関数
+	void CreateMatcLocal();						//	matC1,C2,C3・・・毎に分割すべき？
 	void CreateMatc(Tet tets);					//cの要素剛性行列を作る関数
+	//	{F}:熱流束ベクトルを作る関数
 	void CreateVecfLocal();						//
 	void CreateVecf3(Tet tets);					//
+	//	{T}:節点温度ベクトルを作る関数
 	void CreateTempMatrix();					//節点の温度が入った節点配列から、全体縦ベクトルを作る。	この縦行列の節点の並び順は、i番目の節点IDがiなのかな
 	void CreateLocalMatrixAndSet();				//K,C,Fすべての行列・ベクトルについて要素剛性行列を作って、エッジに入れる	又は	全体剛性行列を作る関数
 	
+	//	初期化
+	void InitTcAll();							//	Tcの温度を初期化
+
+
 	//	何用に用いる？	行列作成の関数をまとめるだけ？
 	void CreateMatrix();					
 
+	//	k,c,f共通で用いる計算関数
 	void SetkcfParam(Tet tets);					//エッジや頂点にk,c,fの要素剛性行列の係数を設定する関数	すべての四面体について要素剛性行列を求め、k,c,fに値を入れると、次の要素について処理を実行する	
 	double CalcTriangleArea(int id0, int id2, int id3);		//節点IDを入れると、その点で構成された三角形の面積を求める　四面体での面積分で使用
 	double CalcTetrahedraVolume(Tet tets);		////四面体のIDを入れると、その体積を計算してくれる関数
@@ -119,18 +138,18 @@ protected:
 	void UpdateVertexTemp(unsigned vtxid);			//	計算結果としての温度をTVecAllから節点に更新する
 	void UpdateVertexTempAll(unsigned size);		//	計算結果としての温度をTVecAllから全節点に更新する
 
+	//	SciLab
 	void ScilabTest();								//	Scilabを使ってみる関数
-	void UsingFixedTempBoundaryCondition(unsigned id,double temp);			//	温度固定境界条件
-	void UsingHeatTransferBoundaryCondition();		//	熱伝達境界条件
 
-	//SciLabで固有値を求めるために全体剛性行列を作る
+	//		全体剛性行列を作る		//	SciLab	で用いる
 	void CreateMatKAll();						//	Kの全体剛性行列
-	void CreateMatCAll();						//	Cの全体剛性行列
+	void CreateMatCAll();						//	Cの全体剛性行
 
 	PTM::TMatrixRow<4,4,double> Create44Mat21();	//共通で用いる、4×4の2と1でできた行列を返す関数
 	//あるいは、引数を入れると、引数を変えてくれる関数
 	PTM::TMatrixCol<4,1,double> Create41Vec1();		//共通で用いる、4×1の1でできた行列(ベクトル)を返す関数
 
+	//%%%%%%%%		変数の宣言		%%%%%%%%//
 	//熱計算に用いるパラメータ
 	double thConduct;				//熱伝導率
 	double heatTrans;				//熱伝達率
@@ -138,6 +157,7 @@ protected:
 	double specificHeat;			//比熱
 //	double dt;						//時間刻み幅
 
+	//%%%%%%%%		バイナリスイッチの宣言		%%%%%%%%//
 	bool deformed;					//形状が変わったかどうか
 	
 };
