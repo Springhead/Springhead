18309c6b088055ab2668f14f7bfdf78cf14e5a0d
susa <susa@05cee5c3-a2e9-0310-9523-9dfc2f93dbe1>
2012-03-27 05:04:52 +0000

    サンプルソースにコメントを追記 不要なサンプルを削除
    git-svn-id: http://springhead.info/spr2/Springhead2/trunk@5550 05cee5c3-a2e9-0310-9523-9dfc2f93dbe1

--- src/Samples/Framework/FWHapticSample/MyApp.cpp
+++ src/Samples/Framework/FWHapticSample/MyApp.cpp
@@ -2,8 +2,10 @@
 
 using namespace Spr;
 
+// ヒューマンインタフェースの切り替え
 // 0:SPIDAR-G6, 1:XBoxController
 #define HUMAN_INTERFACE 0
+// 力覚エンジンの切り替え
 // 0:single, 1:impulsemulti, 2:LD
 #define ENGINETYPE 2
 
@@ -11,6 +13,7 @@ void MyApp::InitInterface(){
 	HISdkIf* hiSdk = GetSdk()->GetHISdk();
 
 #if HUMAN_INTERFACE == 0
+	// x86
 	DRUsb20SimpleDesc usbSimpleDesc;
 	hiSdk->AddRealDevice(DRUsb20SimpleIf::GetIfInfoStatic(), &usbSimpleDesc);
 	DRUsb20Sh4Desc usb20Sh4Desc;
@@ -18,6 +21,7 @@ void MyApp::InitInterface(){
 		usb20Sh4Desc.channel = i;
 		hiSdk->AddRealDevice(DRUsb20Sh4If::GetIfInfoStatic(), &usb20Sh4Desc);
 	}
+	// x64
 	DRCyUsb20Sh4Desc cyDesc;
 	for(int i=0; i<10; ++i){
 		cyDesc.channel = i;
@@ -36,18 +40,16 @@ void MyApp::InitInterface(){
 #endif
 }
 
-
 void MyApp::Init(int argc, char* argv[]){
-		FWApp::Init(argc, argv);
-		PHSdkIf* phSdk = GetSdk()->GetPHSdk();
-		InitInterface();
-		phscene = GetSdk()->GetScene()->GetPHScene();
-		phscene->SetTimeStep(0.05);
+		FWApp::Init(argc, argv);							// アプリケーションの初期化
+		InitInterface();									// インタフェースの初期化
+		PHSdkIf* phSdk = GetSdk()->GetPHSdk();				// シェイプ作成のためにPHSdkへのポインタをとってくる
+		phscene = GetSdk()->GetScene()->GetPHScene();		// 剛体作成のためにPHSceneへのポインタをとってくる
 		
-		Vec3d pos = Vec3d(0, 0, 1.21825);
-		GetCurrentWin()->GetTrackball()->SetPosition(pos);
-		GetSdk()->SetDebugMode(true);
-		
+		Vec3d pos = Vec3d(0, 0, 1.21825);					// カメラ初期位置
+		GetCurrentWin()->GetTrackball()->SetPosition(pos);	// カメラ初期位置の設定
+		GetSdk()->SetDebugMode(true);						// デバック表示の有効化
+			
 		// 床を作成
 		CDBoxDesc bd;
 		bd.boxsize = Vec3f(5.0f, 1.0f, 5.0f);
@@ -66,42 +68,47 @@ void MyApp::Init(int argc, char* argv[]){
 		soBox->SetInertia(soBox->GetShape(0)->CalcMomentOfInertia() * soBox->GetMass());
 		soBox->SetFramePosition(Vec3d(-0.5 , -0.35, 0.0));
 
+		// 力覚ポインタの作成
+		pointer = phscene->CreateHapticPointer();	// 力覚ポインタの作成
 		CDSphereDesc cd;
 		cd.radius = 0.1f;
 		cd.material.mu = 0.4;
 		bd.boxsize = Vec3f(0.2f, 0.2f, 0.2f);
-		pointer = phscene->CreateHapticPointer();
-		pointer->AddShape(phSdk->CreateShape(cd));
+		pointer->AddShape(phSdk->CreateShape(bd));	// シェイプの追加
+		pointer->AddShape(phSdk->CreateShape(cd));	// シェイプの追加
 		Posed defaultPose;
 		defaultPose.Pos() = Vec3d(0.0, -0.35, 0.0);
-		pointer->SetDefaultPose(defaultPose);
-		pointer->SetHumanInterface(spg);
-		pointer->SetInertia(pointer->GetShape(0)->CalcMomentOfInertia());
-		pointer->SetLocalRange(0.1);
-		pointer->SetPosScale(50);
-		pointer->SetReflexSpring(5000);
-		pointer->SetReflexDamper(0.1 * 0.0);
-		pointer->EnableFriction(false);
+		pointer->SetDefaultPose(defaultPose);		// 力覚ポインタ初期姿勢の設定
+		pointer->SetHumanInterface(spg);			// インタフェースの設定
+		pointer->SetInertia(pointer->GetShape(0)->CalcMomentOfInertia());	// 慣性テンソルの設定
+		pointer->SetLocalRange(0.1);				// 局所シミュレーション範囲の設定
+		pointer->SetPosScale(50);					// 力覚ポインタの移動スケールの設定
+		pointer->SetReflexSpring(5000);				// バネ係数の設定
+		pointer->SetReflexDamper(0.1 * 0.0);		// ダンパ係数の設定
+		pointer->EnableFriction(false);				// 摩擦を有効にするかどうか
 
-		PHHapticEngineIf* he = phscene->GetHapticEngine();
-		he->EnableHapticEngine(true);
+		PHHapticEngineIf* he = phscene->GetHapticEngine();	// 力覚エンジンをとってくる
+		he->EnableHapticEngine(true);						// 力覚エンジンの有効化
 
 #if ENGINETYPE == 0
+		// シングルスレッドモード
 		he->SetHapticEngineMode(PHHapticEngineDesc::SINGLE_THREAD);
 		phscene->SetTimeStep(0.001);
 #elif ENGINETYPE == 1
+		// マルチスレッドモード
 		he->SetHapticEngineMode(PHHapticEngineDesc::MULTI_THREAD);
 		phscene->SetTimeStep(0.02);
 #elif ENGINETYPE == 2
+		// 局所シミュレーションモード
 		he->SetHapticEngineMode(PHHapticEngineDesc::LOCAL_DYNAMICS);
 		phscene->SetTimeStep(0.02);
 #endif
-		physicsTimerID = GetTimer(0)->GetID();
-		GetTimer(0)->SetMode(UTTimerIf::IDLE);
-		UTTimerIf* timer = CreateTimer(UTTimerIf::MULTIMEDIA);
-		timer->SetResolution(1);					// 分解能(ms)
-		timer->SetInterval(1);	// 刻み(ms)h
-		hapticTimerID = timer->GetID();
+		physicsTimerID = GetTimer(0)->GetID();					// 物理スレッドのタイマIDの取得
+		GetTimer(0)->SetMode(UTTimerIf::IDLE);					// 物理スレッドのタイマをIDLEモードに設定
+		UTTimerIf* timer = CreateTimer(UTTimerIf::MULTIMEDIA);	// 力覚スレッド用のマルチメディアタイマを作成
+		timer->SetResolution(1);		// 分解能(ms)
+		timer->SetInterval(1);			// 刻み(ms)h
+		hapticTimerID = timer->GetID();	// 力覚スレッドのタイマIDの取得
 		timer->Start();		// タイマスタート
 }
 
@@ -118,9 +125,6 @@ void MyApp::TimerFunc(int id){
 #else
 void MyApp::TimerFunc(int id){
 	if(hapticTimerID == id){
-		//static float time = 0;
-		//time += 0.001;
-		//CSVOUT << time << "," << sobox->GetFramePosition().x << std::endl;
 		phscene->Step();
 	}else if(physicsTimerID == id){
 		PostRedisplay();
@@ -130,25 +134,30 @@ void MyApp::TimerFunc(int id){
 
 
 void MyApp::Keyboard(int key, int x, int y){
+	// 各スレッドの共有メモリのアクセス違反回避のために全てのタイマをとめる
 	for(int i = 0; i < NTimers(); i++)	GetTimer(i)->Stop();
 	switch(key){
 		case 'q':
+			// アプリケーションの終了
 			exit(0);
 			break;
 		case '1':
 			{
+				// レンダリングモードをPenaltyに
 				DSTR << "Penalty mode" << std::endl;
 				pointer->SetHapticRenderMode(PHHapticPointerDesc::PENALTY);
 				break;
 			}
 		case '2':
 			{
+				// レンダリングモードをConstraintに
 				DSTR << "Constraint mode" << std::endl;
 				pointer->SetHapticRenderMode(PHHapticPointerDesc::CONSTRAINT);
 				break;
 			}
 		case 'c':
 			{
+				// インタフェースのキャリブレーション
 				spg->Calibration();
 				DSTR << "CameraPosition" << std::endl;
 				DSTR << GetCurrentWin()->GetTrackball()->GetPosition() << std::endl;
@@ -156,6 +165,7 @@ void MyApp::Keyboard(int key, int x, int y){
 			break;
 		case 'f':
 			{
+				// 力覚提示のON/OFF
 				if(pointer){
 					static bool bForce = false;
 					if(!bForce){
@@ -172,6 +182,7 @@ void MyApp::Keyboard(int key, int x, int y){
 			break;
 		case 'v':
 			{
+				// 振動提示のON/OFF
 				if(pointer){
 					static bool bVibration = false;
 					if(!bVibration){
@@ -188,6 +199,7 @@ void MyApp::Keyboard(int key, int x, int y){
 			break;
 		case 'd':
 			{
+				// デバック表示のON/OFF
 				static bool bDebug = false;
 				if(!bDebug){
 					bDebug = true;
@@ -202,6 +214,7 @@ void MyApp::Keyboard(int key, int x, int y){
 			break;
 		case'a':
 			{
+				// バネ係数を100増やす
 				if(pointer){
 					float spring = pointer->GetReflexSpring();
 					spring += 100.0;
@@ -212,6 +225,7 @@ void MyApp::Keyboard(int key, int x, int y){
 			break;
 		case'z':
 			{
+				// バネ係数を100減らす
 				if(pointer){
 					float spring = pointer->GetReflexSpring();
 					spring -= 100.0;
@@ -222,6 +236,7 @@ void MyApp::Keyboard(int key, int x, int y){
 			break;
 		case ' ':
 			{
+				// 新たに剛体を生成する
 				CDBoxDesc bd;
 				bd.boxsize.clear(0.4f);
 				PHSolidIf* box = phscene->CreateSolid();
@@ -233,5 +248,6 @@ void MyApp::Keyboard(int key, int x, int y){
 		default:
 			break;
 	}
+	// 全てのタイマを始動
 	for(int i = 0; i < NTimers(); i++)	GetTimer(i)->Start();
 }
--- src/Samples/Framework/FWHapticSample/MyApp.h
+++ src/Samples/Framework/FWHapticSample/MyApp.h
@@ -8,16 +8,16 @@ using namespace Spr;
 
 class MyApp : public FWApp{
 public:
-	PHSceneIf* phscene;
-	PHHapticPointerIf* pointer;
-	int physicsTimerID;
-	int hapticTimerID;
-	UTRef<HIBaseIf> spg;
+	PHSceneIf* phscene;			// PHSceneへのポインタ
+	PHHapticPointerIf* pointer; // 力覚ポインタへのポインタ
+	int physicsTimerID;			// 物理スレッドのタイマ
+	int hapticTimerID;			// 力覚スレッドのタイマ
+	UTRef<HIBaseIf> spg;		// 力覚インタフェースへのポインタ
 
-	void InitInterface();
-	void Init(int argc = 0, char* argv[] = 0);
-	void Keyboard(int key, int x, int y);
-	virtual void TimerFunc(int id);
+	void InitInterface();						// 力覚インタフェースの初期化
+	void Init(int argc = 0, char* argv[] = 0);	// アプリケーションの初期化
+	void Keyboard(int key, int x, int y);		// キーボード関数
+	virtual void TimerFunc(int id);				// 各タイマが呼ぶコールバック関数
 };
 
 
