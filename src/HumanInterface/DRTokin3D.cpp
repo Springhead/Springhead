1ca2bed5eeea8b1c16ea48975ae7c3b110f26f3a
hase <hase@05cee5c3-a2e9-0310-9523-9dfc2f93dbe1>
2012-04-20 03:27:42 +0000

    git-svn-id: http://springhead.info/spr2/Springhead2/trunk@5576 05cee5c3-a2e9-0310-9523-9dfc2f93dbe1

--- src/HumanInterface/DRTokin3D.cpp
+++ src/HumanInterface/DRTokin3D.cpp
@@ -19,18 +19,11 @@ bool DRTokin3D::Init(){
 	SetName("Tokin 3D motion sensor");
 	return false;
 }
-
-void DRTokin3D::GetMatrix(Matrix3f& ori){
-
-}
-
-Vec3f DRTokin3D::GetEuler(){
-	return Vec3f(-FLT_MAX,-FLT_MAX,-FLT_MAX);
-}
-
-int DRTokin3D::GetPort(int ch){
-	return -1;
-}
-
+void DRTokin3D::GetMatrix(Matrix3f& ori){}
+Vec3f DRTokin3D::GetEuler(){return Vec3f(-FLT_MAX,-FLT_MAX,-FLT_MAX);}
+int DRTokin3D::GetPort(int ch){return -1;}
+void DRTokin3D::GetMatrix(Matrix3f& ori){}
+Vec3f DRTokin3D::GetEuler(){ return Vec3f(-FLT_MAX,-FLT_MAX,-FLT_MAX); }
+int DRTokin3D::GetPort(int ch){ return -1; }
 }	//	namespace Spr
-#endif USE_CLOSED_SRC
+#endif
