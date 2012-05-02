1380f175983a9f1ea93b79a5b4e5544446a89811
mitake <mitake@05cee5c3-a2e9-0310-9523-9dfc2f93dbe1>
2012-05-02 07:54:56 +0000

    git-svn-id: http://springhead.info/spr2/Springhead2/trunk@5616 05cee5c3-a2e9-0310-9523-9dfc2f93dbe1

--- src/EmbPython/RunSwig.bat
+++ src/EmbPython/RunSwig.bat
@@ -1,13 +1,21 @@
-setlocal enabledelayedexpansion
-@echo off
-call .\EmbPythonSwig.bat Base
-call .\EmbPythonSwig.bat Foundation
-call .\EmbPythonSwig.bat FileIO Foundation
-call .\EmbPythonSwig.bat Collision Foundation
-call .\EmbPythonSwig.bat Physics Collision Foundation
-call .\EmbPythonSwig.bat Graphics Physics Foundation
-call .\EmbPythonSwig.bat Creature Graphics Physics Foundation
-call .\EmbPythonSwig.bat HumanInterface Graphics Foundation
-call .\EmbPythonSwig.bat Framework FileIO HumanInterface Graphics Physics Foundation
+set MAKEFILE=EmbPython.mak.txt
+set PATHORG=%PATH%;
+set PATH=%PATH%;..\..\bin;..\..\bin\swig
 
-exit 0
+echo #	Do not edit. RunSwig.bat will update this file.> %MAKEFILE%
+echo all: %MODULE%Stub.cpp>>%MAKEFILE%
+echo %MODULE%Stub.cpp: %SRCINTF% %SRCIMP%>>%MAKEFILE%
+echo 	call .\EmbPythonSwig.bat Base >>%MAKEFILE%
+echo 	call .\EmbPythonSwig.bat Foundation >>%MAKEFILE%
+echo 	call .\EmbPythonSwig.bat FileIO Foundation >>%MAKEFILE%
+echo 	call .\EmbPythonSwig.bat Collision Foundation >>%MAKEFILE%
+echo 	call .\EmbPythonSwig.bat Physics Collision Foundation >>%MAKEFILE%
+echo 	call .\EmbPythonSwig.bat Graphics Physics Foundation >>%MAKEFILE%
+echo 	call .\EmbPythonSwig.bat Creature Graphics Physics Foundation >>%MAKEFILE%
+echo 	call .\EmbPythonSwig.bat HumanInterface Graphics Foundation >>%MAKEFILE%
+echo 	call .\EmbPythonSwig.bat Framework FileIO HumanInterface Graphics Physics Foundation >>%MAKEFILE%
+
+make -f%MAKEFILE%
+
+set PATH=%PATHORG%
+set PATHORG=
