0a6d3f55a800904a2249c3320248ea4b879d8f67
mitake <mitake@05cee5c3-a2e9-0310-9523-9dfc2f93dbe1>
2012-05-02 10:18:27 +0000

    with tar
    git-svn-id: http://springhead.info/spr2/Springhead2/trunk@5618 05cee5c3-a2e9-0310-9523-9dfc2f93dbe1

--- MakePack.bat
+++ MakePack.bat
@@ -5,6 +5,12 @@ svn export svn+ssh://springhead/home/svn/spr2/Springhead2/trunk ./Springhead2
 
 cd springhead2
 
+rem 公開できないファイルの削除と設定変更
+del /F /S /Q closed
+echo #undef USE_CLOSED_SRC > include/SprDefs.h
+
+set PATH=%PATH%;bin
+
 minitar -c -z9 ../Springhead2.tgz *.*
 cd ..
 cd ..
