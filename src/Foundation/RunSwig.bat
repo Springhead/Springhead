80e6342f779fe9b3fcb384a9a7db8286a21c92cc
mitake <mitake@05cee5c3-a2e9-0310-9523-9dfc2f93dbe1>
2012-06-19 15:01:26 +0000

    git-svn-id: http://springhead.info/spr2/Springhead2/trunk@5655 05cee5c3-a2e9-0310-9523-9dfc2f93dbe1

--- src/Foundation/RunSwig.bat
+++ src/Foundation/RunSwig.bat
@@ -11,7 +11,30 @@ set MAKEFILE=%MODULE%Stub.mak.txt
 rem makefileÇÃçÏê¨Ç∆MakeÇÃé¿çs
 set PATHORG=%PATH%;
 
-set PATH=..\..\bin;..\..\bin\swig
+
+rem Create SJIS World
+set PATH=..\..\bin;
+if not exist ..\..\swigtemp mkdir ..\..\swigtemp
+if not exist ..\..\swigtemp\src mkdir ..\..\swigtemp\src
+if not exist ..\..\swigtemp\include mkdir ..\..\swigtemp\include
+if not exist ..\..\swigtemp\src\Foundation mkdir ..\..\swigtemp\src\Foundation
+if not exist ..\..\swigtemp\include\Base mkdir ..\..\swigtemp\include\Base
+
+for %%p in (%TARGET%) do if not exist ..\..\swigtemp\src\%%p mkdir ..\..\swigtemp\src\%%p
+for %%p in (%TARGET%) do if not exist ..\..\swigtemp\include\%%p mkdir ..\..\swigtemp\include\%%p
+
+for %%p in (%TARGET%) do for %%f in (..\..\include\%%p\*.h) do nkf -s < ..\..\include\%%p\%%f > ..\..\swigtemp\include\%%p\%%f
+for %%p in (%TARGET%) do for %%f in (..\%%p\*.h) do nkf -s < ..\%%p\%%f > ..\..\swigtemp\src\%%p\%%f
+rem for %%p in (%TARGET%) do for %%f in (..\%%p\*.i) do nkf -s < ..\%%p\%%f > ..\..\swigtemp\src\%%p\%%f
+nkf -s < ..\..\include\Springhead.h > ..\..\swigtemp\include\Springhead.h
+nkf -s < ..\..\include\base\Env.h > ..\..\swigtemp\include\base\Env.h
+nkf -s < ..\..\include\Base\BaseDebug.h > ..\..\swigtemp\include\Base\BaseDebug.h
+nkf -s < ..\..\src\Foundation\UTTypeDesc.h > ..\..\swigtemp\src\Foundation\UTTypeDesc.h
+
+cd ..\..\swigtemp\src\Foundation
+set PATH=..\..\..\bin;..\..\..\bin\swig
+
+
 set SRCINTF=
 for %%p in (%TARGET%) do for %%f in (../../include/%%p/*.h) do set SRCINTF=!SRCINTF! ../../include/%%p/%%f
 set SRCIMP=
@@ -34,7 +57,9 @@ echo #	Do not edit. RunSwig.bat will update this file.> %MAKEFILE%
 echo all: %MODULE%Stub.cpp>>%MAKEFILE%
 echo %MODULE%Stub.cpp: %SRCINTF% %SRCIMP%>>%MAKEFILE%
 echo 	swig -spr -w312,319,325,401,402 -DSWIG_%MODULE% -c++ %MODULE%.i>>%MAKEFILE%
-echo 	copy Spr%MODULE%Decl.hpp ..\..\include\%MODULE% >>%MAKEFILE%
+echo 	copy Spr%MODULE%Decl.hpp ..\..\..\include\%MODULE% >>%MAKEFILE%
+echo 	copy %MODULE%Stub.cpp ..\..\..\src\%MODULE% >>%MAKEFILE%
+echo 	copy %MODULE%Decl.hpp ..\..\..\src\%MODULE% >>%MAKEFILE%
 make -f%MAKEFILE%
 set PATH=%PATHORG%
 set PATHORG=
