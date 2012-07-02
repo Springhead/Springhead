b22d68aabb6a72c08d9cc902143c6e0a6ea00cc9
hase <hase@05cee5c3-a2e9-0310-9523-9dfc2f93dbe1>
2012-07-02 07:52:24 +0000

    git-svn-id: http://springhead.info/spr2/Springhead2/trunk@5678 05cee5c3-a2e9-0310-9523-9dfc2f93dbe1

--- src/Foundation/RunSwig.bat
+++ src/Foundation/RunSwig.bat
@@ -55,8 +55,8 @@ echo %%include "../../src/Foundation/UTTypeDesc.h">> %MODULE%.i
 for %%p in (%SRCIMP%) do echo %%include "%%p">> %MODULE%.i
 
 echo #	Do not edit. RunSwig.bat will update this file.> %MAKEFILE%
-echo all: %MODULE%Stub.cpp>>%MAKEFILE%
-echo %MODULE%Stub.cpp: %SRCINTFDEP% %SRCIMPDEP%>>%MAKEFILE%
+echo all: ..\..\..\src\%MODULE%\%MODULE%Stub.cpp>>%MAKEFILE%
+echo ..\..\..\src\%MODULE%\%MODULE%Stub.cpp: %SRCINTFDEP% %SRCIMPDEP%>>%MAKEFILE%
 echo 	swig -spr -w312,319,325,401,402 -DSWIG_%MODULE% -c++ %MODULE%.i>>%MAKEFILE%
 echo 	copy Spr%MODULE%Decl.hpp ..\..\..\include\%MODULE% >>%MAKEFILE%
 echo 	copy %MODULE%Stub.cpp ..\..\..\src\%MODULE% >>%MAKEFILE%
