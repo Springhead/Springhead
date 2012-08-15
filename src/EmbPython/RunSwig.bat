ff39aaad646003a5dadc3df8d13a0823a9508cc0
hase <hase@05cee5c3-a2e9-0310-9523-9dfc2f93dbe1>
2012-08-15 12:36:44 +0000

    SWIGをコメント部分をUTF8対応に、また先頭にUTF8のBOM（3バイト）が付いていても動くように修正。 RunSWIGでswigをUTFのままで動かすように修正。
    git-svn-id: http://springhead.info/spr2/Springhead2/trunk@5749 05cee5c3-a2e9-0310-9523-9dfc2f93dbe1

--- src/EmbPython/RunSwig.bat
+++ src/EmbPython/RunSwig.bat
@@ -8,32 +8,16 @@ set PLACES=include src
 
 
 
-rem Create swigtemp folders.
-if not exist ..\..\swigtemp mkdir ..\..\swigtemp
-for %%P in (%PLACES%) do if not exist ..\..\swigtemp\%%P mkdir ..\..\swigtemp\%%P
-for %%P in (%PLACES%) do for %%M in (%MODULES%) do if not exist ..\..\swigtemp\%%P\%%M mkdir ..\..\swigtemp\%%P\%%M
-
-rem Convert
-for %%P in (%PLACES%) do for %%M in (%MODULES%) do for %%F in (..\..\%%P\%%M\*.h) do for %%G in (..\..\..\swigtemp\%%P\%%M\%%~nxF) do if "%%~tF" GTR "%%~tG" nkf -s %%F >..\..\swigtemp\%%P\%%M\%%~nxF
-
-if not exist ..\..\swigtemp\src\EmbPython\Utility mkdir ..\..\swigtemp\src\EmbPython\Utility
-for %%f in (..\EmbPython\*.bat) do copy ..\EmbPython\%%f ..\..\swigtemp\src\EmbPython\%%f
-for %%f in (..\EmbPython\*.i) do copy ..\EmbPython\%%f ..\..\swigtemp\src\EmbPython\%%f
-for %%f in (..\EmbPython\Utility\*.cpp) do nkf -s < %%f > ..\..\swigtemp\src\EmbPython\%%f
-for %%f in (..\EmbPython\Utility\*.i)   do nkf -s < %%f > ..\..\swigtemp\src\EmbPython\%%f
-
-
-cd ..\..\swigtemp\src\EmbPython
-set PATH=%PATH%;..\..\..\bin;..\..\..\bin\swig
+set PATH=%PATH%;..\..\bin;..\..\bin\swig
 
 set SRCINTF=
-for %%p in (%TARGET%) do for %%f in (../../include/%%p/*.h) do set SRCINTF=!SRCINTF! ../../../include/%%p/%%f
+for %%p in (%TARGET%) do for %%f in (../../include/%%p/*.h) do set SRCINTF=!SRCINTF! ../../include/%%p/%%f
 set SRCIMP=
-for %%p in (%TARGET%) do for %%f in (../%%p/*.h) do set SRCIMP=!SRCIMP! ../../../src/%%p/%%f
+for %%p in (%TARGET%) do for %%f in (../%%p/*.h) do set SRCIMP=!SRCIMP! ../../src/%%p/%%f
 
 echo #	Do not edit. RunSwig.bat will update this file.> %MAKEFILE%
-echo all: ..\..\..\src\EmbPython\EPBase.cpp>>%MAKEFILE%
-echo ..\..\..\src\EmbPython\EPBase.cpp: %SRCINTF% %SRCIMP%>>%MAKEFILE%
+echo all: ..\..\src\EmbPython\EPBase.cpp>>%MAKEFILE%
+echo ..\..\src\EmbPython\EPBase.cpp: %SRCINTF% %SRCIMP%>>%MAKEFILE%
 echo 	call .\EmbPythonSwig.bat Base >>%MAKEFILE%
 echo 	call .\EmbPythonSwig.bat Foundation >>%MAKEFILE%
 echo 	call .\EmbPythonSwig.bat FileIO Foundation >>%MAKEFILE%
@@ -46,6 +30,5 @@ echo 	call .\EmbPythonSwig.bat Framework FileIO HumanInterface Graphics Physics 
 
 make -f%MAKEFILE%
 
-cd ..\..\..\src\EmbPython
 set PATH=%PATHORG%
 set PATHORG=
--- src/Foundation/RunSwig.bat
+++ src/Foundation/RunSwig.bat
@@ -13,25 +13,7 @@ set PATHORG=%PATH%;
 
 
 rem Create SJIS World
-set PATH=..\..\bin;
-if not exist ..\..\swigtemp mkdir ..\..\swigtemp
-if not exist ..\..\swigtemp\src mkdir ..\..\swigtemp\src
-if not exist ..\..\swigtemp\include mkdir ..\..\swigtemp\include
-if not exist ..\..\swigtemp\src\Foundation mkdir ..\..\swigtemp\src\Foundation
-if not exist ..\..\swigtemp\include\Base mkdir ..\..\swigtemp\include\Base
-
-for %%p in (%TARGET%) do if not exist ..\..\swigtemp\src\%%p mkdir ..\..\swigtemp\src\%%p
-for %%p in (%TARGET%) do if not exist ..\..\swigtemp\include\%%p mkdir ..\..\swigtemp\include\%%p
-
-for %%p in (%TARGET%) do for %%f in (..\..\include\%%p\*.h) do nkf -s < ..\..\include\%%p\%%f > ..\..\swigtemp\include\%%p\%%f
-for %%p in (%TARGET%) do for %%f in (..\%%p\*.h) do nkf -s < ..\%%p\%%f > ..\..\swigtemp\src\%%p\%%f
-nkf -s < ..\..\include\Springhead.h > ..\..\swigtemp\include\Springhead.h
-nkf -s < ..\..\include\base\Env.h > ..\..\swigtemp\include\base\Env.h
-nkf -s < ..\..\include\Base\BaseDebug.h > ..\..\swigtemp\include\Base\BaseDebug.h
-nkf -s < ..\..\src\Foundation\UTTypeDesc.h > ..\..\swigtemp\src\Foundation\UTTypeDesc.h
-
-cd ..\..\swigtemp\src\Foundation
-set PATH=..\..\..\bin;..\..\..\bin\swig
+set PATH=..\..\bin;..\..\bin\swig
 
 
 set SRCINTF=
@@ -40,9 +22,9 @@ set SRCIMP=
 for %%p in (%TARGET%) do for %%f in (../%%p/*.h) do set SRCIMP=!SRCIMP! ../%%p/%%f
 
 set SRCINTFDEP=
-for %%p in (%TARGET%) do for %%f in (../../include/%%p/*.h) do set SRCINTFDEP=!SRCINTFDEP! ../../../include/%%p/%%f
+for %%p in (%TARGET%) do for %%f in (../../include/%%p/*.h) do set SRCINTFDEP=!SRCINTFDEP! ../../include/%%p/%%f
 set SRCIMPDEP=
-for %%p in (%TARGET%) do for %%f in (../%%p/*.h) do set SRCIMPDEP=!SRCIMPDEP! ../../../src/%%p/%%f
+for %%p in (%TARGET%) do for %%f in (../%%p/*.h) do set SRCIMPDEP=!SRCIMPDEP! ../../src/%%p/%%f
 
 echo #	Do not edit. RunSwig.bat will update this file.> %MODULE%.i
 echo %%module %MODULE%>> %MODULE%.i
@@ -58,9 +40,6 @@ echo #	Do not edit. RunSwig.bat will update this file.> %MAKEFILE%
 echo all: ..\..\..\src\%MODULE%\%MODULE%Stub.cpp>>%MAKEFILE%
 echo ..\..\..\src\%MODULE%\%MODULE%Stub.cpp: %SRCINTFDEP% %SRCIMPDEP%>>%MAKEFILE%
 echo 	swig -spr -w312,319,325,401,402 -DSWIG_%MODULE% -c++ %MODULE%.i>>%MAKEFILE%
-echo 	copy Spr%MODULE%Decl.hpp ..\..\..\include\%MODULE% >>%MAKEFILE%
-echo 	copy %MODULE%Stub.cpp ..\..\..\src\%MODULE% >>%MAKEFILE%
-echo 	copy %MODULE%Decl.hpp ..\..\..\src\%MODULE% >>%MAKEFILE%
 make -f%MAKEFILE%
 set PATH=%PATHORG%
 set PATHORG=
