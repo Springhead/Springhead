12e2fc1b12fae1aa30bd440a3c2782b8680268dc
hase <hase@05cee5c3-a2e9-0310-9523-9dfc2f93dbe1>
2012-08-23 02:42:08 +0000

    git-svn-id: http://springhead.info/spr2/Springhead2/trunk@5767 05cee5c3-a2e9-0310-9523-9dfc2f93dbe1

--- src/Foundation/RunSwig.bat
+++ src/Foundation/RunSwig.bat
@@ -39,7 +39,7 @@ for %%p in (%SRCIMP%) do echo %%include "%%p">> %MODULE%.i
 echo #	Do not edit. RunSwig.bat will update this file.> %MAKEFILE%
 echo all: ..\..\src\%MODULE%\%MODULE%Stub.cpp>>%MAKEFILE%
 echo ..\..\src\%MODULE%\%MODULE%Stub.cpp: %SRCINTFDEP% %SRCIMPDEP%>>%MAKEFILE%
-echo 	swig -spr -w312,319,325,401,402 -DSWIG_%MODULE% -c++ %MODULE%.i>>%MAKEFILE%
+echo 	swig -spr -w305,312,319,325,401,402 -DSWIG_%MODULE% -c++ %MODULE%.i>>%MAKEFILE%
 make -f%MAKEFILE%
 set PATH=%PATHORG%
 set PATHORG=
