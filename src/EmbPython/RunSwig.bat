ef5022012b1b91d436992ab2995ef542f014b1e8
mitake <mitake@05cee5c3-a2e9-0310-9523-9dfc2f93dbe1>
2012-11-15 06:37:37 +0000

    EmbPythonSwigのバッチファイルにバグがあったので修正．APIヘッダ変更時に正しく自動実行されるようになったはず．
    git-svn-id: http://springhead.info/spr2/Springhead2/trunk@5946 05cee5c3-a2e9-0310-9523-9dfc2f93dbe1

--- src/EmbPython/RunSwig.bat
+++ src/EmbPython/RunSwig.bat
@@ -3,30 +3,6 @@ set MAKEFILE=EmbPython.mak.txt
 set PATHORG=%PATH%;
 
 set PATH=%PATH%;..\..\bin
-set MODULES=Base Foundation Collision Physics FileIO EmbPython Framework Graphics GraphicsD3D HumanInterface
-set PLACES=include src
-
-
-
-set PATH=%PATH%;..\..\bin;..\..\bin\swig
-
-set SRCINTF=
-for %%p in (%TARGET%) do for %%f in (../../include/%%p/*.h) do set SRCINTF=!SRCINTF! ../../include/%%p/%%f
-set SRCIMP=
-for %%p in (%TARGET%) do for %%f in (../%%p/*.h) do set SRCIMP=!SRCIMP! ../../src/%%p/%%f
-
-echo #	Do not edit. RunSwig.bat will update this file.> %MAKEFILE%
-echo all: ..\..\src\EmbPython\EPBase.cpp>>%MAKEFILE%
-echo ..\..\src\EmbPython\EPBase.cpp: %SRCINTF% %SRCIMP%>>%MAKEFILE%
-echo 	call .\EmbPythonSwig.bat Base >>%MAKEFILE%
-echo 	call .\EmbPythonSwig.bat Foundation >>%MAKEFILE%
-echo 	call .\EmbPythonSwig.bat FileIO Foundation >>%MAKEFILE%
-echo 	call .\EmbPythonSwig.bat Collision Foundation >>%MAKEFILE%
-echo 	call .\EmbPythonSwig.bat Physics Collision Foundation >>%MAKEFILE%
-echo 	call .\EmbPythonSwig.bat Graphics Physics Foundation >>%MAKEFILE%
-echo 	call .\EmbPythonSwig.bat Creature Graphics Physics Foundation >>%MAKEFILE%
-echo 	call .\EmbPythonSwig.bat HumanInterface Graphics Foundation >>%MAKEFILE%
-echo 	call .\EmbPythonSwig.bat Framework FileIO HumanInterface Graphics Physics Foundation >>%MAKEFILE%
 
 make -f%MAKEFILE%
 
