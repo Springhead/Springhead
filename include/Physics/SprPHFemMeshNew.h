5a0a0cc77898060c7a26089764ad0c37a7d98722
susa <susa@05cee5c3-a2e9-0310-9523-9dfc2f93dbe1>
2012-11-21 07:02:16 +0000

    コミットミスしました。
    git-svn-id: http://springhead.info/spr2/Springhead2/trunk@5966 05cee5c3-a2e9-0310-9523-9dfc2f93dbe1

---
+++ include/Physics/SprPHFemMeshNew.h
@@ -0,0 +1,40 @@
+/*
+ *  Copyright (c) 2003-2012, Shoichi Hasegawa and Springhead development team 
+ *  All rights reserved.
+ *  This software is free software. You can freely use, distribute and modify this 
+ *  software. Please deal with this software under one of the following licenses: 
+ *  This license itself, Boost Software License, The MIT License, The BSD License.   
+ */
+/**
+ *	@file SprPHFemMeshNew.h
+ *	@brief 剛体
+*/
+#ifndef SPR_PH_FEMMESH_NEW_IF_H
+#define SPR_PH_FEMMESH_NEW_IF_H
+
+#include <Foundation/SprObject.h>
+
+/**	\addtogroup gpPhysics	*/
+//@{
+namespace Spr{;
+
+/// PHFemMeshNewへのインタフェース
+struct PHFemMeshNewIf : public SceneObjectIf{
+	SPR_IFDEF(PHFemMeshNew);
+};
+
+///	FemMeshのステート
+struct PHFemMeshNewState{};
+
+///	FemMeshのディスクリプタ
+struct PHFemMeshNewDesc: public PHFemMeshNewState{
+	SPR_DESCDEF(PHFemMeshNew);
+	std::vector<Vec3d> vertices;
+	std::vector<int> tets;
+	std::vector<int> faces;
+};
+
+//@}
+
+}	//	namespace Spr
+#endif
