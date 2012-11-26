fcfbc3561323b3b181d68aa9b6751bbeb30186db
susa <susa@05cee5c3-a2e9-0310-9523-9dfc2f93dbe1>
2012-11-26 06:41:27 +0000

    include/SprDefs.hでUSE_CLOSED_SRCをdefineしなおしました。 USE_CLOSED_SRCはmakepack時にundefしてコミットされていました。 また、USE_CLOSED_SRCが#ifdefされているコードにSprDefs.hをインクルードするようにしました。
    git-svn-id: http://springhead.info/spr2/Springhead2/trunk@5977 05cee5c3-a2e9-0310-9523-9dfc2f93dbe1

--- src/HumanInterface/DRCyUsb20Sh4.cpp
+++ src/HumanInterface/DRCyUsb20Sh4.cpp
@@ -5,7 +5,7 @@
  *  software. Please deal with this software under one of the following licenses: 
  *  This license itself, Boost Software License, The MIT License, The BSD License.   
  */
-//#include <Springhead.h>
+#include <SprDefs.h>
 #ifdef USE_CLOSED_SRC
 #include "../../closed/HumanInterface/DRCyUsb20Sh4.cpp"
 #else
@@ -13,7 +13,10 @@
 namespace Spr {;
 DRCyUsb20Sh4::DRCyUsb20Sh4(const DRCyUsb20Sh4Desc& d):DRUsb20Sh4(d), sendBuf(NULL), recvBuf(NULL){}
 DRCyUsb20Sh4::~DRCyUsb20Sh4(){}
-void* DRCyUsb20Sh4::UsbOpen(int id){ return 0; }
+void* DRCyUsb20Sh4::UsbOpen(int id){ 
+	DSTR << "You have to define USE_CLOSED_SRC SprDefs.h to use Spidar" << std::endl;
+	assert(0);
+	return 0; }
 bool DRCyUsb20Sh4::UsbClose(void*& h){ return false; }
 void DRCyUsb20Sh4::Reset(){}
 void DRCyUsb20Sh4::SetupBuffer(){}
