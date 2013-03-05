06f10319f69851da855124f633afde0bf60073bf
susa <susa@05cee5c3-a2e9-0310-9523-9dfc2f93dbe1>
2013-03-05 16:52:48 +0000

    Scilabを5.4に対応させました。 まだ、ScilabMatrix()関数は機能していません。 Scilabを利用するのに必要な関数はextern宣言されていないので、 swigのdll.cppのextern宣言されている関数のみをグルーコードとしてはくを全てはくに変更しました。
    git-svn-id: http://springhead.info/spr2/Springhead2/trunk@6349 05cee5c3-a2e9-0310-9523-9dfc2f93dbe1

--- src/Foundation/Scilab.i
+++ src/Foundation/Scilab.i
@@ -1,7 +1,21 @@
 %module Scilab
-%include "../../include/Scilab/machine.h"
-%include "../../include/Scilab/interf/stack1.h"
-%include "../../include/Scilab/interf/stack2.h"
-%include "../../include/Scilab/interf/stack3.h"
-extern int TerminateScilab(char *ScilabQuit);
-extern int SendScilabJob(char *job);
+//%include "../../include/Scilab/core/includes/stack-c.h"
+//%include "../../include/Scilab/call_scilab/includes/call_scilab.h"
+%include "../../include/Scilab/call_scilab/includes/dynlib_call_scilab.h"
+%include "../../include/Scilab/api_scilab/includes/api_scilab.h"
+#define __INTERNAL_API_SCILAB__
+//%include "../../include/Scilab/api_scilab/includes/api_common.h"
+%include "../../include/Scilab/api_scilab/includes/api_double.h"
+%include "../../include/Scilab/api_scilab/includes/api_string.h"
+%include "../../include/Scilab/api_scilab/includes/api_int.h"
+%include "../../include/Scilab/api_scilab/includes/api_poly.h"
+%include "../../include/Scilab/api_scilab/includes/api_sparse.h"
+%include "../../include/Scilab/api_scilab/includes/api_boolean.h"
+%include "../../include/Scilab/api_scilab/includes/api_boolean_sparse.h"
+%include "../../include/Scilab/api_scilab/includes/api_pointer.h"
+%include "../../include/Scilab/api_scilab/includes/api_list.h"
+%include "../../include/Scilab/api_scilab/includes/api_error.h"
+%include "../../include/Scilab/api_scilab/includes/dynlib_api_scilab.h"
+BOOL TerminateScilab(char *ScilabQuit);
+int SendScilabJob(char *job);
+
--- src/Samples/SciLab/SciLab.cpp
+++ src/Samples/SciLab/SciLab.cpp
@@ -7,65 +7,67 @@
  */
 
 /* ScilabとSpringhead2をリンクさせるためのサンプル
-動作必要要件：LibScilab.dll(Scilabインストール時にscilab\bin内にあるはず）
-scilab-5.3.3で動作確認、別バージョンを利用したい場合はScilabStub.cpp:StartScilab()内に
-pathを記述する必要がある。
+動作必要要件：api_scilab.dll, call_scilab.dll(Scilabインストール時にscilab\bin内にあるはず）
+scilab-5.4で動作確認、別バージョンを利用したい場合はScilabStub.cpp:StartScilab()内にpathを記述する必要がある。
+dllをdynamic linkして利用するため、ScilabSwig.batで中間コードScilabStub.cpp, ScilabStub.hpp, ScilabStubImpl.cxxを生成する必要がある。
 また、Releaseモードで実行する場合にはdllの呼び出し規約を_cdeclにする必要がある。
 その変更はScilabStub.cpp, ScilabStub.hpp, ScilabStubImpl.cxxで宣言されている関数ポインタを_cdeclで呼び出しにする。
-これらのファイルはswigで自動生成されるので、参考ファイルとしてScilabStub.hpp.org, ScilabStubImpl.cxx.orgを参照すること。
+ScilabStub.hpp, ScilabStubImpl.cxxはswigで自動生成されるので、参考ファイルとしてScilabStub.hpp.org, ScilabStubImpl.cxx.orgを参照すること。
 */
 #include <SciLab/SprSciLab.h>
 #include <Base/Affine.h>
+#include <conio.h>
 using namespace Spr;
 
-int main(){
+int _cdecl main(){
 	if (!ScilabStart()){
 		std::cout << "Error : ScilabStart \n";
 	}
 	//	行列の読み書き
+	std::cout << "Springheadメモリ空間内に行列を生成" << std::endl;
 	Matrix2f A;
 	A.Ex() = Vec2f(1,2);
 	A.Ey() = Vec2f(3,4);
 	std::cout << "A:  " << A ;
-	std::cout << "A00:" << A[0][0] << std::endl;
-	std::cout << "A01:" << A[0][1] << std::endl;
-	std::cout << "A10:" << A[1][0] << std::endl;
-	std::cout << "A11:" << A[1][1] << std::endl;
+	std::cout << "A00:" << A[0][0] << std::endl;	std::cout << "A01:" << A[0][1] << std::endl;
+	std::cout << "A10:" << A[1][0] << std::endl;	std::cout << "A11:" << A[1][1] << std::endl;
 	
+	std::cout << std::endl;
+	std::cout << "Scilabメモリ空間内に行列を生成し計算" << std::endl;
 	ScilabSetMatrix("A", A);
 	ScilabJob("b=[4;5]");
-	std::cout << "A=";
-	ScilabJob("disp(A);");
-	std::cout << "b=";
-	ScilabJob("disp(b);");
-	std::cout << "x=A\\b" << std::endl;
-	ScilabJob("A,b,x=A\\b;");
-	
-	ScilabGetMatrix(A, "A");
-	SCMatrix b = ScilabMatrix("b");
-	SCMatrix x = ScilabMatrix("x");
-	std::cout << "x:" << x << std::endl;
+	std::cout << "A=";		ScilabJob("disp(A);");
+	std::cout << "b=";		ScilabJob("disp(b);");
+	std::cout << "Ax=b" << std::endl;		
+	ScilabJob("x=A\\b;");
+	std::cout << "x=";		ScilabJob("disp(x);");
 
-	ScilabJob("y = A;");
-	SCMatrix y = ScilabMatrix("y");
-	std::cout << "y=" << y;
-	y = 2*A;
-	std::cout << "y = 2*A is done by C++ code" << std::endl;
-	std::cout << "y=";
-	ScilabJob("disp(y);");
-	std::cout << A;
-	std::cout << y;
-	ScilabJob("clear;");
+	std::cout << std::endl;
+	std::cout << "Scilabメモリ空間内の行列をSpringheadメモリ空間へコピー" << std::endl;
+	PTM::VMatrixRow<double> b, x;
+	ScilabGetMatrix(b, "b");
+	ScilabGetMatrix(x, "x");
+	std::cout << "A=" << A << std::endl;
+	std::cout << "b=" << b << std::endl;
+	std::cout << "x=" << x << std::endl;
 
-	//	グラフ描画
-	ScilabJob("t = 0:0.01:2*3.141592653;");
-	ScilabJob("x = sin(t);");
-	ScilabJob("y = cos(t);");
-	ScilabJob("plot2d(x, y);");
-	for(int i=0; i<100000; ++i){
-		ScilabJob("");
-	}
+	//std::cout << "Scilabメモリ空間内の行列をSpringheadで計算" << std::endl;
+	//ScilabJob("y = A;");
+	//SCMatrix y = ScilabMatrix("y");
+	//y = 2*A;
+	//std::cout << "y = 2*A : Done by C++ code" << std::endl;
+	//std::cout << "y=";	ScilabJob("disp(y);");
+	//ScilabJob("clear;");
+
+	////	グラフ描画
+	//ScilabJob("t = 0:0.01:2*3.141592653;");
+	//ScilabJob("x = sin(t);");
+	//ScilabJob("y = cos(t);");
+	//ScilabJob("plot2d(x, y);");
+	//for(int i=0; i<100000; ++i){
+	//	ScilabJob("");
+	//}
 
 	ScilabEnd();
-	return 0;
+	if(_getch())	return 0;
 }
