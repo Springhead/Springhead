6f3d1cb51e5bd9981d7a163fd3b48da25807a375
susa <susa@05cee5c3-a2e9-0310-9523-9dfc2f93dbe1>
2013-03-05 16:52:48 +0000

    Scilabを5.4に対応させました。 まだ、ScilabMatrix()関数は機能していません。 Scilabを利用するのに必要な関数はextern宣言されていないので、 swigのdll.cppのextern宣言されている関数のみをグルーコードとしてはくを全てはくに変更しました。
    git-svn-id: http://springhead.info/spr2/Springhead2/trunk@6349 05cee5c3-a2e9-0310-9523-9dfc2f93dbe1

--- src/Foundation/ScilabStub.cpp
+++ src/Foundation/ScilabStub.cpp
@@ -4,49 +4,22 @@
 
 namespace Scilab{
 static Spr::UTDllLoader dllLoader;
-#define SWIGDLL_GETPROC(x)	dllLoader.GetProc(x)
-
+static Spr::UTDllLoader dllLoader2;
+#define SWIGDLL_GETPROC(x) ((dllLoader.GetProc(x)) ? dllLoader.GetProc(x) : dllLoader2.GetProc(x))
 #include "ScilabStubImpl.cxx"
-int (_cdecl *StartScilabOrg)(char *SCIpath,char *ScilabStartup,int *Stacksize);
 
-#define SC_IMP_STACKVAR(x) SC##x* p##x
-SC_IMP_STACKVAR(stack);
-SC_IMP_STACKVAR(vstk);
-SC_IMP_STACKVAR(recu);
-SC_IMP_STACKVAR(iop);
-SC_IMP_STACKVAR(errgst);
-SC_IMP_STACKVAR(com);
-SC_IMP_STACKVAR(adre);
-SC_IMP_STACKVAR(intersci);
-SC_IMP_STACKVAR(cha1);
-SC_IMP_STACKVAR(dbg);
-#undef SC_IMP_STACKVAR
+int (_cdecl *StartScilabOrg)(char *SCIpath, char *ScilabStartup,int *Stacksize);
+StrCtx** ppvApiCtx;
 
 bool StartScilab(char* SCIpath, char *ScilabStartup,int *Stacksize){
-	if (!dllLoader.Load("LibScilab.dll", 
-		"C:\\Program Files (x86)\\scilab-5.3.3\\bin;"
-		"C:\\Program Files\\scilab-5.3.3\\bin;D:\\Program Files\\scilab-5.0.3\\bin;"
-		"C:\\Program Files\\scilab-5.0.3\\bin;D:\\Program Files\\scilab-5.0.3\\bin;"
-		"C:\\Program Files (x86)\\scilab-5.0.3\\bin;"
-		"C:\\Program Files\\scilab-4.1.2\\bin;D:\\Program Files\\scilab-4.1.2\\bin;"
-		"C:\\Program Files (x86)\\scilab-4.1.2\\bin;"
-		)) return false;
+	char* dllPath = "C:\Program Files\scilab-5.4.0\bin;C:\Program Files (x86)\scilab-5.4.0\bin";
+	if(!dllLoader.Load("call_Scilab.dll", dllPath)) return false;
+	if(!dllLoader2.Load("api_Scilab.dll", dllPath)) return false;
+
 	InitScilabFunc();
+	*(void **)&ppvApiCtx = SWIGDLL_GETPROC("pvApiCtx");
 	*(void **)&StartScilabOrg = SWIGDLL_GETPROC("StartScilab");
 	if (!StartScilabOrg) return false;
-
-#define SC_INIT_STACKVAR(x) *(void**)&p##x = SWIGDLL_GETPROC(#x "_");
-	SC_INIT_STACKVAR(stack);
-	SC_INIT_STACKVAR(vstk);
-	SC_INIT_STACKVAR(recu);
-	SC_INIT_STACKVAR(iop);
-	SC_INIT_STACKVAR(errgst);
-	SC_INIT_STACKVAR(com);
-	SC_INIT_STACKVAR(adre);
-	SC_INIT_STACKVAR(intersci);
-	SC_INIT_STACKVAR(cha1);
-	SC_INIT_STACKVAR(dbg);
-#undef SC_INIT_STACKVAR
 
 	return StartScilabOrg(SCIpath, ScilabStartup, Stacksize) != 0;
 }
