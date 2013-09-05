4bd652a5bb8024e26bca8ac66a57dacede2810ab
(no author) <(no author)@05cee5c3-a2e9-0310-9523-9dfc2f93dbe1>
2013-09-05 02:41:50 +0000

    dailybuild: EmbPython version control の修正
    git-svn-id: http://springhead.info/spr2/Springhead2/trunk@6708 05cee5c3-a2e9-0310-9523-9dfc2f93dbe1

--- src/EmbPython/CurrEPVersion.bat
+++ src/EmbPython/CurrEPVersion.bat
@@ -1,2 +1,6 @@
-@echo off                             
-set CURR_PY_VERSION=32      
+@echo off
+setlocal
+set PY_VERSION=%1
+if "%PY_VERSION%" equ "" set PY_VERSION=32
+endlocal && set CURR_PY_VERSION=%PY_VERSION%
+
