49fa40e1a2022752e240638417b62913e2c97570
(no author) <(no author)@05cee5c3-a2e9-0310-9523-9dfc2f93dbe1>
2013-11-18 07:46:00 +0000

    dailybuild: Doc: HTML/CHM 両方の作成
    git-svn-id: http://springhead.info/spr2/Springhead2/trunk@6841 05cee5c3-a2e9-0310-9523-9dfc2f93dbe1

--- include/SpringheadDoc.bat
+++ include/SpringheadDoc.bat
@@ -1,4 +1,5 @@
 set PATHORG=%PATH%
 set PATH=..\BIN;..\BIN\Graphviz\bin;%PATH%
+(type springhead.doxy & echo GENERATE_HTMLHELP=YES) | doxygen - 2> springhead_tree.log
 doxygen springhead.doxy 2> springhead.log
 set PATH=%PATHORG%
