8a498e1f36ae46ebe00093a77dbdb477d8d9d784
tazaki <tazaki@05cee5c3-a2e9-0310-9523-9dfc2f93dbe1>
2014-04-01 12:31:11 +0000

    git-svn-id: http://springhead.info/spr2/Springhead2/trunk@7100 05cee5c3-a2e9-0310-9523-9dfc2f93dbe1

---
+++ include/Physics/SprPHJointMotor.h
@@ -0,0 +1,67 @@
+﻿/*
+ *  Copyright (c) 2003-2008, Shoichi Hasegawa and Springhead development team 
+ *  All rights reserved.
+ *  This software is free software. You can freely use, distribute and modify this 
+ *  software. Please deal with this software under one of the following licenses: 
+ *  This license itself, Boost Software License, The MIT License, The BSD License.   
+ */
+/**
+ *	@file SprPHJointLimit.h
+ *	@brief 関節可動域制限
+*/
+#ifndef SPR_PHJOINTMOTORIf_H
+#define SPR_PHJOINTMOTORIf_H
+
+#include <Foundation/SprObject.h>
+
+namespace Spr{;
+
+/** \addtogroup gpJoint */
+//@{
+
+/// 1自由度関節モータのインタフェース
+struct PH1DJointMotorIf : public SceneObjectIf{
+	SPR_IFDEF(PH1DJointMotor);
+
+};
+///	1自由度関節モータのデスクリプタ
+struct PH1DJointMotorDesc{
+	PH1DJointMotorDesc() {}
+};
+
+// -----  -----  -----  -----  -----  -----  -----  -----  -----  -----  -----  -----  -----  ----- 
+
+/// ボールジョイントモータのインタフェース
+struct PHBallJointMotorIf : public SceneObjectIf {
+	SPR_IFDEF(PHBallJointMotor);
+
+};
+	
+/// ボールジョイントモータのデスクリプタ
+struct PHBallJointMotorDesc{
+	SPR_DESCDEF(PHBallJointMotor);
+
+	PHBallJointMotorDesc() {
+	}
+};
+
+// -----  -----  -----  -----  -----  -----  -----  -----  -----  -----  -----  -----  -----  ----- 
+
+/// ボールジョイントモータのインタフェース
+struct PHSpringMotorIf : public SceneObjectIf {
+	SPR_IFDEF(PHSpringMotor);
+
+};
+	
+/// ボールジョイントモータのデスクリプタ
+struct PHSpringMotorDesc{
+	SPR_DESCDEF(PHSpringMotor);
+
+	PHSpringMotorDesc() {
+	}
+};
+
+
+}
+
+#endif//SPR_PHJOINTMOTORIf_H
