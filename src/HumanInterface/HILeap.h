f339b43fb398dbb58c67a853fb1d351f3d082d0c
mitake <mitake@05cee5c3-a2e9-0310-9523-9dfc2f93dbe1>
2014-10-20 07:12:12 +0000

    git-svn-id: http://springhead.info/spr2/Springhead2/trunk@7562 05cee5c3-a2e9-0310-9523-9dfc2f93dbe1

--- src/HumanInterface/HILeap.h
+++ src/HumanInterface/HILeap.h
@@ -4,7 +4,6 @@
  *  This software is free software. You can freely use, distribute and modify this 
  *  software. Please deal with this software under one of the following licenses: 
  *  This license itself, Boost Software License, The MIT License, The BSD License.   
-
  */
 #ifndef HI_LEAP_H
 #define HI_LEAP_H
