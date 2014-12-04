e8e2c817ec1a45084a0b4d653b202afc3cba817c
kanehori <kanehori@05cee5c3-a2e9-0310-9523-9dfc2f93dbe1>
2014-12-04 05:56:17 +0000

    dailybuild: テスト実行不正対策 　(1) src/tests/Graphics/GRSimple: VS2013: x64/Release,x64/Trace: 組み込み関数の使用を中止 　(2) src/tests/Graphics/GRColorSpheres: VS2010: x86/Trace: Physics module のみ ReleaseDll 構成を使用
    git-svn-id: http://springhead.info/spr2/Springhead2/trunk@7633 05cee5c3-a2e9-0310-9523-9dfc2f93dbe1

--- src/Samples/EmbPython/MyEPApp/EPMyEPApp.h
+++ src/Samples/EmbPython/MyEPApp/EPMyEPApp.h
@@ -1,10 +1,6 @@
 #ifndef EPMYEPAPP_H
 #define EPMYEPAPP_H
 
-//{*********EPFWApp*******
-void toEPFWApp( EPObject* obj);
-//}EPFWApp
-
 //{*********EPSampleApp*******
 void toEPSampleApp( EPObject* obj);
 //}EPSampleApp
@@ -15,12 +11,9 @@ void toEPHogeshi( EPObject* obj);
 
 //{*********EPMyEPApp*******
 void toEPMyEPApp( EPObject* obj);
-PyObject* __PYDECL EPMyEPApp_CallbackFunc( PyObject* self );
 PyObject* __PYDECL EPMyEPApp_Drop( PyObject* self,PyObject* arg );
-PyObject* __PYDECL EPMyEPApp_GetWin( PyObject* self );
 PyObject* __PYDECL EPMyEPApp_OnAction( PyObject* self,PyObject* arg );
 PyObject* __PYDECL EPMyEPApp_OnDraw( PyObject* self,PyObject* arg );
 PyObject* __PYDECL EPMyEPApp_OnStep( PyObject* self );
-PyObject* __PYDECL EPMyEPApp_SetCallbackFunc( PyObject* self,PyObject* arg );
 //}EPMyEPApp
 #endif
