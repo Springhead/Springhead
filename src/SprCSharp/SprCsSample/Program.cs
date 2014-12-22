2b2e33fba0c84ee56cbe26bfe075573e389d7dc8
mitake <mitake@05cee5c3-a2e9-0310-9523-9dfc2f93dbe1>
2014-12-22 04:37:22 +0000

    C#用APIのテスト
    git-svn-id: http://springhead.info/spr2/Springhead2/trunk@7665 05cee5c3-a2e9-0310-9523-9dfc2f93dbe1

---
+++ src/SprCSharp/SprCsSample/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Diagnostics;
+using Spr;
+
+namespace SprCsSample {
+    class Program {
+        static void Main(string[] args) {
+            Vec3d v1 = new Vec3d(1, 2, 3), v2 = new Vec3d(4, 5, 6);
+            while (true) {
+                System.Console.WriteLine(v1 + v2);
+            }
+        }
+    }
+}
