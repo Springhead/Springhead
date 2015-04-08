0cc8a709f4d6bb36f8cbb44c2078bcc4760e287a
kanehori <kanehori@05cee5c3-a2e9-0310-9523-9dfc2f93dbe1>
2015-04-08 03:13:43 +0000

    git-svn-id: http://springhead.info/spr2/Springhead2/trunk@7825 05cee5c3-a2e9-0310-9523-9dfc2f93dbe1

---
+++ src/SprCSharp/RunSwig_CSharp/CSharpSwig.bat
@@ -0,0 +1,85 @@
+@echo off
+:: ===================================================================================
+::  File:
+::	RunSwig.bat
+::
+::  Description:
+::	Springhead のライブラリ(DLL) を C# から利用するためのコードを生成する。
+::
+::  Version:
+::	Ver 1.0	 2015/01/26 F.Kanehori	初版
+:: ===================================================================================
+setlocal enabledelayedexpansion
+set DEBUG=0
+if %DEBUG% == 1 (
+    set CS_INFO=SprInfo
+    if not exist !CS_INFO! mkdir !CS_INFO!
+)
+
+set MODULE=%1
+if "%MODULE%" equ "" (
+    echo Error: Module name MUST be specified.
+    exit /b
+)
+
+set DUMPTREE=0
+if "%1" equ "dumptree" set DUMPTREE=1
+
+:: ----------
+::  各種定義
+:: ----------
+:: ディレクトリの定義
+::
+call :set_abspath SRCDIR ..\..\src
+set CS_SRC=SprCSharp
+set CS_IMP=SprImport
+set CS_EXP=SprExport
+
+echo. 
+echo *** %MODULE% ***
+echo using src directory: %SRCDIR%
+
+:: 使用するパス
+::
+set SWIGPATH=..\..\bin\swig
+set PATH=%SWIGPATH%;%PATH% 
+
+:: 使用するプログラム名
+::
+set SWIG=swig
+set ARGS=-sprcs -c++ -I%SWIGPATH%\Lib -w305,312,319,325,401,402
+
+:: ----------
+::  処理開始
+:: ----------
+if exist %MODULE%.i (
+    if %DUMPTREE% == 1 (
+        cmd /c %SWIG% -debug-module 4 !MODULE!.i > CS%MODULE%.tree
+    ) else (
+	cmd /c %SWIG% %ARGS% -DSWIG_%MODULE% !MODULE!.i
+	move /Y !MODULE!Cs.cs  %CS_SRC%\CS%MODULE%.cs  > NUL 2>&1
+	move /Y !MODULE!CsP.cs %CS_IMP%\CS%MODULE%.cs  > NUL 2>&1
+	move /Y !MODULE!Cs.cpp %CS_EXP%\CS%MODULE%.cpp > NUL 2>&1
+	echo %CS_SRC%\CS%MODULE%.cs  created
+	echo %CS_IMP%\CS%MODULE%.cs  created
+	echo %CS_EXP%\CS%MODULE%.cpp created
+	if %DEBUG% == 1 (
+	    move /Y %MODULE%CS.info %CS_INFO%\CS%MODULE%.info >NUL 2>&1
+	) else (
+	    del %MODULE%CS.info 2>NUL
+	)
+    )
+) else (
+    echo "%MODULE%.i" not found
+)
+echo. 
+
+:: ----------
+::  処理終了
+:: ----------
+endlocal
+exit /b
+
+:set_abspath
+    set %1=%~f2
+exit /b
