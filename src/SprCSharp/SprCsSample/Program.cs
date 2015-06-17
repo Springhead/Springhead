96b9cd1218c38ddbaa1314509f14fae4e2b85dc5
kanehori <kanehori@05cee5c3-a2e9-0310-9523-9dfc2f93dbe1>
2015-06-17 07:57:51 +0000

    C# Interface: in debugging
    git-svn-id: http://springhead.info/spr2/Springhead2/trunk@7934 05cee5c3-a2e9-0310-9523-9dfc2f93dbe1

--- src/SprCSharp/SprCsSample/Program.cs
+++ src/SprCSharp/SprCsSample/Program.cs
@@ -9,12 +9,48 @@ namespace SprCsSample {
     class Program {
         static void Main(string[] args) {
             PHSceneDesc descScene = new PHSceneDesc();
-            //descScene.airResistanceRate = 0.5;
-            //descScene.gravity = new Vec3d(0, 0, -4.5);
+            PHRaycastHit raycastHit = new PHRaycastHit();
+            descScene.numIteration = 123;
+            System.Console.WriteLine("int:      expected: 123   result: " + descScene.numIteration);
+            descScene.bCCDEnabled = true;
+            System.Console.WriteLine("bool:     expected: true  result: " + descScene.bCCDEnabled);
+            raycastHit.distance = 0.123F;
+            System.Console.WriteLine("float:    expected: 0.123 result: " + raycastHit.distance);
+            descScene.airResistanceRate = 0.123;
+            System.Console.WriteLine("double:   expected: 0.123 result: " + descScene.airResistanceRate);
 
-            Vec3d d = descScene.gravity;
-            //System.Console.WriteLine(d.ToString());
-            System.Console.WriteLine(descScene.gravity);
+            descScene.gravity.x = 0;
+            descScene.gravity.y = 0;
+            descScene.gravity.z = -4.5;
+/**/        //System.Console.WriteLine("st_elm:   expected: (0.0, 0.0, -4.5)  result: " + descScene.gravity);
+            descScene.gravity = new Vec3d(2.5, -5.2, 0.5);
+/**/        //System.Console.WriteLine("st_assign expected: (2.5, -5.2, 0.5)  result: " + descScene.gravity);
+            
+/**/        // Vec3d d = descScene.gravity;
+/**/        //System.Console.WriteLine(d.ToString());
+/**/        //System.Console.WriteLine(d);
+
+            // vector - intrinsic
+            PHFemMeshNewDesc descFemMeshNew = new PHFemMeshNewDesc();
+            vectorwrapper_int tets = descFemMeshNew.tets;
+
+            tets.push_back(101);
+            tets.push_back(102);
+            System.Console.WriteLine("vec<int>: expected: 2     result: " + tets.size());
+            System.Console.WriteLine("vec<int>: expected: 101   result: " + tets[0]);
+            System.Console.WriteLine("vec<int>: expected: 102   result: " + tets[1]);
+            tets[0] = 201;
+            tets[1] = 202;
+            tets.push_back(203);
+            System.Console.WriteLine("vec<int>: expected: 3     result: " + tets.size());
+            System.Console.WriteLine("vec<int>: expected: 201   result: " + tets[0]);
+            System.Console.WriteLine("vec<int>: expected: 202   result: " + tets[1]);
+            System.Console.WriteLine("vec<int>: expected: 203   result: " + tets[2]);
+
+            // vector -struct
+            vectorwrapper_PHFemMeshNewDesc_Vec3d vertices = descFemMeshNew.vertices;
+            //vertices.push_back(new Vec3d(0.1, 0.2, 0.3));
+            //vertices.push_back(new Vec3d(0.4, 0.5, 0.6));
         }
     }
 }
--- src/SprCSharp/SprImport/wrapper.cs
+++ src/SprCSharp/SprImport/wrapper.cs
@@ -7,51 +7,87 @@ namespace SprCs {
     public partial class SprExport {
         // std::vector
         //  int
-        [DllImport("SprExport.dll")]
+        [DllImport("SprExport.dll", CallingConvention=CallingConvention.Cdecl)]
         public static extern int Spr_vector_get_int(IntPtr _ptr, int index);
-        [DllImport("SprExport.dll")]
+        [DllImport("SprExport.dll", CallingConvention=CallingConvention.Cdecl)]
         public static extern void Spr_vector_set_int(IntPtr _ptr, int index, int value);
+        [DllImport("SprExport.dll", CallingConvention=CallingConvention.Cdecl)]
+        public static extern int Spr_vector_size_int(IntPtr _ptr);
+        [DllImport("SprExport.dll", CallingConvention=CallingConvention.Cdecl)]
+        public static extern void Spr_vector_push_back_int(IntPtr _ptr, int _val);
+        [DllImport("SprExport.dll", CallingConvention=CallingConvention.Cdecl)]
+        public static extern void Spr_vector_clear_int(IntPtr _ptr);
         //  unsigned int
-        [DllImport("SprExport.dll")]
+        [DllImport("SprExport.dll", CallingConvention=CallingConvention.Cdecl)]
         public static extern uint Spr_vector_get_unsigned_int(IntPtr _ptr, int index);
-        [DllImport("SprExport.dll")]
+        [DllImport("SprExport.dll", CallingConvention=CallingConvention.Cdecl)]
         public static extern void Spr_vector_set_unsigned_int(IntPtr _ptr, int index, uint value);
+        [DllImport("SprExport.dll", CallingConvention=CallingConvention.Cdecl)]
+        public static extern int Spr_vector_size_unsigned_int(IntPtr _ptr);
+        [DllImport("SprExport.dll", CallingConvention=CallingConvention.Cdecl)]
+        public static extern void Spr_vector_push_back_unsigned_int(IntPtr _ptr, uint value);
+        [DllImport("SprExport.dll", CallingConvention=CallingConvention.Cdecl)]
+        public static extern void Spr_vector_clear_unsigned_int(IntPtr _ptr);
         //  size_t
-        [DllImport("SprExport.dll")]
+        [DllImport("SprExport.dll", CallingConvention=CallingConvention.Cdecl)]
         public static extern ulong Spr_vector_get_size_t(IntPtr _ptr, int index);
-        [DllImport("SprExport.dll")]
+        [DllImport("SprExport.dll", CallingConvention=CallingConvention.Cdecl)]
         public static extern void Spr_vector_set_size_t(IntPtr _ptr, int index, ulong value);
+        [DllImport("SprExport.dll", CallingConvention=CallingConvention.Cdecl)]
+        public static extern int Spr_vector_size_size_t(IntPtr _ptr);
+        [DllImport("SprExport.dll", CallingConvention=CallingConvention.Cdecl)]
+        public static extern void Spr_vector_push_back_size_t(IntPtr _ptr, ulong value);
+        [DllImport("SprExport.dll", CallingConvention=CallingConvention.Cdecl)]
+        public static extern void Spr_vector_clear_size_t(IntPtr _ptr);
         //  float
-        [DllImport("SprExport.dll")]
+        [DllImport("SprExport.dll", CallingConvention=CallingConvention.Cdecl)]
         public static extern float Spr_vector_get_float(IntPtr _ptr, int index);
-        [DllImport("SprExport.dll")]
+        [DllImport("SprExport.dll", CallingConvention=CallingConvention.Cdecl)]
         public static extern void Spr_vector_set_float(IntPtr _ptr, int index, float value);
+        [DllImport("SprExport.dll", CallingConvention=CallingConvention.Cdecl)]
+        public static extern int Spr_vector_size_float(IntPtr _ptr);
+        [DllImport("SprExport.dll", CallingConvention=CallingConvention.Cdecl)]
+        public static extern void Spr_vector_push_back_float(IntPtr _ptr, float value);
+        [DllImport("SprExport.dll", CallingConvention=CallingConvention.Cdecl)]
+        public static extern void Spr_vector_clear_float(IntPtr _ptr);
         //  double
-        [DllImport("SprExport.dll")]
+        [DllImport("SprExport.dll", CallingConvention=CallingConvention.Cdecl)]
         public static extern double Spr_vector_get_double(IntPtr _ptr, int index);
-        [DllImport("SprExport.dll")]
+        [DllImport("SprExport.dll", CallingConvention=CallingConvention.Cdecl)]
         public static extern void Spr_vector_set_double(IntPtr _ptr, int index, double value);
+        [DllImport("SprExport.dll", CallingConvention=CallingConvention.Cdecl)]
+        public static extern int Spr_vector_size_double(IntPtr _ptr);
+        [DllImport("SprExport.dll", CallingConvention=CallingConvention.Cdecl)]
+        public static extern void Spr_vector_push_back_double(IntPtr _ptr, double value);
+        [DllImport("SprExport.dll", CallingConvention=CallingConvention.Cdecl)]
+        public static extern void Spr_vector_clear_double(IntPtr _ptr);
         //  string
-        [DllImport("SprExport.dll")]
+        [DllImport("SprExport.dll", CallingConvention=CallingConvention.Cdecl)]
         public static extern IntPtr Spr_vector_get_string(IntPtr _ptr, int index);
-        [DllImport("SprExport.dll")]
+        [DllImport("SprExport.dll", CallingConvention=CallingConvention.Cdecl)]
         public static extern void Spr_vector_set_string(IntPtr _ptr, int index, IntPtr value);
+        [DllImport("SprExport.dll", CallingConvention=CallingConvention.Cdecl)]
+        public static extern int Spr_vector_size_string(IntPtr _ptr);
+        [DllImport("SprExport.dll", CallingConvention=CallingConvention.Cdecl)]
+        public static extern void Spr_vector_push_back_string(IntPtr _ptr, string value);
+        [DllImport("SprExport.dll", CallingConvention=CallingConvention.Cdecl)]
+        public static extern void Spr_vector_clear_string(IntPtr _ptr);
 
         // array
         //  int
-        [DllImport("SprExport.dll")]
+        [DllImport("SprExport.dll", CallingConvention=CallingConvention.Cdecl)]
         public static extern int Spr_array_get_int(IntPtr _ptr, int index);
-        [DllImport("SprExport.dll")]
+        [DllImport("SprExport.dll", CallingConvention=CallingConvention.Cdecl)]
         public static extern void Spr_array_set_int(IntPtr _ptr, int index, int value);
         //  float
-        [DllImport("SprExport.dll")]
+        [DllImport("SprExport.dll", CallingConvention=CallingConvention.Cdecl)]
         public static extern float Spr_array_get_float(IntPtr _ptr, int index);
-        [DllImport("SprExport.dll")]
+        [DllImport("SprExport.dll", CallingConvention=CallingConvention.Cdecl)]
         public static extern void Spr_array_set_float(IntPtr _ptr, int index, float value);
         //  double
-        [DllImport("SprExport.dll")]
+        [DllImport("SprExport.dll", CallingConvention=CallingConvention.Cdecl)]
         public static extern double Spr_array_get_double(IntPtr _ptr, int index);
-        [DllImport("SprExport.dll")]
+        [DllImport("SprExport.dll", CallingConvention=CallingConvention.Cdecl)]
         public static extern void Spr_array_set_double(IntPtr _ptr, int index, double value);
     }
 }
