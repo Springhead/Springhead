7e508b14fc03f7944231bdbcb7a726b4c2f3dfee
kanehori <kanehori@05cee5c3-a2e9-0310-9523-9dfc2f93dbe1>
2015-06-18 06:51:07 +0000

    C# Interface: in debugging
    git-svn-id: http://springhead.info/spr2/Springhead2/trunk@7938 05cee5c3-a2e9-0310-9523-9dfc2f93dbe1

--- src/SprCSharp/SprCsSample/Program.cs
+++ src/SprCSharp/SprCsSample/Program.cs
@@ -8,8 +8,23 @@ using SprCs;
 namespace SprCsSample {
     class Program {
         static void Main(string[] args) {
+            bool do_intrinsic = true;
+            bool do_tostring = true;
+            bool do_vector = true;
+            bool do_array = true;
+
+            if (do_intrinsic)   test_intrinsic();
+            if (do_tostring)    test_tostring();
+            if (do_vector)      test_vector();
+            if (do_array)       test_array();
+        }
+
+        static void test_intrinsic() {
+            System.Console.WriteLine("---[ intrinsic ]---");
+            // intrinsic member
             PHSceneDesc descScene = new PHSceneDesc();
             PHRaycastHit raycastHit = new PHRaycastHit();
+            // simple
             descScene.numIteration = 123;
             System.Console.WriteLine("int:      expected: 123   result: " + descScene.numIteration);
             descScene.bCCDEnabled = true;
@@ -18,27 +33,38 @@ namespace SprCsSample {
             System.Console.WriteLine("float:    expected: 0.123 result: " + raycastHit.distance);
             descScene.airResistanceRate = 0.123;
             System.Console.WriteLine("double:   expected: 0.123 result: " + descScene.airResistanceRate);
-
+            // nested
             descScene.gravity.x = 0;
             descScene.gravity.y = 0;
             descScene.gravity.z = -4.5;
-/**/        //System.Console.WriteLine("st_elm:   expected: (0.0, 0.0, -4.5)  result: " + descScene.gravity);
+/**/        System.Console.WriteLine("set by elm:   expected: (0.0, 0.0, -4.5)  result: " + descScene.gravity);
             descScene.gravity = new Vec3d(2.5, -5.2, 0.5);
-/**/        //System.Console.WriteLine("st_assign expected: (2.5, -5.2, 0.5)  result: " + descScene.gravity);
-            
-/**/        // Vec3d d = descScene.gravity;
-/**/        //System.Console.WriteLine(d.ToString());
-/**/        //System.Console.WriteLine(d);
+/**/        System.Console.WriteLine("set struct:   expected: (2.5, -5.2, 0.5)  result: " + descScene.gravity);
+        }
 
-            // vector - intrinsic
+        static void test_tostring() {
+            System.Console.WriteLine("---[ ToString ]---");
+            //PHSceneDesc descScene = new PHSceneDesc();
+/**/        //Vec3d v3d = descScene.gravity;
+            Vec3d v3d = new Vec3d(0.1, 0.2, 0.3);
+/**/        System.Console.WriteLine("ToString: expected: (0.1, 0.2, 0.3)   result: " + v3d.ToString());
+/**/        System.Console.WriteLine("ToString: C# ToString:                        " + v3d);
+        }
+
+        static void test_vector() {
+            System.Console.WriteLine("---[ vector ]---");
+            // vector member
             PHFemMeshNewDesc descFemMeshNew = new PHFemMeshNewDesc();
             vectorwrapper_int tets = descFemMeshNew.tets;
-
+            // intrinsic element
             tets.push_back(101);
             tets.push_back(102);
             System.Console.WriteLine("vec<int>: expected: 2     result: " + tets.size());
             System.Console.WriteLine("vec<int>: expected: 101   result: " + tets[0]);
             System.Console.WriteLine("vec<int>: expected: 102   result: " + tets[1]);
+            tets.clear();
+            tets.push_back(101);
+            tets.push_back(102);
             tets[0] = 201;
             tets[1] = 202;
             tets.push_back(203);
@@ -46,11 +72,27 @@ namespace SprCsSample {
             System.Console.WriteLine("vec<int>: expected: 201   result: " + tets[0]);
             System.Console.WriteLine("vec<int>: expected: 202   result: " + tets[1]);
             System.Console.WriteLine("vec<int>: expected: 203   result: " + tets[2]);
-
-            // vector -struct
+            // structure element
             vectorwrapper_PHFemMeshNewDesc_Vec3d vertices = descFemMeshNew.vertices;
-            //vertices.push_back(new Vec3d(0.1, 0.2, 0.3));
-            //vertices.push_back(new Vec3d(0.4, 0.5, 0.6));
+            vertices.push_back(new Vec3d(0.1, 0.2, 0.3));
+            vertices.push_back(new Vec3d(0.4, 0.5, 0.6));
+            System.Console.WriteLine("vec<Vec3d>: expected: 2     result: " + vertices.size());
+            System.Console.WriteLine("vec<Vec3d>: expected: 0.1   result: " + vertices[0].x);
+            System.Console.WriteLine("vec<Vec3d>: expected: 0.2   result: " + vertices[0].y);
+            System.Console.WriteLine("vec<Vec3d>: expected: 0.3   result: " + vertices[0].z);
+            System.Console.WriteLine("vec<Vec3d>: expected: (0.4, 0.5, 0.6) result: " + vertices[1].ToString());
+        }
+
+        static void test_array() {
+            System.Console.WriteLine("---[ array ]---");
+            PHOpObjDesc descOpObj = new PHOpObjDesc();
+            GRMeshFace meshFace = new GRMeshFace();
+            for (int i = 0; i < 4; i++) {
+                meshFace.indices[i] = 100 + i;
+            }
+            for (int i = 0; i < 4; i++) {
+                System.Console.WriteLine("array<int>: expected: " + (100 + i) + " result: " + meshFace.indices[i]);
+            }
         }
     }
 }
