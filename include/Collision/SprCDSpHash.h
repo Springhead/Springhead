4ad106578b40d0f01595fa357735964ec34683a5
dinghaiyang <dinghaiyang@05cee5c3-a2e9-0310-9523-9dfc2f93dbe1>
2015-07-23 06:35:50 +0000

    suppliment files
    git-svn-id: http://springhead.info/spr2/Springhead2/trunk@8008 05cee5c3-a2e9-0310-9523-9dfc2f93dbe1

---
+++ include/Collision/SprCDSpHash.h
@@ -0,0 +1,23 @@
+#ifndef SPRCDSPHASH_H
+#define SPRCDSPHASH_H
+
+#include <Foundation/SprObject.h>
+#include <Physics\PHOpObj.h>
+
+namespace Spr{;
+
+
+struct CDSpatialHashTableIf : ObjectIf{
+	SPR_IFDEF(CDSpatialHashTable);
+
+	void init(float cellSize,Bounds  bounds);
+	bool ReducedSelectPointsToAdd(Vec3f &pCtr, float radius, int objInd, int pInd, int timestamp);
+};
+
+struct CDSpatialHashTableDesc{
+	
+	
+};
+
+}
+#endif
