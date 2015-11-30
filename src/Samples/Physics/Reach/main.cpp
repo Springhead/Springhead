f613a85b5e6bc9d94179f1754c32e0100360b7d4
tazaki <tazaki@05cee5c3-a2e9-0310-9523-9dfc2f93dbe1>
2015-11-30 13:13:53 +0000

    git-svn-id: http://springhead.info/spr2/Springhead2/trunk@8119 05cee5c3-a2e9-0310-9523-9dfc2f93dbe1

--- src/Samples/Physics/Reach/main.cpp
+++ src/Samples/Physics/Reach/main.cpp
@@ -43,11 +43,11 @@ public:
 		AddHotKey(MENU_SCENE, ID_LEFT, 'l');
 		AddAction(MENU_SCENE, ID_RIGHT, "select right target");
 		AddHotKey(MENU_SCENE, ID_RIGHT, 'r');
-		AddAction(MENU_SCENE, ID_XDIR, "move target along x-axis");
+		AddAction(MENU_SCENE, ID_XDIR, "select x-axis");
 		AddHotKey(MENU_SCENE, ID_XDIR, 'x');
-		AddAction(MENU_SCENE, ID_YDIR, "move target along y-axis");
+		AddAction(MENU_SCENE, ID_YDIR, "select y-axis");
 		AddHotKey(MENU_SCENE, ID_YDIR, 'y');
-		AddAction(MENU_SCENE, ID_ZDIR, "move target along z-axis");
+		AddAction(MENU_SCENE, ID_ZDIR, "select z-axis");
 		AddHotKey(MENU_SCENE, ID_ZDIR, 'z');
 		AddAction(MENU_SCENE, ID_INC, "move target in positive direction");
 		AddHotKey(MENU_SCENE, ID_INC, 'm');
@@ -69,7 +69,7 @@ public:
 		hip = GetFWScene()->GetPHScene()->CreateSolid();
 		hip->AddShape(phSdk->CreateShape(bd));
 		hip->SetFramePosition(Vec3f(0.0, 0.0, 0.0));
-		hip->SetDynamical(false);
+		hip->SetDynamical(false);		
 		// body
 		bd.boxsize = Vec3f(0.15, 0.08, 0.108);
 		body = GetFWScene()->GetPHScene()->CreateSolid();
@@ -78,34 +78,28 @@ public:
 		bd.boxsize = Vec3f(0.18, 0.2, 0.1);
 		chest = GetFWScene()->GetPHScene()->CreateSolid();
 		chest->AddShape(phSdk->CreateShape(bd));
-		// body joints
-		jd.damper = 60.0;
-		jd.spring = 160.0;
-		jd.posePlug.Pos() = Vec3d(0,0.04, 0);
-		jd.posePlug.Ori() = Quaternionf::Rot((float)Rad(90), 'x');
-		jd.poseSocket.Pos() = Vec3d(0, -0.04, 0);
-		jd.poseSocket.Ori() = Quaternionf::Rot((float)Rad(90), 'x');
-		GetFWScene()->GetPHScene()->CreateJoint(body, hip, jd);
-		//
-		jd.posePlug.Pos() = Vec3d(0, 0.04, 0);
-		jd.posePlug.Ori() = Quaternionf::Rot((float)Rad(90), 'y');
-		jd.poseSocket.Pos() = Vec3d(0, -0.1, 0);
-		jd.poseSocket.Ori() = Quaternionf::Rot((float)Rad(90), 'y');
-		GetFWScene()->GetPHScene()->CreateJoint(chest, body, jd);
-
 		// head
 		sd.radius = 0.06f;
 		head = GetFWScene()->GetPHScene()->CreateSolid();
 		head->AddShape(phSdk->CreateShape(sd));
+
+		// body joints
+		jd.damper     = 60.0;
+		jd.spring     = 160.0;
+		jd.poseSocket = Posef(Vec3d(0.0,  0.04, 0.0), Quaternionf::Rot((float)Rad(90), 'x'));
+		jd.posePlug   = Posef(Vec3d(0.0, -0.04, 0.0), Quaternionf::Rot((float)Rad(90), 'x'));
+		GetFWScene()->GetPHScene()->CreateJoint(hip, body, jd);
+		//
+		jd.poseSocket = Posef(Vec3d(0.0,  0.04, 0.0), Quaternionf::Rot((float)Rad(90), 'y'));
+		jd.posePlug   = Posef(Vec3d(0.0, -0.1 , 0.0), Quaternionf::Rot((float)Rad(90), 'y'));
+		GetFWScene()->GetPHScene()->CreateJoint(body, chest, jd);
 		// neck
-		jd.posePlug.Ori() = Quaternionf::Rot((float)Rad(90), 'y');
-		jd.posePlug.Pos() = Vec3d(0, 0.1, 0);
-		jd.poseSocket.Ori() = Quaternionf::Rot((float)Rad(90), 'y');
-		jd.poseSocket.Pos() = Vec3d(0, -0.08, 0);
-		jd.damper = 1.0;
-		jd.spring = 100.0;
+		jd.damper         = 1.0;
+		jd.spring         = 100.0;
+		jd.poseSocket     = Posef(Vec3d(0.0,  0.1 , 0.0), Quaternionf::Rot((float)Rad(90), 'y'));
+		jd.posePlug       = Posef(Vec3d(0.0, -0.08, 0.0), Quaternionf::Rot((float)Rad(90), 'y'));
 		jd.targetPosition = Rad(0);
-		GetFWScene()->GetPHScene()->CreateJoint(head, chest, jd);
+		GetFWScene()->GetPHScene()->CreateJoint(chest, head, jd);
 		
 		// upper arm
 		bd.boxsize = Vec3f(0.049, 0.16, 0.048);
@@ -124,33 +118,29 @@ public:
 		loArmL->AddShape(shapeLoArm);
 		loArmL->SetOrientation(Quaterniond::Rot(Rad(90), 'x'));
 		// shoulder
-		bjd.posePlug.Pos() = Vec3d(0.13, 0.1, 0.0);
-		bjd.poseSocket.Pos() = Vec3d(0, 0.1, 0);
 		bjd.damper = 6.0;
 		bjd.spring = 10.0;
-		(Vec3d&)bjd.targetPosition = Vec3d(Rad(-30), Rad(30), Rad(-30));
-		GetFWScene()->GetPHScene()->CreateJoint(upArmR, chest, bjd);
-		
-		bjd.posePlug.Pos() = Vec3d(-0.13, 0.1, 0.0);
-		bjd.poseSocket.Pos() = Vec3d(0, 0.1, 0);
-		(Vec3d&)bjd.targetPosition = Vec3d(Rad(30), Rad(30), Rad(30));
-		GetFWScene()->GetPHScene()->CreateJoint(upArmL, chest, bjd);
+		bjd.poseSocket.Pos() = Vec3d(0.13, 0.1, 0.0);
+		bjd.posePlug  .Pos() = Vec3d(0.0 , 0.1, 0.0);
+		(Vec3d&)bjd.targetPosition = Vec3d(Rad(-0), Rad(-0), Rad(+0));
+		GetFWScene()->GetPHScene()->CreateJoint(chest, upArmR, bjd);
+		bjd.poseSocket.Pos() = Vec3d(-0.13, 0.1, 0.0);
+		bjd.posePlug  .Pos() = Vec3d( 0.0 , 0.1, 0.0);
+		(Vec3d&)bjd.targetPosition = Vec3d(Rad(-0), Rad(-0), Rad(-0));
+		GetFWScene()->GetPHScene()->CreateJoint(chest, upArmL, bjd);
+
 		// elbow
-		jd.posePlug.Ori() = Quaternionf::Rot((float)Rad(90), 'y');
-		jd.posePlug.Pos() = Vec3d(0, -0.09, 0);
-		jd.poseSocket.Ori() = Quaternionf::Rot((float)Rad(90), 'y');
-		jd.poseSocket.Pos() = Vec3d(0, 0.07, 0);
-		jd.damper = 4.0;
-		jd.spring = 6.0;
-		jd.targetPosition = Rad(-30);
-		GetFWScene()->GetPHScene()->CreateJoint(loArmR, upArmR, jd);
+		jd.damper         = 4.0;
+		jd.spring         = 6.0;
+		jd.poseSocket     = Posef(Vec3d(0, -0.09, 0), Quaternionf::Rot((float)Rad(90), 'y'));
+		jd.posePlug       = Posef(Vec3d(0,  0.07, 0), Quaternionf::Rot((float)Rad(90), 'y'));
+		jd.targetPosition = Rad(+30);
+		GetFWScene()->GetPHScene()->CreateJoint(upArmR, loArmR, jd);
 		
-		jd.posePlug.Ori() = Quaternionf::Rot((float)Rad(90), 'y');
-		jd.posePlug.Pos() = Vec3d(0, -0.09, 0);
-		jd.poseSocket.Ori() = Quaternionf::Rot((float)Rad(90), 'y');
-		jd.poseSocket.Pos() = Vec3d(0, 0.07, 0);
-		jd.targetPosition = Rad(-30);
-		GetFWScene()->GetPHScene()->CreateJoint(loArmL, upArmL, jd);
+		jd.poseSocket     = Posef(Vec3d(0, -0.09, 0), Quaternionf::Rot((float)Rad(90), 'y'));
+		jd.posePlug       = Posef(Vec3d(0,  0.07, 0), Quaternionf::Rot((float)Rad(90), 'y'));
+		jd.targetPosition = Rad(+30);
+		GetFWScene()->GetPHScene()->CreateJoint(upArmL, loArmL, jd);
 
 		// reaching targets
 		sd.radius = 0.02f;
@@ -172,8 +162,16 @@ public:
 		GetFWScene()->GetPHScene()->CreateJoint(targetR, loArmR, spd);
 		GetFWScene()->GetPHScene()->CreateJoint(targetL, loArmL, spd);
 
+		// 関節座標系を作成（不要ならコメントアウト）
+		PHRootNodeIf* root = GetFWScene()->GetPHScene()->CreateTreeNodes(hip);
+		
 		// 重力を設定
-		GetFWScene()->GetPHScene()->SetGravity(Vec3f(0, -9.8, 0));
+		GetFWScene()->GetPHScene()->SetGravity(Vec3f(0, 0, 0));
+
+		//	接触を切る
+		//scene->SetContactMode(&soTarget[0], PHSceneDesc::MODE_NONE);
+		//scene->SetContactMode(&soTarget[1], PHSceneDesc::MODE_NONE);
+		//GetFWScene()->GetPHScene()->SetContactMode(PHSceneDesc::MODE_NONE);
 
 		//
 		GetFWScene()->SetRenderMode(true, false);
@@ -189,10 +187,11 @@ public:
 		GetFWScene()->SetSolidMaterial(GRRenderIf::RED, targetL);
 		GetFWScene()->SetSolidMaterial(GRRenderIf::RED, targetR);
 		GetFWScene()->EnableRenderAxis(false, false, false);
-
-		//	接触を切る
-		//scene->SetContactMode(&soTarget[0], PHSceneDesc::MODE_NONE);
-		//scene->SetContactMode(&soTarget[1], PHSceneDesc::MODE_NONE);
+		
+		// 初期視点
+		GetCurrentWin()->GetTrackball()->SetTarget  (Vec3f(0.0f, 0.2f, 0.0f));
+		GetCurrentWin()->GetTrackball()->SetDistance(2.0);
+		GetCurrentWin()->GetTrackball()->SetAngle   (Rad(135.0), Rad(30.0));
 	}
 
 	virtual void OnAction(int menu, int id){
@@ -240,6 +239,7 @@ public:
 				message = "target moved.";
 			}
 		}
+		SampleApp::OnAction(menu, id);
 	}
 
 } app;
