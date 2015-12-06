f094c8d495c3dcb1095c7f18664bdee6856a6a02
mitake <mitake@05cee5c3-a2e9-0310-9523-9dfc2f93dbe1>
2015-12-06 06:08:22 +0000

    git-svn-id: http://springhead.info/spr2/Springhead2/trunk@8132 05cee5c3-a2e9-0310-9523-9dfc2f93dbe1

---
+++ src/Unity/Assets/Springhead/PHIKBallActuatorBehaviour.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+using SprCs;
+
+public class PHIKBallActuatorBehaviour : SpringheadBehaviour {
+    public PHIKBallActuatorIf phIKAct = null;
+    public PHIKBallActuatorDescStruct ballActuatorDescriptor = null;
+
+    void Reset() {
+        SetDLLPath();
+        ballActuatorDescriptor = new PHIKBallActuatorDesc();
+    }
+
+    void Awake() {
+        PHSceneIf phScene = gameObject.GetComponentInParent<PHSceneBehaviour>().GetPHScene();
+
+        PHIKBallActuatorDesc dIK = ballActuatorDescriptor;
+        phIKAct = phScene.CreateIKActuator(PHIKBallActuatorIf.GetIfInfoStatic(), dIK).Cast();
+        phIKAct.SetName("ika:" + gameObject.name);
+        phIKAct.Enable(true);
+
+        PHBallJointBehaviour jointBehaviour = gameObject.transform.parent.GetComponentInParent<PHBallJointBehaviour>();
+        if (jointBehaviour != null && jointBehaviour.phJoint != null) {
+            phIKAct.AddChildObject(jointBehaviour.phJoint);
+        }
+    }
+
+    void Start () {
+        PHIKBallActuatorBehaviour ba = gameObject.transform.parent.GetComponentInParent<PHIKBallActuatorBehaviour>();
+        if (ba != null && ba.phIKAct != null && phIKAct != null) {
+            ba.phIKAct.AddChildObject(phIKAct);
+        }
+        OnValidate();
+    }
+
+    public void OnValidate() {
+        SetDLLPath();
+        if (phIKAct != null) {
+            phIKAct.SetBias(ballActuatorDescriptor.bias > 0.1F ? ballActuatorDescriptor.bias : 0.1F);
+            phIKAct.SetPullbackRate(ballActuatorDescriptor.pullbackRate > 0 ? ballActuatorDescriptor.pullbackRate : 0);
+        }
+    }
+}
---
+++ src/Unity/Assets/Springhead/PHIKEndEffectorBehaviour.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+using SprCs;
+
+public class PHIKEndEffectorBehaviour : SpringheadBehaviour {
+    public PHIKEndEffectorIf phIKee = null;
+    public PHIKEndEffectorDescStruct phIKeeDescriptor = null;
+
+    void Reset() {
+        SetDLLPath();
+        phIKeeDescriptor = new PHIKEndEffectorDesc();
+        phIKeeDescriptor.targetLocalDirection = new Vec3d(0, 0, 1);
+    }
+
+    void Awake() {
+        PHSceneIf phScene = gameObject.GetComponentInParent<PHSceneBehaviour>().GetPHScene();
+        PHIKEndEffectorDesc dIKEE = phIKeeDescriptor;
+        phIKee = phScene.CreateIKEndEffector(dIKEE);
+        phIKee.SetName("ike:" + gameObject.name);
+        phIKee.Enable(true);
+        phIKee.EnableOrientationControl(true);
+
+        PHSolidBehaviour solidBehaviour = gameObject.transform.parent.GetComponentInParent<PHSolidBehaviour>();
+        if (solidBehaviour != null && solidBehaviour.phSolid != null) {
+            phIKee.AddChildObject(solidBehaviour.phSolid);
+        }
+
+        UpdateIKTargetPosition();
+    }
+
+    void Start () {
+        PHIKBallActuatorBehaviour ba = gameObject.GetComponentInParent<PHIKBallActuatorBehaviour>();
+        if (ba != null && ba.phIKAct != null && phIKee != null) {
+            ba.phIKAct.AddChildObject(phIKee);
+        }
+    }
+
+    void Update () {
+        UpdateIKTargetPosition();
+    }
+
+    public void UpdateIKTargetPosition() {
+        GameObject iktarget = GameObject.Find("IKTarget");
+        if (iktarget != null) {
+            Vector3 p = iktarget.transform.position;
+            Quaternion q = iktarget.transform.rotation;
+            if (phIKee != null) {                
+                if (phIKee.GetOriCtlMode() == PHIKEndEffectorDesc.OriCtlMode.MODE_LOOKAT) {
+                    phIKee.SetTargetLookat(new Vec3f(p.x, p.y, p.z));
+                } else {
+                    phIKee.SetTargetPosition(new Vec3f(p.x, p.y, p.z));
+                    phIKee.SetTargetOrientation(new Quaternionf(q.w, q.x, q.y, q.z));
+                }
+            }
+        }
+    }
+}
