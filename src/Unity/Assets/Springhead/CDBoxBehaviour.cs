7e249db99c5e90fbe72150393f09199f908c7153
mitake <mitake@05cee5c3-a2e9-0310-9523-9dfc2f93dbe1>
2015-12-06 06:34:54 +0000

    git-svn-id: http://springhead.info/spr2/Springhead2/trunk@8134 05cee5c3-a2e9-0310-9523-9dfc2f93dbe1

---
+++ src/Unity/Assets/Springhead/CDBoxBehaviour.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using SprCs;
+
+public class CDBoxBehaviour : SpringheadBehaviour {
+    public CDBoxDescStruct boxDescripter = null;
+
+    void Reset() {
+        SetDLLPath();
+        boxDescripter = new CDBoxDesc();
+    }
+
+    void Awake () {
+        PHSceneIf phScene = gameObject.GetComponentInParent<PHSceneBehaviour>().GetPHScene();
+        PHSdkIf phSdk = phScene.GetSdk();
+
+        BoxCollider bc = gameObject.GetComponent<BoxCollider>();
+        PHSolidBehaviour solidBehaviour = gameObject.GetComponentInParent<PHSolidBehaviour>();
+        if (bc != null && solidBehaviour != null && solidBehaviour.phSolid != null) {
+            CDBoxDesc descBox = new CDBoxDesc();
+            Vector3 size = bc.size;
+            Vector3 scale = gameObject.transform.lossyScale;
+            descBox.boxsize = new Vec3f((float)(size.x * scale.x), (float)(size.y * scale.y), (float)(size.z * scale.z));
+            solidBehaviour.phSolid.AddShape(phSdk.CreateShape(CDBoxIf.GetIfInfoStatic(), descBox));
+        }
+    }
+
+    public void OnValidate() {
+
+    }
+}
---
+++ src/Unity/Assets/Springhead/CDSphereBehaviour.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using SprCs;
+
+public class CDSphereBehaviour : SpringheadBehaviour {
+    public CDSphereDescStruct sphereDescripter = null;
+
+    void Reset() {
+        SetDLLPath();
+        sphereDescripter = new CDSphereDesc();
+    }
+
+    void Awake() {
+        PHSceneIf phScene = gameObject.GetComponentInParent<PHSceneBehaviour>().GetPHScene();
+        PHSdkIf phSdk = phScene.GetSdk();
+
+        SphereCollider sc = gameObject.GetComponent<SphereCollider>();
+        PHSolidBehaviour solidBehaviour = gameObject.GetComponentInParent<PHSolidBehaviour>();
+        if (sc != null && solidBehaviour != null && solidBehaviour.phSolid != null) {
+            CDSphereDesc descSphere = new CDSphereDesc();
+            Vector3 scale = gameObject.transform.lossyScale;
+            descSphere.radius = sc.radius * (Mathf.Max(Mathf.Max(scale.x, scale.y), scale.z));
+            solidBehaviour.phSolid.AddShape(phSdk.CreateShape(CDSphereIf.GetIfInfoStatic(), descSphere));
+        }
+    }
+
+    public void OnValidate() {
+
+    }
+}
