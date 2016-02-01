7564471305938c7ddb3e0fdcbe26829e8b1133a0
dinghaiyang <dinghaiyang@05cee5c3-a2e9-0310-9523-9dfc2f93dbe1>
2016-02-01 05:24:29 +0000

    CDConvexMesh is on Unity
    git-svn-id: http://springhead.info/spr2/Springhead2/trunk@8210 05cee5c3-a2e9-0310-9523-9dfc2f93dbe1

---
+++ src/Unity/Assets/Springhead/CDConvexMeshBehaviour.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+using SprCs;
+
+public class CDConvexMeshBehaviour : SpringheadBehaviour {
+	public CDConvexMeshDescStruct meshDescripter = null;
+
+	void Reset() {
+		SetDLLPath();
+		meshDescripter = new CDConvexMeshDesc();
+	}
+
+	void Awake(){
+		PHSceneIf phScene = gameObject.GetComponentInParent<PHSceneBehaviour>().GetPHScene();
+		PHSdkIf phSdk = phScene.GetSdk();
+
+		Mesh mr = gameObject.GetComponent<MeshFilter>().mesh;
+		PHSolidBehaviour solidBehaviour = gameObject.GetComponentInParent<PHSolidBehaviour>();
+		if((mr != null)&&solidBehaviour != null && solidBehaviour.phSolid != null) {
+			CDConvexMeshDesc cmesh= new CDConvexMeshDesc();
+			//initial CDConvexMeshDesc by Unity Mesh
+			for(int vi=0;vi<mr.vertices.Length;vi++)
+			{
+				Vector3 vU = mr.vertices[vi];
+				Vec3f v = new Vec3f();
+				v.x = vU.x;
+				v.y = vU.y;
+				v.z = vU.z;
+				cmesh.vertices.push_back(v);
+			}
+			solidBehaviour.phSolid.AddShape(phSdk.CreateShape(CDConvexMeshIf.GetIfInfoStatic(), cmesh));
+		}
+	}
+
+	public void OnValidate() {
+
+	}
+}
