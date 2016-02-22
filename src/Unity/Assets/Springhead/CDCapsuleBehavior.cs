b851ea1d4a81bbc0a564df87770259d84fd3a9a8
orishige <orishige@05cee5c3-a2e9-0310-9523-9dfc2f93dbe1>
2016-02-22 13:11:26 +0000

    カプセルのCollision
    git-svn-id: http://springhead.info/spr2/Springhead2/trunk@8242 05cee5c3-a2e9-0310-9523-9dfc2f93dbe1

---
+++ src/Unity/Assets/Springhead/CDCapsuleBehavior.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using SprCs;
+
+public class CDCapsuleBehavior : SpringheadBehaviour{
+    public CDCapsuleDescStruct capsuleDescripter = null;
+
+    void Reset(){
+        SetDLLPath();
+        capsuleDescripter = new CDCapsuleDesc();
+    }
+
+    void Awake(){       
+        PHSceneIf phScene = gameObject.GetComponentInParent<PHSceneBehaviour>().GetPHScene();
+        PHSdkIf phSdk = phScene.GetSdk();
+         
+        CapsuleCollider cc = gameObject.GetComponent<CapsuleCollider>();
+        PHSolidBehaviour solidBehaviour = gameObject.GetComponentInParent<PHSolidBehaviour>();
+        if (cc != null && solidBehaviour != null && solidBehaviour.phSolid != null)
+        {
+            CDCapsuleDesc descCapsule = new CDCapsuleDesc();
+            Vector3 scale = gameObject.transform.lossyScale;
+            descCapsule.radius = cc.radius * (Mathf.Max(Mathf.Max(scale.x, scale.y), scale.z));
+            descCapsule.length = cc.height * scale.y;// cc.radius * (Mathf.Max(Mathf.Max(scale.x, scale.y), scale.z));
+            solidBehaviour.phSolid.AddShape(phSdk.CreateShape(CDCapsuleIf.GetIfInfoStatic(), descCapsule));
+        }
+        
+    }
+
+    public void OnValidate()
+    {
+
+    }
+}
