3d053d90f1e48cfcf00268b3ad150b67ace79002
kanehori <kanehori@05cee5c3-a2e9-0310-9523-9dfc2f93dbe1>
2016-02-22 03:30:28 +0000

    C# binding: SEH_Exception translator will be loaded/freed by DllMain.
    git-svn-id: http://springhead.info/spr2/Springhead2/trunk@8241 05cee5c3-a2e9-0310-9523-9dfc2f93dbe1

--- src/SprCSharp/SprImport/CSUtility.cs
+++
@@ -1,8 +0,0 @@
-//_[module: 1900] 
-using System;
-using System.Runtime.InteropServices;
-
-public partial class SprExport {
-	[DllImport("SprExport.dll", CallingConvention=CallingConvention.Cdecl)]
-	public static extern void initUtility();
-}
