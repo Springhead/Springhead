12049893ac102bdab25721fd5aef71ce2d57087b
mitake <mitake@05cee5c3-a2e9-0310-9523-9dfc2f93dbe1>
2016-03-24 02:58:01 +0000

    git-svn-id: http://springhead.info/spr2/Springhead2/trunk@8298 05cee5c3-a2e9-0310-9523-9dfc2f93dbe1

--- src/Unity/Assets/Springhead/CDRoundConeBehavior.cs
+++ src/Unity/Assets/Springhead/CDRoundConeBehavior.cs
@@ -1,10 +1,12 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Linq;
 using SprCs;
 
 public class CDRoundConeBehavior : SpringheadBehaviour
 {
     public CDRoundConeDescStruct roundConeDescripter = null;
+    public GameObject child = null;
 
     void Reset()
     {
@@ -16,37 +18,44 @@ public class CDRoundConeBehavior : SpringheadBehaviour
     {
         PHSceneIf phScene = gameObject.GetComponentInParent<PHSceneBehaviour>().GetPHScene();
         PHSdkIf phSdk = phScene.GetSdk();
-                                                                                      
-        GameObject child = gameObject.GetComponentInChildren<CDSphereBehaviour>().gameObject;
-        if (child == null)
-        {
-            Debug.LogError("child is null");
+
+        if (child == null) {
+            foreach (Transform x in gameObject.transform) {
+                if (x.gameObject.GetComponentInChildren<SphereCollider>() != null) {
+                    child = x.gameObject;
+                    break;
+                }
+            }
         }
-        SphereCollider sc1 = gameObject.GetComponent<SphereCollider>();
-        SphereCollider sc2 = child.GetComponent<SphereCollider>();
-        Transform tr1 = gameObject.GetComponent<Transform>();
-        Transform tr2 = child.GetComponent<Transform>();
-        PHSolidBehaviour solidBehaviour = gameObject.GetComponentInParent<PHSolidBehaviour>();
-        if (sc1 != null && sc2 != null && tr1 != null && tr2 != null && solidBehaviour != null && solidBehaviour.phSolid != null)
-        {
-            CDRoundConeDesc descRoundCone = new CDRoundConeDesc();
-            Vector3 scale1 = gameObject.transform.lossyScale;
-            Vector3 scale2 = child.transform.lossyScale; 
 
-            descRoundCone.radius = new Vec2f(sc2.radius * (Mathf.Max(Mathf.Max(scale2.x, scale2.y), scale2.z)), sc1.radius * (Mathf.Max(Mathf.Max(scale1.x, scale1.y), scale1.z)));
-            descRoundCone.length = (tr2.position-tr1.position).magnitude; 
+        if (child == null) {
+            Debug.LogError("child is null");
+        } else {
+            SphereCollider sc1 = gameObject.GetComponent<SphereCollider>();
+            SphereCollider sc2 = child.GetComponent<SphereCollider>();
+            Transform tr1 = gameObject.GetComponent<Transform>();
+            Transform tr2 = child.GetComponent<Transform>();
+            PHSolidBehaviour solidBehaviour = gameObject.GetComponentInParent<PHSolidBehaviour>();
+            if (sc1 != null && sc2 != null && tr1 != null && tr2 != null && solidBehaviour != null && solidBehaviour.phSolid != null) {
+                CDRoundConeDesc descRoundCone = new CDRoundConeDesc();
+                Vector3 scale1 = gameObject.transform.lossyScale;
+                Vector3 scale2 = child.transform.lossyScale;
 
-            solidBehaviour.phSolid.AddShape(phSdk.CreateShape(CDRoundConeIf.GetIfInfoStatic(), descRoundCone));
-            double dposx = descRoundCone.length * Mathf.Sin(tr1.rotation.x * Mathf.Deg2Rad);
-            double dposy = descRoundCone.length * Mathf.Cos(tr1.rotation.x * Mathf.Deg2Rad) * Mathf.Cos(tr1.rotation.z * Mathf.Deg2Rad);
-            double dposz = descRoundCone.length * Mathf.Sin(tr1.rotation.z * Mathf.Deg2Rad);                                             
-            Vec3d p = new Vec3d(dposx/2, dposy/2, dposz/2);            
-            Quaterniond q = Quaterniond.Rot(90.0 * Mathf.Deg2Rad, new Vec3d(1.0, 0.0, 0.0));
-            Quaterniond qx = Quaterniond.Rot(-tr1.rotation.x * Mathf.Deg2Rad, new Vec3d(1.0, 0.0, 0.0));
-            Quaterniond qy = Quaterniond.Rot(-tr1.rotation.y * Mathf.Deg2Rad, new Vec3d(0.0, 1.0, 0.0));
-            Quaterniond qz = Quaterniond.Rot(-tr1.rotation.z * Mathf.Deg2Rad, new Vec3d(0.0, 0.0, 1.0));
-            q = q * qx * qy * qz;
-            solidBehaviour.phSolid.SetShapePose(solidBehaviour.phSolid.NShape() - 1, new Posed(q.w, q.x, q.y, q.z, p.x, p.y, p.z));
+                descRoundCone.radius = new Vec2f(sc2.radius * (Mathf.Max(Mathf.Max(scale2.x, scale2.y), scale2.z)), sc1.radius * (Mathf.Max(Mathf.Max(scale1.x, scale1.y), scale1.z)));
+                descRoundCone.length = (tr2.position - tr1.position).magnitude;
+
+                solidBehaviour.phSolid.AddShape(phSdk.CreateShape(CDRoundConeIf.GetIfInfoStatic(), descRoundCone));
+                double dposx = descRoundCone.length * Mathf.Sin(tr1.rotation.x * Mathf.Deg2Rad);
+                double dposy = descRoundCone.length * Mathf.Cos(tr1.rotation.x * Mathf.Deg2Rad) * Mathf.Cos(tr1.rotation.z * Mathf.Deg2Rad);
+                double dposz = descRoundCone.length * Mathf.Sin(tr1.rotation.z * Mathf.Deg2Rad);
+                Vec3d p = new Vec3d(dposx / 2, dposy / 2, dposz / 2);
+                Quaterniond q = Quaterniond.Rot(90.0 * Mathf.Deg2Rad, new Vec3d(1.0, 0.0, 0.0));
+                Quaterniond qx = Quaterniond.Rot(-tr1.rotation.x * Mathf.Deg2Rad, new Vec3d(1.0, 0.0, 0.0));
+                Quaterniond qy = Quaterniond.Rot(-tr1.rotation.y * Mathf.Deg2Rad, new Vec3d(0.0, 1.0, 0.0));
+                Quaterniond qz = Quaterniond.Rot(-tr1.rotation.z * Mathf.Deg2Rad, new Vec3d(0.0, 0.0, 1.0));
+                q = q * qx * qy * qz;
+                solidBehaviour.phSolid.SetShapePose(solidBehaviour.phSolid.NShape() - 1, new Posed(q.w, q.x, q.y, q.z, p.x, p.y, p.z));
+            }
         }
     }                                             
     public void OnValidate()
---
+++ src/Unity/Assets/Springhead/Editor/RoundConeEditor.cs
@@ -0,0 +1,42 @@
+﻿using UnityEditor;
+using UnityEngine;
+using System.Collections;
+
+[CustomEditor(typeof(MeshRoundCone))]
+public class RoundConeEditor : Editor {
+	void OnSceneGUI () {
+        MeshRoundCone mrc = target as MeshRoundCone;
+        Transform trans = mrc.gameObject.transform;
+
+        float defLeng = HandleUtility.GetHandleSize(Vector3.left);
+        Vector3 pos = trans.position;
+        Vector3 scale = trans.localScale;
+        Quaternion rot = trans.rotation;
+
+        Vector3 posOffset = new Vector3(mrc.length * 0.5f, 0, 0);
+
+        EditorGUI.BeginChangeCheck();
+        float r1 = Handles.RadiusHandle(rot, pos - rot * posOffset, mrc.r1);
+        if (EditorGUI.EndChangeCheck()) {
+            Undo.RecordObject(target, "Change Radius 1");
+            mrc.r1 = r1;
+            mrc.OnValidate();
+        }
+
+        EditorGUI.BeginChangeCheck();
+        float r2 = Handles.RadiusHandle(rot, pos + rot * posOffset, mrc.r2);
+        if (EditorGUI.EndChangeCheck()) {
+            Undo.RecordObject(target, "Change Radius 2");
+            mrc.r2 = r2;
+            mrc.OnValidate();
+        }
+
+        EditorGUI.BeginChangeCheck();
+        float length = Handles.ScaleSlider(mrc.length, pos, rot * new Vector3(0,1,0), rot, defLeng, 0.5f);
+        if (EditorGUI.EndChangeCheck()) {
+            Undo.RecordObject(target, "Change Length");
+            mrc.length = length;
+            mrc.OnValidate();
+        }
+    }
+}
