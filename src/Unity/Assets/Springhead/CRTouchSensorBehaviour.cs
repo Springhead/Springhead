d38899b0919a5dd1425bf977c35db9c9fea97d16
kesumu <kesumu@05cee5c3-a2e9-0310-9523-9dfc2f93dbe1>
2016-04-08 09:24:49 +0000

    Add CRCreature part of sprBehaviours.
    git-svn-id: http://springhead.info/spr2/Springhead2/trunk@8336 05cee5c3-a2e9-0310-9523-9dfc2f93dbe1

---
+++ src/Unity/Assets/Springhead/CRTouchSensorBehaviour.cs
@@ -0,0 +1,83 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System;
+using SprCs;
+
+public class CRTouchSensorBehaviour : SprBehaviourBase
+{
+    public CRTouchSensorIf crTouchSensor = null;
+    public CRTouchSensorDescStruct touchSensorDescriptor = null;
+
+    void Reset()
+    {
+        SetDLLPath();
+
+        touchSensorDescriptor = new CRTouchSensorDesc();
+    }
+
+    void Awake()
+    {
+        CRCreatureIf crCreature = gameObject.GetComponentInParent<CRCreatureBehaviour>().GetCRCreature();
+
+        CRTouchSensorDesc d = touchSensorDescriptor;
+        //Here needs cast to CRVisualSensorIf
+        crTouchSensor = (CRTouchSensorIf)crCreature.CreateEngine(CRTouchSensorIf.GetIfInfoStatic(), d);
+        crTouchSensor.SetName("ts:" + gameObject.name);
+
+        //Here adds all bones to this body from this GameObject's childs
+        //Search all childs
+        Transform[] childGameObjects = this.gameObject.GetComponentsInChildren<Transform>();
+        for (int i = 0; i < childGameObjects.Length; i++)
+        {
+            //Dont get itself
+            if (childGameObjects[i] == this.gameObject.transform)
+            {
+                //Debug.Log("<color=blue>Body: </color>" + childGameObjects[i].gameObject.name);
+                continue;
+            }
+
+            //real childs
+            //If it has PHSolidBehaviour component, it is a valid bone, otherwise not
+            if (childGameObjects[i].gameObject.GetComponent<PHSolidBehaviour>() == null)
+            {
+                //Debug.Log("<color=blue>Body: </color>" + childGameObjects[i].gameObject.name);
+                continue;
+            }
+
+            //Add solid
+            PHSolidIf phSolid = childGameObjects[i].gameObject.GetComponent<PHSolidBehaviour>().sprObject as PHSolidIf;
+            crTouchSensor.AddChildObject(phSolid);
+
+            //Debug.Log("<color=yellow>#: </color>" + i);
+        }
+    }
+
+    void OnValidate()
+    {
+        SetDLLPath();
+    }
+
+    public List<CRContactInfo> GetTouchSensorData()
+    {
+        List<CRContactInfo> contactsInfo = new List<CRContactInfo>();
+        crTouchSensor.Update();
+        for (int i = 0; i < crTouchSensor.NContacts(); i++)
+        {
+            CRContactInfo info = crTouchSensor.GetContact(i);
+            contactsInfo.Add(info);
+        }
+        return contactsInfo;
+    }
+
+    //This is a test function
+    public void TestGetTouchSensorData()
+    {
+        crTouchSensor.Update();
+        for (int i = 0; i < crTouchSensor.NContacts(); i++)
+        {
+            CRContactInfo info = crTouchSensor.GetContact(i);
+            Debug.Log("<color=yellow>TouchSensor: </color>" + info.soOther.GetName());
+        }
+    }
+}
---
+++ src/Unity/Assets/Springhead/CRVisualSensorBehaviour.cs
@@ -0,0 +1,94 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System;
+using SprCs;
+
+public class CRVisualSensorBehaviour : SprBehaviourBase{
+
+    public GameObject head;
+
+    public CRVisualSensorIf crVisualSensor = null;
+    public CRVisualSensorDescStruct visualSensorDescriptor = null;
+
+    public Vector3 rotateAngleVector;
+
+    public bool debugMode;
+    public GameObject visualSensorVisualizer;
+
+    void Reset() {
+        SetDLLPath();
+        visualSensorDescriptor = new CRVisualSensorDesc();
+
+        visualSensorDescriptor.range = new Vec2d((Math.PI / 180) * (90), (Math.PI / 180) * (60));
+        visualSensorDescriptor.centerRange = new Vec2d((Math.PI / 180) * (10), (Math.PI / 180) * (10));
+        visualSensorDescriptor.limitDistance = 100;
+    }
+
+    void Awake() {
+        Posed posed = new Posed();
+        Quaternion rotateAngle = Quaternion.Euler(rotateAngleVector);
+        posed.w = rotateAngle.w;
+        posed.x = rotateAngle.x;
+        posed.y = rotateAngle.y;
+        posed.z = rotateAngle.z;
+        visualSensorDescriptor.pose = posed;
+
+        CRCreatureIf crCreature = gameObject.GetComponentInParent<CRCreatureBehaviour>().GetCRCreature();
+
+        CRVisualSensorDesc d = visualSensorDescriptor;
+        //Here needs cast to CRVisualSensorIf
+        crVisualSensor = (CRVisualSensorIf)crCreature.CreateEngine(CRVisualSensorIf.GetIfInfoStatic(), d);
+        crVisualSensor.SetName("vs:"+gameObject.name);
+        //Here depends on the GameObject that has PHSolidBehaviour
+        PHSolidIf phSolid = head.GetComponent<PHSolidBehaviour>().sprObject as PHSolidIf;
+        crVisualSensor.SetSolid(phSolid);
+
+        if(this.debugMode){
+            this.visualSensorDirectionVisualize();
+        }
+    }
+
+    public void visualSensorDirectionVisualize() {
+        
+        
+        
+        Quaternion visualSensorOri = new Quaternion((float)this.crVisualSensor.GetPose().Ori().x
+                                                    ,(float)this.crVisualSensor.GetPose().Ori().y
+                                                    ,(float)this.crVisualSensor.GetPose().Ori().z
+                                                    ,(float)this.crVisualSensor.GetPose().Ori().w);
+        Vector3 visualSensorPosition = new Vector3(
+                                     (float)this.crVisualSensor.GetSolid().GetCenterPosition().x
+                                     ,(float)this.crVisualSensor.GetSolid().GetCenterPosition().y
+                                     ,(float)this.crVisualSensor.GetSolid().GetCenterPosition().z);
+        GameObject visualizer = (GameObject)Instantiate(visualSensorVisualizer, visualSensorPosition, visualSensorOri);
+        visualizer.transform.parent = head.transform;
+    }
+
+    void OnValidate()
+    {
+        SetDLLPath();
+    }
+
+    public List<CRVisualInfo> GetVisualSensorData() {
+        List<CRVisualInfo> visiblesInfo = new List<CRVisualInfo>();
+        crVisualSensor.Update();
+        for (int i = 0; i < crVisualSensor.NVisibles(); i++)
+        {
+            CRVisualInfo info = crVisualSensor.GetVisible(i);
+            visiblesInfo.Add(info);
+        }
+        return visiblesInfo;
+    }
+
+    //This is a test function
+    public void TestGetVisualSensorData(){
+        crVisualSensor.Update();
+        for (int i = 0; i < crVisualSensor.NVisibles();i++ )
+        {
+            CRVisualInfo info = crVisualSensor.GetVisible(i);
+            Debug.Log("<color=green>VisualSensor: </color>" + info.solid.GetName());
+        }
+        crVisualSensor.GetPose();
+    }
+}
