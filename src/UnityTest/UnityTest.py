ccb74dab2999d36f7c0438d6826716b04fb5dcc3
kanehori <kanehori@05cee5c3-a2e9-0310-9523-9dfc2f93dbe1>
2016-06-16 08:09:36 +0000

    dailybuild: Add unity test scripts
    git-svn-id: http://springhead.info/spr2/Springhead2/trunk@8460 05cee5c3-a2e9-0310-9523-9dfc2f93dbe1

---
+++ src/UnityTest/UnityTest.py
@@ -0,0 +1,222 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# ======================================================================
+#  FILE:
+#	UnityTest.py
+#
+#  DESCRIPTION:
+#	Build and run unity scene automatically.
+#	Only one scene at a time is available.
+#
+#  VERSION:
+#	Ver 1.0  2016/06/16 F.Kanehori	First release
+# ======================================================================
+version = 1.0
+import sys
+import os
+import subprocess
+from multiprocessing import Process
+from time import sleep
+from datetime import datetime
+from datetime import timedelta
+from optparse import OptionParser
+from KvFile import KvFile
+
+# ----------------------------------------------------------------------
+def get_date():
+	today = str(datetime.today())
+	return today[0:19].replace('-', '/')
+
+def dir_part(path):
+	return path[0:path.rindex('/')]
+
+def s16(value):
+	return -(value & 0b1000000000000000) | (value & 0b0111111111111111)
+
+def result_str(code):
+	str = 'success' if code >= 0 else 'fail'
+	return '%d (%s)' % (code, str)
+
+def verbose(msg, level=0):
+	if options.verbose > level:
+		print(msg)
+
+def info(msg, name=None, has_next=False, continued=False):
+	if options.verbose > 0 or not continued:
+		if options.timestamp:
+			sys.stdout.write(get_date() + ': ')
+		if not name is None:
+			sys.stdout.write(name + ': ')
+	sys.stdout.write(msg)
+	eol = '.. ' if not options.verbose and has_next else '\n'
+	sys.stdout.write(eol)
+	sys.stdout.flush()
+
+def fatal(msg, code = -1):
+	sys.stderr.write(script + ': Error: ' + msg + '\n')
+	sys.exit(code)
+
+# ----------------------------------------------------------------------
+#  Process for command line
+#
+usage = "Usage: %prog [options] scene"
+parser = OptionParser(usage = usage)
+parser.add_option('-i', '--init',
+			dest='inifile', default='UnityTest.ini',
+			help='init file name [default: %default]', metavar='FILE')
+parser.add_option('-t', '--timeout',
+			dest='timeout', default=15, metavar='TIME',
+			help='execution limit time in seconds [default: %default]')
+parser.add_option('-T', '--timestamp',
+			dest='timestamp', action='store_true', default=False,
+			help='add time stamp')
+parser.add_option('-v', '--verbose',
+			dest='verbose', action='count', default=0,
+			help='set verbose mode')
+parser.add_option('-V', '--version',
+			dest='version', action='store_true', default=False,
+			help='show version')
+(options, args) = parser.parse_args()
+
+script = sys.argv[0].split('\\')[-1].split('.')[0]
+if options.version:
+	print('%s: Version %s' % (script, version))
+	sys.exit(0)
+#
+if len(args) != 1:
+	parser.error("incorrect number of arguments")
+
+# Scene name
+scene   = args[0]
+if scene[-6:] != '.unity':
+	scene += '.unity'
+
+if options.verbose:
+	print('  inifile:\t%s' % (options.inifile))
+	print('  scene:\t%s' % (scene))
+
+# ----------------------------------------------------------------------
+#  Read init file
+#
+mandatory_keys = ['Unity', 'Python', 'Springhead2', 'DllPath' ]
+
+kvf = KvFile(options.inifile)
+if (kvf.read() < 0):
+	sys.exit(-1)
+if kvf.check(mandatory_keys) != 0:
+	sys.exit(-1)
+
+# set defaults
+defaults = {}
+spr2root = kvf.get('Springhead2')
+defaults['UnityProject'] = spr2root + '/src/Unity'
+defaults['ScenesDir']	 = 'Assets/Scenes'
+defaults['TestRoot']	 = spr2root + '/UnityTest'
+defaults['OutFile']	 = defaults['TestRoot'] + '/player.exe'
+defaults['LogFile']	 = defaults['TestRoot'] + '/build.log'
+for key in defaults.keys():
+	if kvf.get(key) is None:
+		kvf.set(key, defaults[key])
+if options.verbose:
+	print('  ------------------------------------------------')
+	for key in sorted(mandatory_keys):
+		print('  %s:\t%s' % (key, kvf.get(key)))
+	print('  ------------------------------------------------')
+	for key in sorted(defaults.keys()):
+		print('  %s:\t%s' % (key, kvf.get(key)))
+
+# prepare directories
+outdir = dir_part(kvf.get('OutFile'))
+logdir = dir_part(kvf.get('LogFile'))
+if not os.path.isdir(outdir):
+	verbose('creating directory "%s"' % (outdir))
+	os.makedirs(outdir)
+if not os.path.isdir(logdir):
+	verbose('creating directory "%s"' % (outdir))
+	os.makedirs(logdir)
+
+# ----------------------------------------------------------------------
+#  Change ProjectSettings so as not to display runtime dialog
+#
+info('compiling ', scene, has_next=True)
+project_base = kvf.get('UnityProject')
+setting_file = project_base + '/ProjectSettings/ProjectSettings.asset'
+saved_file = setting_file + '.save'
+verbose('saving ' + setting_file)
+
+# save original one
+cmd = ' '.join(['copy', setting_file, saved_file, '> NUL'])
+result = subprocess.call(cmd.replace('/', '\\'), shell=True)
+if result != 0:
+	fatal('can\'t save ProjectSettings file')
+
+# replace parameter
+replace_pattern ="displayResolutionDialog: 1=displayResolutionDialog: 0"
+script = 'replace.py'
+params = '-p "' + replace_pattern + '"'
+if options.verbose:
+	params += ' -v'
+ifile = saved_file
+ofile = setting_file
+cmd = ' '.join([kvf.get('Python'), script, params, ifile, ofile])
+verbose(cmd, 1)
+result = subprocess.call(cmd, shell=True)
+if result != 0:
+	fatal('can\'t replace ProjectSettings file')
+
+# ----------------------------------------------------------------------
+#  Build and Run
+#
+cmd = ' '.join(['del', kvf.get('OutFile'), '1>NUL 2>&1'])
+verbose(cmd, 1)
+result = subprocess.call(cmd.replace('/', '\\'), shell=True)
+#
+projpath = '-projectPath ' + kvf.get('UnityProject')
+execmode = '-executeMethod BuildClass.Build'
+quiet	 = '-batchmode -quit'
+logfile	 = '-logfile ' + kvf.get('LogFile')
+outfile	 = '-output ' + kvf.get('OutFile')
+target	 = '-target ' + kvf.get('ScenesDir') + '/' + scene
+args = ' '.join([projpath, execmode, quiet, outfile, logfile, target])
+cmd  = ' '.join([kvf.get('Unity'), args])
+verbose(cmd, 1)
+#
+result = subprocess.call(cmd.replace('/', '\\'), shell=True)
+result = result if os.path.isfile(kvf.get('OutFile')) else -1
+info(result_str(s16(result)), scene, continued=True)
+exitcode = result
+#
+if result == 0:
+	info('executing ', scene, has_next=True)
+	cmd = kvf.get('OutFile')
+	newpath = os.environ.get('PATH') + ';' + kvf.get('DllPath').replace('/', '\\')
+	#
+	verbose(cmd, 1)
+	proc = subprocess.Popen(cmd.replace('/', '\\'), env={"PATH": newpath})
+	sleep(float(options.timeout))
+	if proc.poll() is None:
+		# process is still alive
+		proc.kill()
+		result = 0
+	else:
+		result = p.poll()
+	#
+	info(result_str(s16(result)), scene, continued=True)
+	exitcode = result
+
+# ----------------------------------------------------------------------
+#  Revice ProjectSettings
+#
+verbose('reviving ' + setting_file)
+
+cmd = ' '.join(['del', setting_file, '1>NUL 2>&1'])
+verbose(cmd, 1)
+result = subprocess.call(cmd.replace('/', '\\'), shell=True)
+
+cmd = ' '.join(['move', saved_file, setting_file, '1>NUL 2>&1'])
+verbose(cmd, 1)
+result = subprocess.call(cmd.replace('/', '\\'), shell=True)
+
+sys.exit(exitcode)
+
+# end: UnityTest.py
---
+++ src/UnityTest/replace.py
@@ -0,0 +1,101 @@
+#! C:/Python34/python
+# ==============================================================================
+#  SYNOPSIS:
+#	replace [options] infile outfile
+#	options:
+#	    -p PATTERN		specify replace pattern (from=to)
+#	    -v			set verbose mode
+#
+#  DESCRIPTION:
+#	Replace specified pattern(from) in the file to new pattern(to).
+#	Currently:
+#	    only one replace pattern can be specified
+#	    only one pattern per one line will be replaced
+#	    replacement will be applied to all lines in the file
+#
+#  VERSION:
+#       Ver 1.0  2016/06/16 F.Kanehori	First release
+# ==============================================================================
+version = 1.0
+import os
+import sys
+from optparse import OptionParser
+import types
+
+#-------------------------------------------------------------------------------
+# Constants
+#
+encoding = 'utf-8'
+
+#-------------------------------------------------------------------------------
+# Options
+#
+usage = "Usage: %prog [options] infile outfile"
+parser = OptionParser(usage = usage)
+parser.add_option('-p', '--pattern', action="store", dest='pattern',
+                    help='replace pattern (from=to)')
+parser.add_option('-v', '--verbose', action="count", dest='verbose',
+                    default=0, help='set verbose mode')
+parser.add_option('-V', '--version', action="store_true", dest='version',
+                    help='show version')
+(options, args) = parser.parse_args()
+#
+script = sys.argv[0].split('\\')[-1].split('.')[0]
+verbose = options.verbose
+if options.version:
+	print('%s: Version %s' % (script, version))
+	sys.exit(0)
+
+#-------------------------------------------------------------------------------
+def replace(ifname, ofname):
+	with open(ofname, 'w') as ofile:
+		with open(ifname, 'r') as ifile:
+			line = ifile.readline()
+			count = 1
+			while (line):
+				replaced = line.replace(pat_fm, pat_to)
+				ofile.write(replaced)
+				if verbose > 0 and replaced != line:
+					print(fixed(count, 4) + line.rstrip())
+					print("   => " + replaced.rstrip())
+				elif verbose > 1:
+					print(fixed(count, 4) + replaced.rstrip())
+				line = ifile.readline()
+				count += 1
+	return 0
+
+def fixed(num, width):
+	string = str(num)
+	length = len(string)
+	if length < width:
+		return ' ' * (width-length) + string + ': '
+	return string + ': '
+	
+#-------------------------------------------------------------------------------
+# Main process
+#
+status = 0
+if len(args) != 2:
+	parser.error("incorrect number of arguments")
+	sys.exit(-1)
+if not isinstance(options.pattern, str):
+	parser.error('pattern not specified')
+	sys.exit(-1)
+
+pattern = options.pattern.split('=')
+pat_fm = pattern[0]
+pat_to = pattern[1]
+
+ifname = args[0];
+ofname = args[1];
+if verbose > 0:
+	sys.stderr.write(script + ':\n')
+	sys.stderr.write('    ifile: ' + ifname + '\n')
+	sys.stderr.write('    ofile: ' + ofname + '\n')
+	sys.stderr.write('    pattern from: [' + pat_fm + ']\n') 
+	sys.stderr.write('    pattern to:   [' + pat_to + ']\n') 
+
+status = replace(ifname, ofname)
+sys.exit(status)
+
+#end: replace.py
