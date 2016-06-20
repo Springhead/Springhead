98621338c96f64978de6d5447f38d863da63723e
kanehori <kanehori@05cee5c3-a2e9-0310-9523-9dfc2f93dbe1>
2016-06-20 07:53:01 +0000

    dailybuild: unity test system
    git-svn-id: http://springhead.info/spr2/Springhead2/trunk@8466 05cee5c3-a2e9-0310-9523-9dfc2f93dbe1

--- src/UnityTest/log_extraction.py
+++ src/UnityTest/log_extraction.py
@@ -7,19 +7,28 @@
 #	Extract compiling warning/error log lines from unity build log file.
 #
 #  VERSION:
-#       Ver 1.0  2016/06/16 F.Kanehori  First release
+#       Ver 1.0  2016/06/20 F.Kanehori  First version
 # ==============================================================================
 version = 1.0
 import os
 import sys
 from optparse import OptionParser
 import re
-#import types
 
 #-------------------------------------------------------------------------------
 def verbose(msg, level=0):
 	if options.verbose > level:
 		sys.stderr.write(msg + '\n')
+
+def error(msg):
+	sys.stderr.write('%s: %s\n' % (script, msg))
+
+def is_new(str, list):
+	for s in list:
+		if s == str:
+			return False
+	list.append(str)
+	return True
 
 #-------------------------------------------------------------------------------
 # Options
@@ -35,7 +44,7 @@ parser.add_option('-V', '--version', action="store_true", dest='version',
 script = sys.argv[0].split('\\')[-1].split('.')[0]
 
 #-------------------------------------------------------------------------------
-# Constants
+# Patterns to search
 #
 str_tgt = '^-target: (.+)\.unity$'
 str_bgn = '^-----CompilerOutput:'
@@ -44,6 +53,7 @@ str_end = '^-----EndCompilerOutput---------------'
 pattern_tgt = re.compile(str_tgt)
 pattern_bgn = re.compile(str_bgn)
 pattern_end = re.compile(str_end)
+pattern_err = re.compile('error', re.IGNORECASE)
 
 #-------------------------------------------------------------------------------
 # Main process
@@ -53,6 +63,7 @@ if len(args) != 1:
 	sys.exit(-1)
 
 ifname = args[0];
+already_found = []
 with open(ifname, 'r') as ifile:
 	numlines = 0
 	count = 0
@@ -64,19 +75,17 @@ with open(ifname, 'r') as ifile:
 		include_one = False
 		verbose(line.rstrip(), 1)
 		if not no_more:
-			m = pattern_tgt.match(line)
-			if m:
+			if pattern_tgt.match(line) is not None:
 				include_one = True
-				no_more = True
-			m = pattern_bgn.match(line)
-			if m:
-				include = True
-			m = pattern_end.match(line)
-			if m:
+			elif pattern_bgn.match(line) is not None:
+				if is_new(line, already_found):
+					include = True
+			elif pattern_end.match(line) is not None:
+				if include:
+					include_one = True
 				include = False
-				include_one = True
 			if include or include_one:
-				print(line.rstrip())
+				print('%d: %s' % (numlines, line.rstrip()))
 				count += 1
 		# end not no_more
 		line = ifile.readline()
