a6e1c3d2ac1ce8d6a64a033cf30b37185b2928be
kanehori <kanehori@05cee5c3-a2e9-0310-9523-9dfc2f93dbe1>
2016-06-22 03:29:36 +0000

    dailybuild:
    git-svn-id: http://springhead.info/spr2/Springhead2/trunk@8470 05cee5c3-a2e9-0310-9523-9dfc2f93dbe1

--- src/Unity/Assets/Springhead/Editor/UnityBuildClass.cs
+++ src/Unity/Assets/Springhead/Editor/UnityBuildClass.cs
@@ -61,43 +61,6 @@ public class BuildClass : MonoBehaviour
         }
 	return "";
     }
-
-    // Unity起動引数を取得する
-    static string[] GetSceneNameX()
-    {
-        string[] sceneList = new string[1];
-        string[] progArgs = System.Environment.GetCommandLineArgs();
-	string[] args = progArgs[1].Split(' ');
-        int i, len = args.Length;
-        for (i = 0; i < len; ++i) {
-	    //Debug.Log("args[" + i + "]: " + args[i]);
-            switch (args[i]) {
-                case "-target":
-                    string scene = "Assets/Scenes/" + args[i+1];
-                    sceneList[0] = scene;
-                    Debug.Log( "-target : " + sceneList[0]);
-                    break;
-            }
-        }
-        return sceneList;
-    }
-
-    // ビルド対象シーンを Scenes In Build から抽出する
-    static string[] GetEnabeledScenes()
-    {
-        List<string> sceneList = new List<string>();
-
-        // "Scenes In Build"に登録されているシーンリストを取得
-        EditorBuildSettingsScene[] scenes = EditorBuildSettings.scenes;
-        foreach (EditorBuildSettingsScene scene in scenes) {
-            // チェックがついているか確認
-            if (scene.enabled) {
-                // リストに加える
-                sceneList.Add(scene.path);
-            }
-        }
-
-        // 配列にして返す
-        return sceneList.ToArray();
-    }
 }
+
+// end: UnityBuildClass.cs
