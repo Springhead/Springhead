6fe3f71486b1e123327a0d9055d15b672e6e046f
hase <hase@05cee5c3-a2e9-0310-9523-9dfc2f93dbe1>
2017-06-05 03:49:26 +0000

    動かなくなっていたので直しました。
    git-svn-id: http://springhead.info/spr2/Springhead2/trunk@8991 05cee5c3-a2e9-0310-9523-9dfc2f93dbe1

--- src/tests/Framework/FWAppBoxStack/BoxStack.cpp
+++ src/tests/Framework/FWAppBoxStack/BoxStack.cpp
@@ -14,7 +14,7 @@ BoxStack bstack;
 BoxStack::BoxStack(){
 	dt = 0.02;//0.05;
 	gravity =  Vec3d(0, -9.8f , 0);
-	nIter = 15;
+	nIter = 5;
 	bGravity = true;
 	bDebug = false;
 	bStep = true;
@@ -22,23 +22,17 @@ BoxStack::BoxStack(){
 
 void BoxStack::Init(int argc, char* argv[]){
 	FWApp::Init(argc, argv);
+	Vec3d pos = Vec3d(0, 5, 15.0);						// カメラ初期位置
+	GetCurrentWin()->GetTrackball()->SetPosition(pos);	// カメラ初期位置の設定
 
-	GetSdk()->Clear();															// SDKの作成
-	GetSdk()->CreateScene(PHSceneDesc(), GRSceneDesc());		// Sceneの作成
 	PHSceneIf* phscene = GetSdk()->GetScene()->GetPHScene();
 
-	DesignObject();																// 剛体を作成
-
+	BuildScene();		// 剛体を作成
 	phscene->SetGravity(gravity);				
 	phscene->SetTimeStep(dt);
-	phscene->SetNumIteration(nIter);
+	phscene->SetNumIteration((int)nIter);
 
-	FWWinDesc windowDesc;
-	windowDesc.title = "FWAppBoxStack";
-	window = CreateWin(windowDesc);
-	window->SetScene(GetSdk()->GetScene());
 
-	InitCameraView();
 }
 
 void BoxStack::Reset(){
@@ -46,53 +40,31 @@ void BoxStack::Reset(){
 	GetSdk()->CreateScene(PHSceneDesc(), GRSceneDesc());		// Sceneの作成
 	PHSceneIf* phscene = GetSdk()->GetScene()->GetPHScene();
 
-	DesignObject();																// 剛体を作成
+	BuildScene();																// 剛体を作成
 
 	phscene->SetGravity(gravity);				
 	phscene->SetTimeStep(dt);
-	phscene->SetNumIteration(nIter);
+	phscene->SetNumIteration((int)nIter);
 
 	GetCurrentWin()->SetScene(GetSdk()->GetScene());
-	InitCameraView();
-}
-void BoxStack::InitCameraView(){
-/*	cameraInfo.view[0][0] = 0.9996;
-	cameraInfo.view[0][1] = 0.0107463;
-	cameraInfo.view[0][2] = -0.0261432;
-	cameraInfo.view[0][3] = -0.389004;
-	cameraInfo.view[1][0] = -6.55577e-010;
-	cameraInfo.view[1][1] = 0.924909;
-	cameraInfo.view[1][2] = 0.380188;
-	cameraInfo.view[1][3] = 5.65711;
-	cameraInfo.view[2][0] = 0.0282657;
-	cameraInfo.view[2][1] = -0.380037;
-	cameraInfo.view[2][2] = 0.92454;
-	cameraInfo.view[2][3] = 13.7569;
-	cameraInfo.view[3][0] = 0;
-	cameraInfo.view[3][1] = 0;
-	cameraInfo.view[3][2] = 0;
-	cameraInfo.view[3][3] = 1;	*/
 }
 
-void BoxStack::DesignObject(){
+void BoxStack::BuildScene(){
 	// soFloor用のdesc
-	desc.mass = 1e20f;
-	desc.inertia *= 1e30f;
 	PHSceneIf* phscene = GetSdk()->GetScene()->GetPHScene();
 	soFloor = phscene->CreateSolid(desc);		// 剛体をdescに基づいて作成
 	soFloor->SetDynamical(false);
 	soFloor->SetGravity(false);
 	
 	// soBox用のdesc
-	desc.mass = 0.05;
-	desc.inertia = 0.033 * Matrix3d::Unit();
 	{
 		CDBoxDesc bd;
 		bd.boxsize = Vec3f(2,2,2);
+		bd.material.density = 2.0f;
 		meshBox = XCAST(GetSdk()->GetPHSdk()->CreateShape(bd));
 		meshBox->SetName("meshBox");
 		CDSphereDesc sd;
-		sd.radius = 1.2;
+		sd.radius = 1.2f;
 		meshSphere = XCAST(GetSdk()->GetPHSdk()->CreateShape(sd));
 		meshSphere->SetName("meshSphere");
 		CDCapsuleDesc cd;
@@ -126,7 +98,7 @@ void BoxStack::DesignObject(){
 		meshFloor->SetName("meshFloor");
 	}
 	soFloor->AddShape(meshFloor);
-	soFloor->SetFramePosition(Vec3d(0,-20 - 0.7,0));
+	soFloor->SetFramePosition(Vec3d(0, -20 - 0.7, 0));
 	soFloor->GetShape(0)->SetVibration(-100, 150, 150);
 	soFloor->SetName("solidFloor");
 }
@@ -144,52 +116,6 @@ void BoxStack::Step(){
 	if (exitCount > 60.0/dt) exit(0);
 }
 
-void BoxStack::Display(){
-	// 描画の設定
-	GetSdk()->SetDebugMode(true);
-	GRDebugRenderIf* render = window->render->Cast();
-
-	// 描画モードの設定
-	render->SetRenderMode(true, false);
-//	render->EnableRenderAxis(bDebug);
-	render->EnableRenderForce(bDebug);
-	render->EnableRenderContact(bDebug);
-
-	// カメラ座標の指定
-	GRCameraIf* cam = window->scene->GetGRScene()->GetCamera();
-	if (cam && cam->GetFrame()){
-		cam->GetFrame()->SetTransform(cameraInfo.view);
-	}else{
-		window->render->SetViewMatrix(cameraInfo.view.inv());
-	}
-
-	// 描画の実行
-	if(!GetCurrentWin()) return;
-	GRRenderIf*curRender =  GetCurrentWin()->GetRender();
-	FWSceneIf* curScene = GetCurrentWin()->GetScene();
-
-	GetSdk()->SwitchScene(curScene);
-	GetSdk()->SwitchRender(curRender);
-
-	if(!curRender) return;
-	curRender->ClearBuffer();
-	curRender->BeginScene();
-
-	if (curScene) curScene->Draw(curRender, GetSdk()->GetDebugMode());
-
-	//	光源の追加
-	GRLightDesc ld;
-	ld.diffuse = Vec4f(1,1,1,1) * 0.8f;
-	ld.specular = Vec4f(1,1,1,1) * 0.8f;
-	ld.ambient = Vec4f(1,1,1,1) * 0.4f;
-	ld.position = Vec4f(1,1,1,0);
-	render->PushLight(ld);
-	render->PopLight();	//	光源の削除
-
-	curRender->EndScene();
-	glutSwapBuffers();
-}
-
 void BoxStack::Keyboard(int key, int x, int y){
 	exitCount = 0;
 	PHSceneIf* phscene = GetSdk()->GetScene()->GetPHScene();
@@ -200,10 +126,6 @@ void BoxStack::Keyboard(int key, int x, int y){
 			break;
 		case 'r':
 			Reset();
-			break;
-		case 'w':
-			InitCameraView();
-			DSTR << "InitCameraView" << endl;
 			break;
 		case 'd':
 			if(bDebug){
@@ -243,8 +165,8 @@ void BoxStack::Keyboard(int key, int x, int y){
 				soBox.back()->AddShape(meshBox);
 				soBox.back()->SetFramePosition(Vec3d(-1, 5, 4));
 				soBox.back()->GetShape(0)->SetVibration(-200,120,300);
-				soBox.back()->GetShape(0)->SetStaticFriction(0.8);
-				soBox.back()->GetShape(0)->SetDynamicFriction(0.6);
+				soBox.back()->GetShape(0)->SetStaticFriction(0.8f);
+				soBox.back()->GetShape(0)->SetDynamicFriction(0.6f);
 				//soBox.back()->SetOrientation(
 				//	Quaternionf::Rot(Rad(30), 'y') * 
 				//	Quaternionf::Rot(Rad(10), 'x'));  
@@ -270,13 +192,13 @@ void BoxStack::Keyboard(int key, int x, int y){
 				desc.inertia[2][2] = 0.0325;
 
 				soBox.push_back(phscene->CreateSolid(desc));
-				soBox.back()->SetAngularVelocity(Vec3f(0,0,0.2));
+				soBox.back()->SetAngularVelocity(Vec3f(0, 0, 0.2f));
 				soBox.back()->AddShape(meshCapsule);
-				soBox.back()->SetFramePosition(Vec3f(0.5, 20,0));
+				soBox.back()->SetFramePosition(Vec3f(0.5f, 20, 0));
 				soBox.back()->GetShape(0)->SetVibration(-80,200,150);
-				soBox.back()->GetShape(0)->SetElasticity(0.1);
+				soBox.back()->GetShape(0)->SetElasticity(0.1f);
 //				soBox.back()->SetFramePosition(Vec3f(0.5, 10+3*soBox.size(),0));
-				soBox.back()->SetOrientation(Quaternionf::Rot(Rad(30), 'y'));  
+				soBox.back()->SetOrientation(Quaternionf::Rot((float)Rad(30), 'y'));  
 				ostringstream os;
 				os << "capsule" << (unsigned int)soBox.size();
 				soBox.back()->SetName(os.str().c_str());
@@ -290,9 +212,9 @@ void BoxStack::Keyboard(int key, int x, int y){
 				soBox.push_back(phscene->CreateSolid(desc));
 				soBox.back()->AddShape(meshSphere);
 //				soBox.back()->SetFramePosition(Vec3f(0.5, 10+3*soBox.size(),0));
-				soBox.back()->GetShape(0)->SetElasticity(0.4);
-				soBox.back()->SetFramePosition(Vec3f(0.5, 20,0));
-				soBox.back()->SetOrientation(Quaternionf::Rot(Rad(30), 'y'));  
+				soBox.back()->GetShape(0)->SetElasticity(0.4f);
+				soBox.back()->SetFramePosition(Vec3f(0.5f, 20, 0));
+				soBox.back()->SetOrientation(Quaternionf::Rot((float)Rad(30), 'y'));  
 				ostringstream os;
 				os << "sphere" << (unsigned int)soBox.size();
 				soBox.back()->SetName(os.str().c_str());
@@ -315,13 +237,13 @@ void BoxStack::Keyboard(int key, int x, int y){
 				}
 				CDShapeIf* s = GetSdk()->GetPHSdk()->CreateShape(md);
 				soBox.back()->AddShape(s);
-				soBox.back()->SetFramePosition(Vec3f(0.5, 20,0));
+				soBox.back()->SetFramePosition(Vec3f(0.5, 20, 0));
 //				soBox.back()->SetFramePosition(Vec3f(0.5, 10+3*soBox.size(),0));
 				soBox.back()->GetShape(0)->SetVibration(-100,60,100);
-				soBox.back()->GetShape(0)->SetStaticFriction(0.8);
-				soBox.back()->GetShape(0)->SetDynamicFriction(0.6);
+				soBox.back()->GetShape(0)->SetStaticFriction(0.8f);
+				soBox.back()->GetShape(0)->SetDynamicFriction(0.6f);
 
-				soBox.back()->SetOrientation(Quaternionf::Rot(Rad(30), 'y'));  
+				soBox.back()->SetOrientation(Quaternionf::Rot((float)Rad(30), 'y'));  
 				ostringstream os;
 				os << "sphere" << (unsigned int)soBox.size();
 				soBox.back()->SetName(os.str().c_str());
@@ -352,9 +274,9 @@ void BoxStack::Keyboard(int key, int x, int y){
 				pose.Pos() = Vec3d(0, 0, -3);
 				soBox.back()->SetShapePose(6, pose);
 				
-				soBox.back()->SetFramePosition(Vec3f(0.5, 20,0));
+				soBox.back()->SetFramePosition(Vec3f(0.5, 20, 0));
 	//			soBox.back()->SetFramePosition(Vec3f(0.5, 10+3*soBox.size(),0));
-				soBox.back()->SetOrientation(Quaternionf::Rot(Rad(30), 'y'));  
+				soBox.back()->SetOrientation(Quaternionf::Rot((float)Rad(30), 'y'));  
 				ostringstream os;
 				os << "box" << (unsigned int)soBox.size();
 				soBox.back()->SetName(os.str().c_str());
@@ -375,8 +297,8 @@ void BoxStack::Keyboard(int key, int x, int y){
 						soBox.back()->AddShape(meshBox);
 						soBox.back()->SetFramePosition(Vec3d(-4.0 + (2.0 + 0.1) * j , (2.0 + 0.1) * (double)i, -2.0));  
 						soBox.back()->GetShape(0)->SetVibration(-200,120,300);
-						soBox.back()->GetShape(0)->SetStaticFriction(0.8);
-						soBox.back()->GetShape(0)->SetDynamicFriction(0.6);
+						soBox.back()->GetShape(0)->SetStaticFriction(0.8f);
+						soBox.back()->GetShape(0)->SetDynamicFriction(0.6f);
 
 					}
 				}
@@ -401,8 +323,8 @@ void BoxStack::Keyboard(int key, int x, int y){
 						soBox.back()->SetFramePosition(Vec3d(tower_radius * cos(theta), 2.0 * ((double)i), tower_radius * sin(theta)));
 						soBox.back()->SetOrientation(Quaterniond::Rot(-theta, 'y'));  
 						soBox.back()->GetShape(0)->SetVibration(-200,120,300);
-						soBox.back()->GetShape(0)->SetStaticFriction(0.4);
-						soBox.back()->GetShape(0)->SetDynamicFriction(0.3);
+						soBox.back()->GetShape(0)->SetStaticFriction(0.4f);
+						soBox.back()->GetShape(0)->SetDynamicFriction(0.3f);
 					}
 				}
 			}
