b9a1bc1e69dd04bf398b8d41692550c32ff6143d
hase <hasevr@gmail.com>
2019-10-25 23:26:46 +0900

    Fix old lines brought by merge of finger

--- core/src/Physics/PHOpParticle.h
+++ core/src/Physics/PHOpParticle.h
@@ -1,4 +1,11 @@
-﻿#ifndef PHOpParticle_H
+﻿/*
+*  Copyright (c) 2003-2008, Shoichi Hasegawa and Springhead development team
+*  All rights reserved.
+*  This software is free software. You can freely use, distribute and modify this
+*  software. Please deal with this software under one of the following licenses:
+*  This license itself, Boost Software License, The MIT License, The BSD License.
+*/
+#ifndef PHOpParticle_H
 #define PHOpParticle_H
 
 #include "Base/TQuaternion.h"
@@ -20,14 +27,6 @@ namespace Spr{;
 //	when simulation : build currCenter ,CurrOrien
 //
 */
-
-//friction
-enum FrictionState {
-	FREE,
-	STATIC,
-	DYNAMIC,
-};
-
 class PHOpParticle :public SceneObject, public PHOpParticleDesc
 {
 	
@@ -40,8 +39,7 @@ public :
 	{
 		//
 		pType = 0;//0 は表面particleで、1は内部particle
-		pTempSingleVMass = 1.0f;// 0.001f * 8.0f / 43.0f;
-		//pTempSingleVMass = 0.001f * 8.0f / 43.0f;
+		pSingleVMass = 1.0f;
 		pParaAlpha = 1.0f;
 		pNvertex = 0;
 		pWvel = pVelocity = Vec3f(0.0,0.0,0.0);
@@ -58,16 +56,9 @@ public :
 		hitedByMouse = false;
 		isFixed = false;
 		pMomentInertia = pMomentInertia.Zero();
-		//pColliedForceMove = pColliedForceMove.Zero();
 		
 		initialWArr = false;
 		initialPvarr = false;
-
-		//friction
-		pfrictionState = FREE;
-		fricCount = 0;
-		isColliedBound = false;
-
 	}
 	
 private:
@@ -129,22 +120,7 @@ public :
 	
 	//粒子の到達位置（未使用）
 	std::vector<Vec3f> pGoalCtr;
-
-
-	//friction
-	FrictionState pfrictionState;
-	unsigned fricCount;
-	bool isColliedBound;
 	
-
-	//Delayed Force Base.()
-	
-
-	//The list which include the groups that contain this particle
-	
-	
-
-
 	int getVertexGlbIndex(int vindex)
 	{
 		return pVertArr[vindex];
@@ -163,15 +139,9 @@ public :
 
 	void preCalcu(Vec3f *mPos);
 	
-	void addVertMomtInertias(Vec3f &position);
 	void ptclBuildStep(Vec3f *mPos);
 	
-	void angleVelocityBuild(float dt);
-	void angleVelocityIntegration(float dt);
-
 	bool addNewVertex(int index);
-	
-	void addLinkParticle(int parIndex);
 	
 	void buildParticleCenter(Vec3f *mPos);
 	
@@ -225,6 +195,4 @@ struct DisCmpPoint
 		};
 
 }//namespace
-
-
 #endif
