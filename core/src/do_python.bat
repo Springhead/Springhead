d8681ae460a9e59deeed8af136cb5f2b2a85de87
Kanehori <Kanehori@haselab.net>
2021-03-24 14:04:44 +0900

    do_python.bat SprTopを正しく取れるように修正

--- core/src/do_python.bat
+++ core/src/do_python.bat
@@ -20,23 +20,31 @@ setlocal enabledelayedexpansion
 ::    Ver 3.0    2020/12/07 F.Kanehori	Setup 導入移行期間開始.
 ::    Ver 4.0    2021/03/04 F.Kanehori	Setupは非標準、python 2.7もOK.
 ::    Ver 4.1    2021/03/18 F.Kanehori	判定及びメッセージ修正
+::    Ver 4.1.1  2021/03/24 F.Kanehori	Bug fix.
 :: ============================================================================
 set dry_run=0
 set verbose=0
 
-set SprTop=..\..\..
+:: -------------------------------------------------------------
+::  このスクリプトは "<SprTop>/core/src" に置く
+::
+set CWD=%cd%
+cd /d %~dp0\..\..
+set SprTop=%CD%
+cd %CWD%
+
 ::-----------------------------------------------------------------------------
 :: "%SprTop%/buildtool/win32/python.exe" があれば、それを使用する。
 :: さもなければデフォルトの python を使用する。
 ::
-set BUILDTOOL_PATH=%SprTop%\buildtool\win32
-if exist "%BUILDTOOL_PATH%\python.exe" (
-	set PATH=%BUILDTOOL_PATH%;!PATH!
+set BUILDTOOL_PATH=%SprTop%\buildtool
+if exist "%BUILDTOOL_PATH%\win32\python.exe" (
+	Path=%BUILDTOOL_PATH%\win32;%BUILDTOOL_PATH%;!PATH!
 )
 
 set python_found=0
 if %verbose% equ 0 (
-	where python >NUL 2>1
+	where python >NUL 2>&1
 ) else (
 	echo where python
 	where python
@@ -55,13 +63,13 @@ if %python_found% neq 1 (
 	endlocal
 	exit /b
 )
-echo do_python.bat: using %py_version%
 
 ::----------------------------------------------
 ::  Python を実行する（引数はそのまま渡す）
 ::
 set ARGS=%*
-echo python %ARGS%
+echo (SprTop: %SprTop%, using %py_version%)
+echo (python %ARGS%)
 if %dry_run% equ 0 (
 	python %ARGS%
 )
